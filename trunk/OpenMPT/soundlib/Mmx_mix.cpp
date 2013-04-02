svn2github/OpenMPT

commit cff78a6b7d38a330cf2125d3eb17685432377014

[Mod] Remove SYSMIX_SLOWCPU and associated lower quality resampling in some cases. In 2013, the user can just disable interpolation for CPUs that old. Also remove SYSMIX_FASTCPU which meant exactly the same as SYSMIX_ENABLEMMX.
git-svn-id: https://source.openmpt.org/svn/openmpt@1691 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Mmx_mix.cpp
@@ -137,11 +137,9 @@ DWORD CSoundFile::InitSysInfo()
 			dwProcSupport &= PROCSUPPORT_CPUID|PROCSUPPORT_MMX|PROCSUPPORT_3DNOW|PROCSUPPORT_MMXEX;
 		}
 	}
-	if (dwProcSupport & PROCSUPPORT_MMX) d |= (SYSMIX_ENABLEMMX|SYSMIX_FASTCPU);
 	if (dwProcSupport & PROCSUPPORT_MMXEX) d |= SYSMIX_MMXEX;
 	if (dwProcSupport & PROCSUPPORT_3DNOW) d |= SYSMIX_3DNOW;
 	if (dwProcSupport & PROCSUPPORT_SSE) d |= SYSMIX_SSE;
-	if (!(dwProcSupport & PROCSUPPORT_CPUID)) d |= SYSMIX_SLOWCPU;
 	gdwSysInfo = d;
 	return d;
 }

trunk/OpenMPT/mptrack/Mpdlgs.cpp
@@ -518,9 +518,7 @@ BOOL COptionsPlayer::OnInitDialog()
 	GetDlgItem(IDC_CHECK5)->ShowWindow(SW_HIDE);
 #endif
 #ifndef NO_EQ
-	if (CSoundFile::GetSysInfo() & SYSMIX_SLOWCPU)
-		::EnableWindow(::GetDlgItem(m_hWnd, IDC_CHECK3), FALSE);
-	else if (dwQuality & QUALITY_EQ) CheckDlgButton(IDC_CHECK3, MF_CHECKED);
+	if (dwQuality & QUALITY_EQ) CheckDlgButton(IDC_CHECK3, MF_CHECKED);
 #else
 	GetDlgItem(IDC_CHECK3)->ShowWindow(SW_HIDE);
 	::EnableWindow(::GetDlgItem(m_hWnd, IDC_CHECK3), FALSE);

trunk/OpenMPT/soundlib/Snd_defs.h
@@ -234,8 +234,6 @@ enum enmEnvelopeTypes
 
 // Mixer Hardware-Dependent features
 #define SYSMIX_ENABLEMMX	0x01		// Hardware acceleration features (MMX/3DNow!/SSE) are supported by this processor
-#define SYSMIX_SLOWCPU		0x02		// *Really* old processor (in this context, it doesn't know CPUID instructions => Must be older than Pentium)
-#define SYSMIX_FASTCPU		0x04		// "Fast" processor (in this context, anything that is at least a Pentium MMX)
 #define SYSMIX_MMXEX		0x08		// Processor supports AMD MMX extensions
 #define SYSMIX_3DNOW		0x10		// Processor supports AMD 3DNow! instructions
 #define SYSMIX_SSE			0x20		// Processor supports SSE instructions

trunk/OpenMPT/soundlib/Sndmix.cpp
@@ -1603,10 +1603,10 @@ void CSoundFile::ProcessRamping(ModChannel *pChn)
 		LONG nRightDelta = ((pChn->nNewRightVol - pChn->nRightVol) << VOLUMERAMPPRECISION);
 		LONG nLeftDelta = ((pChn->nNewLeftVol - pChn->nLeftVol) << VOLUMERAMPPRECISION);
 //		if ((gdwSoundSetup & SNDMIX_DIRECTTODISK)
-//			|| ((gdwSysInfo & (SYSMIX_ENABLEMMX|SYSMIX_FASTCPU))
+//			|| ((gdwSysInfo & (SYSMIX_ENABLEMMX))
 //			&& (gdwSoundSetup & SNDMIX_HQRESAMPLER)))
 		if((gdwSoundSetup & SNDMIX_DIRECTTODISK)
-			|| ((gdwSysInfo & (SYSMIX_ENABLEMMX | SYSMIX_FASTCPU)) && (gdwSoundSetup & SNDMIX_HQRESAMPLER) && !enableCustomRamp))
+			|| ((gdwSysInfo & SYSMIX_ENABLEMMX) && (gdwSoundSetup & SNDMIX_HQRESAMPLER) && !enableCustomRamp))
 		{
 			if((pChn->nRightVol | pChn->nLeftVol) && (pChn->nNewRightVol | pChn->nNewLeftVol) && !pChn->dwFlags[CHN_FASTVOLRAMP])
 			{
@@ -2044,11 +2044,7 @@ BOOL CSoundFile::ReadNote()
 					if ((!(gdwSoundSetup & SNDMIX_DIRECTTODISK)) && (!(gdwSoundSetup & SNDMIX_ULTRAHQSRCMODE)))
 					{
 						int fmax = 0x20000;
-						if (gdwSysInfo & SYSMIX_SLOWCPU)
-						{
-							fmax = 0xFE00;
-						} else
-						if (!(gdwSysInfo & (SYSMIX_ENABLEMMX|SYSMIX_FASTCPU))) 
+						if (!(gdwSysInfo & SYSMIX_ENABLEMMX)) 
 						{
 							fmax = 0x18000;
 						}
@@ -2067,8 +2063,7 @@ BOOL CSoundFile::ReadNote()
 					
 				} else
 				{
-					if ((pChn->nInc >= 0x14000)
-					|| ((pChn->nInc >= 0xFF00) && ((pChn->nInc < 0x10100) || (gdwSysInfo & SYSMIX_SLOWCPU))))
+					if ((pChn->nInc >= 0x14000) || ((pChn->nInc >= 0xFF00) && (pChn->nInc < 0x10100)))
 						pChn->dwFlags.set(CHN_NOIDO);
 				}
 			}

commit 12b69b0aad38c736acb9061d6d093f18abc9202b

[Fix] Set SYSMIX_ENABLEMMX if the CPU supports MMX. This was accidentally broken in r1691
git-svn-id: https://source.openmpt.org/svn/openmpt@1710 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Mmx_mix.cpp
@@ -137,6 +137,7 @@ DWORD CSoundFile::InitSysInfo()
 			dwProcSupport &= PROCSUPPORT_CPUID|PROCSUPPORT_MMX|PROCSUPPORT_3DNOW|PROCSUPPORT_MMXEX;
 		}
 	}
+	if (dwProcSupport & PROCSUPPORT_MMX) d |= SYSMIX_ENABLEMMX;
 	if (dwProcSupport & PROCSUPPORT_MMXEX) d |= SYSMIX_MMXEX;
 	if (dwProcSupport & PROCSUPPORT_3DNOW) d |= SYSMIX_3DNOW;
 	if (dwProcSupport & PROCSUPPORT_SSE) d |= SYSMIX_SSE;