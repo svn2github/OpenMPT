svn2github/OpenMPT

commit 65dec5ee82543dacc20197a1330a6faf37faa40b

[Fix] Avoid putting CSoundFile on the stack. [Ref] Untangle header dependencies in libopenmpt_impl.hpp. [Ref] Only use forward declarations for soundlib/ in libopenmpt_impl.hpp. [Ref] Add missing common/Logging.h to libopenmpt project file.
git-svn-id: https://source.openmpt.org/svn/openmpt@2249 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/libopenmpt/libopenmpt_c.cpp
@@ -7,7 +7,7 @@
  * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
  */
 
-#include "common/stdafx.h"
+#include "BuildSettings.h"
 
 #include "libopenmpt_internal.h"
 #include "libopenmpt.h"
@@ -15,6 +15,7 @@
 
 #include "libopenmpt_impl.hpp"
 
+#include <sstream>
 #include <stdexcept>
 
 #include <cmath>
@@ -130,7 +131,7 @@ static std::string format_exception( const char * const function ) {
 static void report_exception( const char * const function, openmpt_log_func const logfunc = nullptr, void * const user = 0, openmpt::module_impl * const impl = nullptr ) {
 	const std::string message = format_exception( function );
 	if ( impl ) {
-		impl->PushToCSoundFileLog( LogError, message );
+		impl->PushToCSoundFileLog( message );
 	} else if ( logfunc ) {
 		logfunc( message.c_str(), user );
 	} else {
@@ -234,7 +235,7 @@ const char * openmpt_get_supported_extensions(void) {
 	try {
 		std::string retval;
 		bool first = true;
-		std::vector<std::string> supported_extensions = openmpt::get_supported_extensions();
+		std::vector<std::string> supported_extensions = openmpt::module_impl::get_supported_extensions();
 		for ( std::vector<std::string>::iterator i = supported_extensions.begin(); i != supported_extensions.end(); ++i ) {
 			if ( first ) {
 				first = false;
@@ -253,7 +254,7 @@ int openmpt_is_extension_supported( const char * extension ) {
 		if ( !extension ) {
 			return 0;
 		}
-		return openmpt::is_extension_supported( extension ) ? 1 : 0;
+		return openmpt::module_impl::is_extension_supported( extension ) ? 1 : 0;
 	} OPENMPT_INTERFACE_CATCH;
 	return 0;
 }

trunk/OpenMPT/libopenmpt/libopenmpt_impl.hpp
@@ -10,16 +10,16 @@
 #ifndef LIBOPENMPT_IMPL_HPP
 #define LIBOPENMPT_IMPL_HPP
 
-#include "common/stdafx.h"
-
 #include "libopenmpt_internal.h"
 #include "libopenmpt.hpp"
-#include "libopenmpt.h"
 
 #include <memory>
+#include <ostream>
 
-#include "soundlib/Sndfile.h"
-#include "soundlib/FileReader.h"
+// forward declarations
+enum LogLevel;
+class FileReader;
+class CSoundFile;
 
 namespace openmpt {
 
@@ -43,14 +43,12 @@ private:
 	const char * text;
 }; // class exception_message
 
-class log_interface : public ILog {
+class log_interface {
 protected:
 	log_interface();
 public:
 	virtual ~log_interface();
 	virtual void log( const std::string & message ) const = 0;
-private:
-	void AddToLog( LogLevel level, const std::string & text ) const;
 }; // class log_interface
 
 class std_ostream_log : public log_interface {
@@ -62,35 +60,32 @@ public:
 	virtual void log( const std::string & message ) const;
 }; // class CSoundFileLog_std_ostream
 
+class log_forwarder;
+
 class module_impl {
 protected:
-	std::shared_ptr<log_interface> m_LogForwarder;
+	std::shared_ptr<log_interface> m_Log;
+	std::unique_ptr<log_forwarder> m_LogForwarder;
 	std::vector<std::int16_t> m_int16Buffer;
 	std::vector<float> m_floatBuffer;
 	double m_currentPositionSeconds;
-protected:
-	mutable CSoundFile m_sndFile;
-	std::vector<std::pair<LogLevel,std::string> > m_loaderMessages;
-	class loader_log : public ILog {
-	private:
-		module_impl & self;
-	public:
-		loader_log( module_impl & s );
-	private:
-		void AddToLog( LogLevel level, const std::string & text ) const;
-	}; // class loader_log
+	std::unique_ptr<CSoundFile> m_sndFile;
+	std::vector<std::string> m_loaderMessages;
 public:
-	void PushToCSoundFileLog( LogLevel level, const std::string & text ) const;
+	void PushToCSoundFileLog( const std::string & text ) const;
+	void PushToCSoundFileLog( const LogLevel & level, const std::string & text ) const;
 private:
 	std::int32_t get_quality() const;
 	void set_quality( std::int32_t value );
-	void apply_mixer_settings( std::int32_t samplerate, int channels, SampleFormat format );
+	void apply_mixer_settings( std::int32_t samplerate, int channels, bool format_float );
 	void apply_libopenmpt_defaults();
 	void init();
-	static void load( CSoundFile & sndFile, FileReader file );
-	void load( FileReader file );
+	static void load( CSoundFile & sndFile, const FileReader & file );
+	void load( const FileReader & file );
 	std::size_t read_wrapper( void * buffer, std::size_t count );
 public:
+	static std::vector<std::string> get_supported_extensions();
+	static bool is_extension_supported( const std::string & extension );
 	static double could_open_propability( std::istream & stream, double effort, std::shared_ptr<log_interface> log );
 	module_impl( std::istream & stream, std::shared_ptr<log_interface> log );
 	module_impl( const std::vector<std::uint8_t> & data, std::shared_ptr<log_interface> log );

trunk/OpenMPT/libopenmpt/libopenmpt.vcxproj
@@ -317,6 +317,7 @@ copy /y bin\libopenmpt.dll bin\libmodplug.dll
     <ClInclude Include="..\common\BuildSettings.h" />
     <ClInclude Include="..\common\CompilerDetect.h" />
     <ClInclude Include="..\common\FlagSet.h" />
+    <ClInclude Include="..\common\Logging.h" />
     <ClInclude Include="..\common\misc_util.h" />
     <ClInclude Include="..\common\mptString.h" />
     <ClInclude Include="..\common\mutex.h" />

trunk/OpenMPT/libopenmpt/libopenmpt.vcxproj.filters
@@ -219,6 +219,9 @@
     <ClInclude Include="..\test\test.h">
       <Filter>Header Files\test</Filter>
     </ClInclude>
+    <ClInclude Include="..\common\Logging.h">
+      <Filter>Header Files\common</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="..\common\AudioCriticalSection.cpp">

trunk/OpenMPT/libopenmpt/libopenmpt_cxx.cpp
@@ -7,7 +7,7 @@
  * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
  */
 
-#include "common/stdafx.h"
+#include "BuildSettings.h"
 
 #include "libopenmpt_internal.h"
 #include "libopenmpt.hpp"
@@ -15,7 +15,6 @@
 #include "libopenmpt_impl.hpp"
 
 #include <algorithm>
-#include <iterator>
 #include <stdexcept>
 
 //#ifndef NO_LIBOPENMPT_CXX
@@ -51,17 +50,11 @@ std::string get( const std::string & key ) {
 } // namespace string
 
 std::vector<std::string> get_supported_extensions() {
-	std::vector<std::string> retval;
-	std::vector<const char *> extensions = CSoundFile::GetSupportedExtensions( false );
-	std::copy( extensions.begin(), extensions.end(), std::back_insert_iterator<std::vector<std::string> >( retval ) );
-	return retval;
+	return openmpt::module_impl::get_supported_extensions();
 }
 
 bool is_extension_supported( const std::string & extension ) {
-	std::vector<std::string> extensions = get_supported_extensions();
-	std::string lowercase_ext = extension;
-	std::transform( lowercase_ext.begin(), lowercase_ext.end(), lowercase_ext.begin(), tolower );
-	return std::find( extensions.begin(), extensions.end(), lowercase_ext ) != extensions.end();
+	return openmpt::module_impl::is_extension_supported( extension );
 }
 
 double could_open_propability( std::istream & stream, double effort, std::ostream & log, const detail::api_version_checker & ) {

trunk/OpenMPT/libopenmpt/libopenmpt_interactive.cpp
@@ -1,5 +1,5 @@
 
-#include "stdafx.h"
+#include "BuildSettings.h"
 
 #include "libopenmpt_internal.h"
 #include "libopenmpt_interactive.hpp"

commit 087732708b1e78b9a18e1817e7457c57b3c6815e

[Fix] Actually forward log messages in libopenmpt (broke in r2249). [Fix] libopenmpt.h needs <stddef.h>. [Fix] GCC build fixes.
git-svn-id: https://source.openmpt.org/svn/openmpt@2250 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/libopenmpt/libopenmpt.h
@@ -11,6 +11,7 @@
 #define LIBOPENMPT_H
 
 #include "libopenmpt_config.h"
+#include <stddef.h>
 #include <stdint.h>
 
 #ifdef __cplusplus

trunk/OpenMPT/libopenmpt/libopenmpt_c.cpp
@@ -15,6 +15,7 @@
 
 #include "libopenmpt_impl.hpp"
 
+#include <limits>
 #include <sstream>
 #include <stdexcept>
 

trunk/OpenMPT/libopenmpt/libopenmpt_impl.cpp
@@ -60,7 +60,7 @@ class log_forwarder : public ILog {
 private:
 	std::shared_ptr<log_interface> destination;
 public:
-	log_forwarder( std::shared_ptr<log_interface> dest ) {
+	log_forwarder( std::shared_ptr<log_interface> dest ) : destination(dest) {
 		return;
 	}
 	virtual ~log_forwarder() {
@@ -104,8 +104,8 @@ void loader_log::AddToLog( LogLevel level, const std::string & text ) const {
 void module_impl::PushToCSoundFileLog( const std::string & text ) const {
 	m_sndFile->AddToLog( LogError, text );
 }
-void module_impl::PushToCSoundFileLog( const LogLevel & level, const std::string & text ) const {
-	m_sndFile->AddToLog( level, text );
+void module_impl::PushToCSoundFileLog( int loglevel, const std::string & text ) const {
+	m_sndFile->AddToLog( (LogLevel)loglevel, text );
 }
 
 std::int32_t module_impl::get_quality() const {

trunk/OpenMPT/libopenmpt/libopenmpt_impl.hpp
@@ -17,7 +17,6 @@
 #include <ostream>
 
 // forward declarations
-enum LogLevel;
 class FileReader;
 class CSoundFile;
 
@@ -73,7 +72,7 @@ protected:
 	std::vector<std::string> m_loaderMessages;
 public:
 	void PushToCSoundFileLog( const std::string & text ) const;
-	void PushToCSoundFileLog( const LogLevel & level, const std::string & text ) const;
+	void PushToCSoundFileLog( int loglevel, const std::string & text ) const;
 private:
 	std::int32_t get_quality() const;
 	void set_quality( std::int32_t value );