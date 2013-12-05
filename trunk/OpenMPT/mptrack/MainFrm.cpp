svn2github/OpenMPT

commit d63eb8221d58d16ce2afef663151cad6cf66f546

[Imp] sounddev: Add hidden setting [Sound Settings]KeepDeviceOpen which keeps the sound device open when playback is stopped and thereby avoids pontential latency caused by (re-)opening the device.
git-svn-id: https://source.openmpt.org/svn/openmpt@3349 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/MainFrm.cpp
@@ -459,6 +459,8 @@ void CMainFrame::OnClose()
 
 	if(gpSoundDevice)
 	{
+		gpSoundDevice->Stop();
+		gpSoundDevice->Close();
 		delete gpSoundDevice;
 		gpSoundDevice = nullptr;
 	}
@@ -786,6 +788,8 @@ bool CMainFrame::audioOpenDevice()
 	const SoundDeviceID deviceID = TrackerSettings::Instance().GetSoundDeviceID();
 	if(gpSoundDevice && (gpSoundDevice->GetDeviceID() != deviceID))
 	{
+		gpSoundDevice->Stop();
+		gpSoundDevice->Close();
 		delete gpSoundDevice;
 		gpSoundDevice = nullptr;
 	}
@@ -819,14 +823,6 @@ bool CMainFrame::audioOpenDevice()
 	deviceSettings.sampleFormat = actualSampleFormat;
 	TrackerSettings::Instance().SetSoundDeviceSettings(deviceID, deviceSettings);
 	return true;
-}
-
-
-bool CMainFrame::audioReopenDevice()
-//----------------------------------
-{
-	audioCloseDevice();
-	return audioOpenDevice();
 }
 
 
@@ -1211,7 +1207,10 @@ void CMainFrame::StopPlayback()
 		m_NotifyTimer = 0;
 	}
 	ResetNotificationBuffer();
-	audioCloseDevice();
+	if(!TrackerSettings::Instance().m_SoundSettingsKeepDeviceOpen)
+	{
+		audioCloseDevice();
+	}
 }
 
 
@@ -1596,25 +1595,23 @@ HWND CMainFrame::GetFollowSong() const
 BOOL CMainFrame::SetupSoundCard(const SoundDeviceSettings &deviceSettings, SoundDeviceID deviceID)
 //------------------------------------------------------------------------------------------------
 {
-	const bool isPlaying = IsPlaying();
 	if((TrackerSettings::Instance().GetSoundDeviceID() != deviceID) || (TrackerSettings::Instance().GetSoundDeviceSettings(deviceID) != deviceSettings))
 	{
-		CModDoc *pActiveMod = NULL;
-		if (isPlaying)
+		CModDoc *pActiveMod = nullptr;
+		if(IsPlaying())
 		{
 			if ((m_pSndFile) && (!m_pSndFile->IsPaused())) pActiveMod = GetModPlaying();
 			PauseMod();
 		}
+		gpSoundDevice->Close();
 		TrackerSettings::Instance().SetSoundDeviceID(deviceID);
 		TrackerSettings::Instance().SetSoundDeviceSettings(deviceID, deviceSettings);
-
 		TrackerSettings::Instance().MixerOutputChannels = deviceSettings.Channels;
 		TrackerSettings::Instance().MixerSamplerate = deviceSettings.Samplerate;
+		if(pActiveMod)
 		{
-			CriticalSection cs;
-			if (pActiveMod) UpdateAudioParameters(pActiveMod->GetrSoundFile(), FALSE);
+			PlayMod(pActiveMod);
 		}
-		if (pActiveMod) PlayMod(pActiveMod);
 		UpdateWindow();
 	} else
 	{

trunk/OpenMPT/mptrack/Mainfrm.h
@@ -343,7 +343,6 @@ public:
 	bool InNotifyHandler() const { return m_InNotifyHandler; }
 
 	bool audioOpenDevice();
-	bool audioReopenDevice();
 	void audioCloseDevice();
 	bool IsAudioDeviceOpen() const;
 	bool DoNotification(DWORD dwSamplesRead, int64 streamPosition);

trunk/OpenMPT/mptrack/TrackerSettings.cpp
@@ -154,6 +154,7 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 	// Sound Settings
 	, m_SoundSampleRates(conf, "Sound Settings", "SampleRates", GetDefaultSampleRates())
 	, m_MorePortaudio(conf, "Sound Settings", "MorePortaudio", false)
+	, m_SoundSettingsKeepDeviceOpen(conf, "Sound Settings", "KeepDeviceOpen", false)
 	, m_SoundDeviceSettingsUseOldDefaults(false)
 	, m_SoundDeviceIdentifier(conf, "Sound Settings", "Device", std::wstring())
 	, MixerMaxChannels(conf, "Sound Settings", "MixChannels", MixerSettings().m_nMaxMixChannels)

trunk/OpenMPT/mptrack/TrackerSettings.h
@@ -274,6 +274,7 @@ public:
 	
 	Setting<std::vector<uint32> > m_SoundSampleRates;
 	Setting<bool> m_MorePortaudio;
+	Setting<bool> m_SoundSettingsKeepDeviceOpen;
 
 	bool m_SoundDeviceSettingsUseOldDefaults;
 	SoundDeviceID m_SoundDeviceID_DEPRECATED;

commit 44591c2a3be6e7a972dbdb6d8e458826e897c7f3

[Fix] OpenMPT crashed if the sound device got changed before any output had ever been started (broke in r3349 ).
git-svn-id: https://source.openmpt.org/svn/openmpt@3385 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/MainFrm.cpp
@@ -1603,7 +1603,10 @@ BOOL CMainFrame::SetupSoundCard(const SoundDeviceSettings &deviceSettings, Sound
 			if ((m_pSndFile) && (!m_pSndFile->IsPaused())) pActiveMod = GetModPlaying();
 			PauseMod();
 		}
-		gpSoundDevice->Close();
+		if(gpSoundDevice)
+		{
+			gpSoundDevice->Close();
+		}
 		TrackerSettings::Instance().SetSoundDeviceID(deviceID);
 		TrackerSettings::Instance().SetSoundDeviceSettings(deviceID, deviceSettings);
 		TrackerSettings::Instance().MixerOutputChannels = deviceSettings.Channels;