svn2github/OpenMPT

commit 8ce11506b513ab60bbbe0c42b38da4359f361529

[Mod] openmpt123: For the default portaudio output, use the portaudio default latency instead of our own default.
git-svn-id: https://source.openmpt.org/svn/openmpt@3421 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/openmpt123/openmpt123.cpp
@@ -1594,6 +1594,7 @@ static int main( int argc, char * argv [] ) {
 			case ModeUI:
 			case ModeBatch: {
 				if ( flags.use_stdout ) {
+					flags.apply_default_buffer_sizes();
 					stdout_stream_raii stdout_audio_stream;
 					render_files( flags, log, stdout_audio_stream );
 				} else if ( !flags.output_filename.empty() ) {
@@ -1606,11 +1607,13 @@ static int main( int argc, char * argv [] ) {
 #endif
 #if defined( MPT_WITH_SDL )
 				} else if ( flags.driver == "sdl" || flags.driver.empty() ) {
+					flags.apply_default_buffer_sizes();
 					sdl_stream_raii sdl_stream( flags, log );
 					render_files( flags, log, sdl_stream );
 #endif
 #if defined( WIN32 )
 				} else if ( flags.driver == "waveout" || flags.driver.empty() ) {
+					flags.apply_default_buffer_sizes();
 					waveout_stream_raii waveout_stream( flags );
 					render_files( flags, log, waveout_stream );
 #endif
@@ -1618,6 +1621,7 @@ static int main( int argc, char * argv [] ) {
 			} break;
 			case ModeRender: {
 				for ( std::vector<std::string>::iterator filename = flags.filenames.begin(); filename != flags.filenames.end(); ++filename ) {
+					flags.apply_default_buffer_sizes();
 					file_audio_stream_raii file_audio_stream( flags, *filename + std::string(".") + flags.output_extension, log );
 					render_file( flags, *filename, log, file_audio_stream );
 				}

trunk/OpenMPT/openmpt123/openmpt123.hpp
@@ -219,6 +219,9 @@ static inline std::string mode_to_string( Mode mode ) {
 	return "";
 }
 
+static const std::int32_t default_low = -2;
+static const std::int32_t default_high = -1;
+
 struct commandlineflags {
 	Mode mode;
 	bool canUI;
@@ -255,12 +258,24 @@ struct commandlineflags {
 	std::string output_extension;
 	bool force_overwrite;
 	bool paused;
+	void apply_default_buffer_sizes() {
+		if ( ui_redraw_interval == default_high ) {
+			ui_redraw_interval = 50;
+		} else if ( ui_redraw_interval == default_low ) {
+			ui_redraw_interval = 10;
+		}
+		if ( buffer == default_high ) {
+			buffer = 250;
+		} else if ( buffer == default_low ) {
+			buffer = 50;
+		}
+	}
 	commandlineflags() {
 		mode = ModeUI;
-		ui_redraw_interval = 50;
+		ui_redraw_interval = default_high; // 50 ... (-2 == 10)
 		driver = "";
 		device = -1;
-		buffer = 250;
+		buffer = default_high; // 250 ... (-2 == 50)
 		samplerate = 48000;
 		channels = 2;
 		use_float = true;

trunk/OpenMPT/openmpt123/openmpt123_portaudio.hpp
@@ -92,12 +92,13 @@ private:
 	openmpt123::mutex audioMutex;
 	bool use_float;
 public:
-	portaudio_stream_callback_raii( const commandlineflags & flags, std::ostream & log = std::cerr )
+	portaudio_stream_callback_raii( commandlineflags & flags, std::ostream & log = std::cerr )
 		: portaudio_raii(flags.verbose, log)
 		, write_buffers_blocking_wrapper(flags)
 		, stream(NULL)
 		, use_float(flags.use_float)
 	{
+		flags.apply_default_buffer_sizes();
 		PaStreamParameters streamparameters;
 		std::memset( &streamparameters, 0, sizeof(PaStreamParameters) );
 		streamparameters.device = ( flags.device == -1 ) ? Pa_GetDefaultOutputDevice() : flags.device;
@@ -178,7 +179,7 @@ private:
 	std::vector<float> sampleBufFloat;
 	std::vector<std::int16_t> sampleBufInt;
 public:
-	portaudio_stream_blocking_raii( const commandlineflags & flags, std::ostream & log = std::cerr )
+	portaudio_stream_blocking_raii( commandlineflags & flags, std::ostream & log = std::cerr )
 		: portaudio_raii(flags.verbose, log)
 		, stream(NULL)
 		, interleaved(false)
@@ -189,14 +190,53 @@ public:
 		streamparameters.device = ( flags.device == -1 ) ? Pa_GetDefaultOutputDevice() : flags.device;
 		streamparameters.channelCount = flags.channels;
 		streamparameters.sampleFormat = ( flags.use_float ? paFloat32 : paInt16 ) | paNonInterleaved;
-		streamparameters.suggestedLatency = flags.buffer * 0.001;
+		if ( flags.buffer == default_high ) {
+			streamparameters.suggestedLatency = Pa_GetDeviceInfo( streamparameters.device )->defaultHighOutputLatency;
+			flags.buffer = static_cast<std::int32_t>( Pa_GetDeviceInfo( streamparameters.device )->defaultHighOutputLatency * 1000.0 );
+		} else if ( flags.buffer == default_low ) {
+			streamparameters.suggestedLatency = Pa_GetDeviceInfo( streamparameters.device )->defaultLowOutputLatency;
+			flags.buffer = static_cast<std::int32_t>( Pa_GetDeviceInfo( streamparameters.device )->defaultLowOutputLatency * 1000.0 );
+		} else {
+			streamparameters.suggestedLatency = flags.buffer * 0.001;
+		}
+		unsigned long framesperbuffer = 0;
+		if ( flags.mode != ModeUI ) {
+			framesperbuffer = paFramesPerBufferUnspecified;
+			flags.ui_redraw_interval = 50;
+			flags.ui_redraw_interval = std::min<std::int32_t>( flags.ui_redraw_interval, flags.buffer / 3 );
+		} else if ( flags.ui_redraw_interval == default_high ) {
+			framesperbuffer = paFramesPerBufferUnspecified;
+			flags.ui_redraw_interval = 50;
+			flags.ui_redraw_interval = std::min<std::int32_t>( flags.ui_redraw_interval, flags.buffer / 3 );
+		} else if ( flags.ui_redraw_interval == default_low ) {
+			framesperbuffer = paFramesPerBufferUnspecified;
+			flags.ui_redraw_interval = 10;
+			flags.ui_redraw_interval = std::min<std::int32_t>( flags.ui_redraw_interval, flags.buffer / 3 );
+		} else {
+			framesperbuffer = flags.ui_redraw_interval * flags.samplerate / 1000;
+		}
+		if ( flags.ui_redraw_interval <= 0 ) {
+			flags.ui_redraw_interval = 1;
+		}
+		if ( flags.verbose ) {
+			log << "PortAudio:" << std::endl;
+			log << " device: "
+				<< streamparameters.device
+				<< " [ " << Pa_GetHostApiInfo( Pa_GetDeviceInfo( streamparameters.device )->hostApi )->name << " / " << Pa_GetDeviceInfo( streamparameters.device )->name << " ] "
+				<< std::endl;
+			log << " low latency: " << Pa_GetDeviceInfo( streamparameters.device )->defaultLowOutputLatency << std::endl;
+			log << " high latency: " << Pa_GetDeviceInfo( streamparameters.device )->defaultHighOutputLatency << std::endl;
+			log << " suggested latency: " << streamparameters.suggestedLatency << std::endl;
+			log << " frames per buffer: " << framesperbuffer << std::endl;
+			log << " ui redraw: " << flags.ui_redraw_interval << std::endl;
+		}
 		PaError e = PaError();
-		e = Pa_OpenStream( &stream, NULL, &streamparameters, flags.samplerate, ( flags.mode == ModeUI ) ? flags.ui_redraw_interval * flags.samplerate / 1000 : paFramesPerBufferUnspecified, 0, NULL, NULL );
+		e = Pa_OpenStream( &stream, NULL, &streamparameters, flags.samplerate, framesperbuffer, 0, NULL, NULL );
 		if ( e != paNoError ) {
 			// Non-interleaved failed, try interleaved next.
 			// This might help broken portaudio on MacOS X.
 			streamparameters.sampleFormat &= ~paNonInterleaved;
-			e = Pa_OpenStream( &stream, NULL, &streamparameters, flags.samplerate, ( flags.mode == ModeUI ) ? flags.ui_redraw_interval * flags.samplerate / 1000 : paFramesPerBufferUnspecified, 0, NULL, NULL );
+			e = Pa_OpenStream( &stream, NULL, &streamparameters, flags.samplerate, framesperbuffer, 0, NULL, NULL );
 			if ( e == paNoError ) {
 				interleaved = true;
 			}
@@ -204,11 +244,6 @@ public:
 		}
 		check_portaudio_error( Pa_StartStream( stream ) );
 		if ( flags.verbose ) {
-			log << "PortAudio:" << std::endl;
-			log << " device: "
-				<< streamparameters.device
-				<< " [ " << Pa_GetHostApiInfo( Pa_GetDeviceInfo( streamparameters.device )->hostApi )->name << " / " << Pa_GetDeviceInfo( streamparameters.device )->name << " ] "
-				<< std::endl;
 			log << " channels: " << streamparameters.channelCount << std::endl;
 			log << " sampleformat: " << ( ( ( streamparameters.sampleFormat & ~paNonInterleaved ) == paFloat32 ) ? "paFloat32" : "paInt16" ) << std::endl;
 			log << " latency: " << Pa_GetStreamInfo( stream )->outputLatency << std::endl;
@@ -313,6 +348,11 @@ static std::string show_portaudio_devices() {
 				}
 				devices << Pa_GetDeviceInfo( i )->name;
 			}
+			devices << " (";
+			devices << "high latency: " << Pa_GetDeviceInfo( i )->defaultHighOutputLatency;
+			devices << ", ";
+			devices << "low latency: " << Pa_GetDeviceInfo( i )->defaultLowOutputLatency;
+			devices << ")";
 			devices << std::endl;
 		}
 	}

commit 1d65a0a5940e320a8f05f999d9fbca291e628335

[Fix] openmpt123: Fix crash when using file output in UI mode: Initializing the buffer sizes was skipped (broken since r3421 ) ( http://bugs.openmpt.org/view.php?id=493 ).
git-svn-id: https://source.openmpt.org/svn/openmpt@3925 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/openmpt123/openmpt123.cpp
@@ -1765,6 +1765,7 @@ static int main( int argc, char * argv [] ) {
 					stdout_stream_raii stdout_audio_stream;
 					render_files( flags, log, stdout_audio_stream );
 				} else if ( !flags.output_filename.empty() ) {
+					flags.apply_default_buffer_sizes();
 					file_audio_stream_raii file_audio_stream( flags, flags.output_filename, log );
 					render_files( flags, log, file_audio_stream );
 #if defined( MPT_WITH_PORTAUDIO )