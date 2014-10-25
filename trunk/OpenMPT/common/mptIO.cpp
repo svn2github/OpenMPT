svn2github/OpenMPT

commit 2442716361a8b3465ba1b994a6c16c305e76ae89

[Ref] Move CMappedFile to mptFstream.h. [Ref] Add class Inputfile that wraps the differences of using either mpt::ifstream or CMappedFile. [Ref] Convert CMappedFile::GetFile() to GetFileReader(CMappedFile&) or GetFileReader(InputFile&). [Ref] Note: Work left to do (not done here in order to separate functional refactoring from moving files around): common/mptFstream.h should be renamed to commmon/mptFileIO.h. common/mptFileIO.cpp and soundlib/FileReader.cpp should be introduced. mptrack/MemoryMappedFile.* should be deleted.
git-svn-id: https://source.openmpt.org/svn/openmpt@4487 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/mptIO.cpp
@@ -255,3 +255,219 @@ IFileDataContainer::off_t FileDataContainerStdStream::GetReadableLength(IFileDat
 
 OPENMPT_NAMESPACE_END
 
+
+
+// following ode should be moved to common/mptFileIO.cpp and common/mptFstream.h should be renamed to common/mptFileIO.h.
+
+#include "mptFstream.h"
+
+
+OPENMPT_NAMESPACE_BEGIN
+
+
+#ifdef MODPLUG_TRACKER
+
+CMappedFile::~CMappedFile()
+//-------------------------
+{
+	Close();
+}
+
+
+bool CMappedFile::Open(const mpt::PathString &filename)
+//-----------------------------------------------------
+{
+	m_hFile = CreateFileW(
+		filename.AsNative().c_str(),
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+	if(m_hFile == INVALID_HANDLE_VALUE)
+	{
+		m_hFile = nullptr;
+		return false;
+	}
+	m_FileName = filename;
+	return true;
+}
+
+
+void CMappedFile::Close()
+//-----------------------
+{
+	m_FileName = mpt::PathString();
+	// Unlock file
+	if(m_hFMap)
+	{
+		if(m_pData)
+		{
+			UnmapViewOfFile(m_pData);
+			m_pData = nullptr;
+		}
+		CloseHandle(m_hFMap);
+		m_hFMap = nullptr;
+	} else if(m_pData)
+	{
+		free(m_pData);
+		m_pData = nullptr;
+	}
+
+	// Close file handle
+	if(m_hFile)
+	{
+		CloseHandle(m_hFile);
+		m_hFile = nullptr;
+	}
+}
+
+
+size_t CMappedFile::GetLength()
+//-----------------------------
+{
+	LARGE_INTEGER size;
+	if(GetFileSizeEx(m_hFile, &size) == FALSE)
+	{
+		return 0;
+	}
+	return mpt::saturate_cast<size_t>(size.QuadPart);
+}
+
+
+const void *CMappedFile::Lock()
+//-----------------------------
+{
+	size_t length = GetLength();
+	if(!length) return nullptr;
+
+	void *lpStream;
+
+	HANDLE hmf = CreateFileMapping(
+		m_hFile,
+		NULL,
+		PAGE_READONLY,
+		0, 0,
+		NULL);
+
+	// Try memory-mapping first
+	if(hmf)
+	{
+		lpStream = MapViewOfFile(
+			hmf,
+			FILE_MAP_READ,
+			0, 0,
+			length);
+		if(lpStream)
+		{
+			m_hFMap = hmf;
+			m_pData = lpStream;
+			return lpStream;
+		}
+		CloseHandle(hmf);
+		hmf = nullptr;
+	}
+	
+	// Fallback if memory-mapping fails for some weird reason
+	if((lpStream = malloc(length)) == nullptr) return nullptr;
+	memset(lpStream, 0, length);
+	size_t bytesToRead = length;
+	size_t bytesRead = 0;
+	while(bytesToRead > 0)
+	{
+		DWORD chunkToRead = mpt::saturate_cast<DWORD>(length);
+		DWORD chunkRead = 0;
+		if(ReadFile(m_hFile, (char*)lpStream + bytesRead, chunkToRead, &chunkRead, NULL) == FALSE)
+		{
+			// error
+			free(lpStream);
+			return nullptr;
+		}
+		bytesRead += chunkRead;
+		bytesToRead -= chunkRead;
+	}
+	m_pData = lpStream;
+	return lpStream;
+}
+
+
+#endif // MODPLUG_TRACKER
+
+
+
+InputFile::InputFile()
+{
+	return;
+}
+
+InputFile::InputFile(const mpt::PathString &filename)
+	: m_Filename(filename)
+{
+	#if defined(MPT_FILEREADER_STD_ISTREAM)
+		m_File.open(m_Filename, std::ios::binary | std::ios::in);
+	#else
+		m_File.Open(m_Filename);
+	#endif
+}
+
+InputFile::~InputFile()
+{
+	return;
+}
+
+
+bool InputFile::Open(const mpt::PathString &filename)
+{
+	m_Filename = filename;
+	#if defined(MPT_FILEREADER_STD_ISTREAM)
+		m_File.open(m_Filename, std::ios::binary | std::ios::in);
+		return m_File.good();
+	#else
+		return m_File.Open(m_Filename);
+	#endif
+}
+
+
+bool InputFile::IsValid() const
+{
+	#if defined(MPT_FILEREADER_STD_ISTREAM)
+		return m_File.good();
+	#else
+		return m_File.IsOpen();
+	#endif
+}
+
+#if defined(MPT_FILEREADER_STD_ISTREAM)
+
+InputFile::ContentsRef InputFile::Get()
+{
+	InputFile::ContentsRef result;
+	result.first = &m_File;
+	result.second = m_File.good() ? &m_Filename : nullptr;
+	return result;
+}
+
+#else
+
+InputFile::ContentsRef InputFile::Get()
+{
+	InputFile::ContentsRef result;
+	result.first.data = nullptr;
+	result.first.size = 0;
+	result.second = nullptr;
+	if(!m_File.IsOpen())
+	{
+		return result;
+	}
+	result.first.data = reinterpret_cast<const char*>(m_File.Lock());
+	result.first.size = m_File.GetLength();
+	result.second = &m_Filename;
+	return result;
+}
+
+#endif
+
+
+
+OPENMPT_NAMESPACE_END

trunk/OpenMPT/common/mptFstream.h
@@ -18,6 +18,7 @@
 #include "../common/mptPathString.h"
 #if defined(MPT_WITH_PATHSTRING)
 #include "../common/mptIO.h"
+#include <utility>
 #endif
 
 
@@ -487,6 +488,64 @@ public:
 
 
 } // namespace mpt
+
+
+
+#ifdef MODPLUG_TRACKER
+//===============
+class CMappedFile
+//===============
+{
+protected:
+	HANDLE m_hFile;
+	HANDLE m_hFMap;
+	void *m_pData;
+	mpt::PathString m_FileName;
+
+public:
+	CMappedFile() : m_hFile(nullptr), m_hFMap(nullptr), m_pData(nullptr) { }
+	~CMappedFile();
+
+public:
+	bool Open(const mpt::PathString &filename);
+	bool IsOpen() const { return m_hFile != NULL && m_hFile != INVALID_HANDLE_VALUE; }
+	const mpt::PathString * GetpFilename() const { return &m_FileName; }
+	void Close();
+	size_t GetLength();
+	const void *Lock();
+};
+#endif // MODPLUG_TRACKER
+
+
+//=============
+class InputFile
+//=============
+{
+private:
+	mpt::PathString m_Filename;
+	#ifdef MPT_FILEREADER_STD_ISTREAM
+		mpt::ifstream m_File;
+	#else
+		CMappedFile m_File;
+	#endif
+public:
+	InputFile();
+	InputFile(const mpt::PathString &filename);
+	~InputFile();
+	bool Open(const mpt::PathString &filename);
+	bool IsValid() const;
+#if defined(MPT_FILEREADER_STD_ISTREAM)
+	typedef std::pair<std::istream*, const mpt::PathString*> ContentsRef;
+#else
+	struct Data
+	{
+		const char *data;
+		std::size_t size;
+	};
+	typedef std::pair<InputFile::Data, const mpt::PathString*> ContentsRef;
+#endif
+	InputFile::ContentsRef Get();
+};
 
 
 #endif // MPT_WITH_PATHSTRING

trunk/OpenMPT/mptrack/Ctrl_ins.cpp
@@ -1433,7 +1433,7 @@ BOOL CCtrlInstruments::OpenInstrument(const mpt::PathString &fileName)
 	}
 	bFirst = FALSE;
 
-	FileReader file = f.GetFile();
+	FileReader file = GetFileReader(f);
 
 	bOk = FALSE;
 	if (file.IsValid())

trunk/OpenMPT/mptrack/Ctrl_smp.cpp
@@ -786,7 +786,7 @@ bool CCtrlSamples::OpenSample(const mpt::PathString &fileName)
 		return false;
 	}
 
-	FileReader file = f.GetFile();
+	FileReader file = GetFileReader(f);
 	if(!file.IsValid())
 	{
 		EndWaitCursor();

trunk/OpenMPT/mptrack/MainFrm.cpp
@@ -1528,7 +1528,7 @@ BOOL CMainFrame::PlaySoundFile(const mpt::PathString &filename, ModCommand::NOTE
 
 			if(f.Open(filename))
 			{
-				FileReader file = f.GetFile();
+				FileReader file = GetFileReader(f);
 				if(file.IsValid())
 				{
 					InitPreview();

trunk/OpenMPT/mptrack/MemoryMappedFile.cpp
@@ -15,136 +15,6 @@
 OPENMPT_NAMESPACE_BEGIN
 
 
-CMappedFile::~CMappedFile()
-//-------------------------
-{
-	Close();
-}
-
-
-bool CMappedFile::Open(const mpt::PathString &filename)
-//-----------------------------------------------------
-{
-	m_hFile = CreateFileW(
-		filename.AsNative().c_str(),
-		GENERIC_READ,
-		FILE_SHARE_READ,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
-	if(m_hFile == INVALID_HANDLE_VALUE)
-	{
-		m_hFile = nullptr;
-		return false;
-	}
-	m_FileName = filename;
-	return true;
-}
-
-
-void CMappedFile::Close()
-//-----------------------
-{
-	m_FileName = mpt::PathString();
-	// Unlock file
-	if(m_hFMap)
-	{
-		if(m_pData)
-		{
-			UnmapViewOfFile(m_pData);
-			m_pData = nullptr;
-		}
-		CloseHandle(m_hFMap);
-		m_hFMap = nullptr;
-	} else if(m_pData)
-	{
-		free(m_pData);
-		m_pData = nullptr;
-	}
-
-	// Close file handle
-	if(m_hFile)
-	{
-		CloseHandle(m_hFile);
-		m_hFile = nullptr;
-	}
-}
-
-
-size_t CMappedFile::GetLength()
-//-----------------------------
-{
-	LARGE_INTEGER size;
-	if(GetFileSizeEx(m_hFile, &size) == FALSE)
-	{
-		return 0;
-	}
-	return mpt::saturate_cast<size_t>(size.QuadPart);
-}
-
-
-const void *CMappedFile::Lock()
-//-----------------------------
-{
-	size_t length = GetLength();
-	if(!length) return nullptr;
-
-	void *lpStream;
-
-	HANDLE hmf = CreateFileMapping(
-		m_hFile,
-		NULL,
-		PAGE_READONLY,
-		0, 0,
-		NULL);
-
-	// Try memory-mapping first
-	if(hmf)
-	{
-		lpStream = MapViewOfFile(
-			hmf,
-			FILE_MAP_READ,
-			0, 0,
-			length);
-		if(lpStream)
-		{
-			m_hFMap = hmf;
-			m_pData = lpStream;
-			return lpStream;
-		}
-		CloseHandle(hmf);
-		hmf = nullptr;
-	}
-	
-	// Fallback if memory-mapping fails for some weird reason
-	if((lpStream = malloc(length)) == nullptr) return nullptr;
-	memset(lpStream, 0, length);
-	size_t bytesToRead = length;
-	size_t bytesRead = 0;
-	while(bytesToRead > 0)
-	{
-		DWORD chunkToRead = mpt::saturate_cast<DWORD>(length);
-		DWORD chunkRead = 0;
-		if(ReadFile(m_hFile, (char*)lpStream + bytesRead, chunkToRead, &chunkRead, NULL) == FALSE)
-		{
-			// error
-			free(lpStream);
-			return nullptr;
-		}
-		bytesRead += chunkRead;
-		bytesToRead -= chunkRead;
-	}
-	m_pData = lpStream;
-	return lpStream;
-}
-
-
-FileReader CMappedFile::GetFile()
-//-------------------------------
-{
-	return FileReader(Lock(), GetLength(), &m_FileName);
-}
 
 
 OPENMPT_NAMESPACE_END

trunk/OpenMPT/mptrack/MemoryMappedFile.h
@@ -11,32 +11,8 @@
 #pragma once
 
 #include "../soundlib/FileReader.h"
+#include "../common/mptFstream.h"
 
 OPENMPT_NAMESPACE_BEGIN
-
-//////////////////////////////////////////////////////////////////
-// File Mapping Class
-
-//===============
-class CMappedFile
-//===============
-{
-protected:
-	HANDLE m_hFile;
-	HANDLE m_hFMap;
-	void *m_pData;
-	mpt::PathString m_FileName;
-
-public:
-	CMappedFile() : m_hFile(nullptr), m_hFMap(nullptr), m_pData(nullptr) { }
-	~CMappedFile();
-
-public:
-	bool Open(const mpt::PathString &filename);
-	void Close();
-	size_t GetLength();
-	const void *Lock();
-	FileReader GetFile();
-};
 
 OPENMPT_NAMESPACE_END

trunk/OpenMPT/mptrack/Moddoc.cpp
@@ -219,25 +219,16 @@ BOOL CModDoc::OnOpenDocument(const mpt::PathString &filename)
 	if(filename.empty()) return OnNewDocument();
 
 	BeginWaitCursor();
-	#if defined(MPT_FILEREADER_STD_ISTREAM)
-		mpt::ifstream f(filename, std::ios_base::binary);
-		m_SndFile.Create(FileReader(&f, &filename), CSoundFile::loadCompleteModule, this);
-	#else
-	{
-		CMappedFile f;
-		if (f.Open(filename))
-		{
-			FileReader file = f.GetFile();
-			if(file.IsValid())
-			{
-				ASSERT(GetPathNameMpt() == mpt::PathString());
-				SetPathName(filename, FALSE);	// Path is not set yet, but ITP loader needs this for relative paths.
 
-				m_SndFile.Create(file, CSoundFile::loadCompleteModule, this);
-			}
-		}
+	InputFile f(filename);
+	if(f.IsValid())
+	{
+		FileReader file = GetFileReader(f);
+		ASSERT(GetPathNameMpt() == mpt::PathString());
+		SetPathName(filename, FALSE);	// Path is not set yet, but ITP loader needs this for relative paths.
+		m_SndFile.Create(file, CSoundFile::loadCompleteModule, this);
 	}
-	#endif
+
 	EndWaitCursor();
 
 	logcapturer.ShowLog(std::wstring()
@@ -345,7 +336,7 @@ BOOL CModDoc::OnOpenDocument(const mpt::PathString &filename)
 
 					if(f.Open(pszMidiMapName))
 					{
-						FileReader file = f.GetFile();
+						FileReader file = GetFileReader(f);
 						if(file.IsValid())
 						{
 							m_SndFile.ReadInstrumentFromFile(nIns, file, false);
@@ -687,7 +678,7 @@ void CModDoc::OnAppendModule()
 	for(size_t counter = 0; counter < files.size(); counter++)
 	{
 		CMappedFile mappedFile;
-		if(mappedFile.Open(files[counter]) && source.Create(mappedFile.GetFile(), CSoundFile::loadCompleteModule))
+		if(mappedFile.Open(files[counter]) && source.Create(GetFileReader(mappedFile), CSoundFile::loadCompleteModule))
 		{
 			AppendModule(source);
 			source.Destroy();

commit 6805dfa44b98b9dc6c0c81328e9cc23cf3489cf0

[Fix] Builds without MPT_WITH_PATHSTRING broke in r4487.
git-svn-id: https://source.openmpt.org/svn/openmpt@4488 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/mptIO.cpp
@@ -396,6 +396,8 @@ const void *CMappedFile::Lock()
 
 
 
+#if defined(MPT_WITH_PATHSTRING)
+
 InputFile::InputFile()
 {
 	return;
@@ -467,6 +469,8 @@ InputFile::ContentsRef InputFile::Get()
 }
 
 #endif
+
+#endif // MPT_WITH_PATHSTRING
 
 
 