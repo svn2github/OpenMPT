svn2github/OpenMPT

commit 0f47ef3543b6c194965a2fdd97e76e2a7e2f7424

[Fix] ProTracker compatibility: Properly implement PT-style sample swapping, which is only supposed to happend at the end of the sample and not instantly. Test case: PTInstrSwap.mod
git-svn-id: https://source.openmpt.org/svn/openmpt@4223 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Fastmix.cpp
@@ -324,6 +324,7 @@ void CSoundFile::CreateStereoMix(int count)
 
 		if ((nMixPlugin > 0) && (nMixPlugin <= MAX_MIXPLUGINS))
 		{
+			// Render into plugin buffer instead of global buffer
 			SNDMIXPLUGINSTATE *pPlugin = m_MixPlugins[nMixPlugin - 1].pMixState;
 			if ((pPlugin) && (pPlugin->pMixBuffer))
 			{
@@ -373,6 +374,7 @@ void CSoundFile::CreateStereoMix(int count)
 			{
 				if (nrampsamples > chn.nRampLength) nrampsamples = chn.nRampLength;
 			}
+
 			if((nSmpCount = GetSampleCount(chn, nrampsamples, ITPingPongMode)) <= 0)
 			{
 				// Stopping the channel
@@ -388,6 +390,7 @@ void CSoundFile::CreateStereoMix(int count)
 				chn.dwFlags.reset(CHN_PINGPONGFLAG);
 				break;
 			}
+
 			// Should we mix this channel ?
 			if((nchmixed >= m_MixerSettings.m_nMaxMixChannels && realtimeMix)	// Too many channels
 				|| (!chn.nRampLength && !(chn.leftVol | chn.rightVol)))			// Channel is completely silent
@@ -463,6 +466,24 @@ void CSoundFile::CreateStereoMix(int count)
 					chn.nRampLength -= nSmpCount;
 				}
 			}
+
+			// ProTracker compatibility: Instrument changes without a note do not happen instantly, but rather when the sample loop has finished playing.
+			// Test case: PTInstrSwap.mod
+			if(m_SongFlags[SONG_PT1XMODE] && chn.nPos >= chn.nLoopEnd && chn.dwFlags[CHN_LOOP] && chn.nNewIns && chn.nNewIns <= GetNumSamples() && chn.pModSample != &Samples[chn.nNewIns])
+			{
+				const ModSample &smp = Samples[chn.nNewIns];
+				chn.pModSample = &smp;
+				chn.pCurrentSample = smp.pSample;
+				chn.dwFlags = (chn.dwFlags & CHN_CHANNELFLAGS) | smp.uFlags;
+				chn.nLoopStart = smp.nLoopStart;
+				chn.nLoopEnd = smp.nLoopEnd;
+				chn.nLength = smp.uFlags[CHN_LOOP] ? smp.nLoopEnd : smp.nLength;
+				chn.nPos = chn.nLoopStart;
+				if(!chn.pCurrentSample)
+				{
+					break;
+				}
+			}
 		} while(nsamples > 0);
 
 		// Restore sample pointer in case it got changed through loop wrap-around
@@ -520,8 +541,8 @@ void CSoundFile::ProcessPlugins(UINT nCount)
 #endif // MPT_INTMIXER
 			} else
 			{
-				memset(pState->pOutBufferL, 0, nCount * sizeof(float));
-				memset(pState->pOutBufferR, 0, nCount * sizeof(float));
+				memset(pState->pOutBufferL, 0, nCount * sizeof(pState->pOutBufferL[0]));
+				memset(pState->pOutBufferR, 0, nCount * sizeof(pState->pOutBufferR[0]));
 			}
 			pState->dwFlags &= ~SNDMIXPLUGINSTATE::psfMixReady;
 		}

trunk/OpenMPT/soundlib/Snd_fx.cpp
@@ -1176,17 +1176,13 @@ void CSoundFile::InstrumentChange(ModChannel *pChn, UINT instr, bool bPorta, boo
 	}
 	if(pChn->dwFlags[CHN_LOOP] && pChn->nLoopEnd < pChn->nLength) pChn->nLength = pChn->nLoopEnd;
 
-	// Fix sample position on instrument change. This is needed for PT1x MOD and IT "on the fly" sample change.
+	// Fix sample position on instrument change. This is needed for IT "on the fly" sample change.
 	// XXX is this actually called? In ProcessEffects(), a note-on effect is emulated if there's an on the fly sample change!
 	if(pChn->nPos >= pChn->nLength)
 	{
 		if((GetType() & (MOD_TYPE_IT | MOD_TYPE_MPT)))
 		{
 			pChn->nPos = pChn->nPosLo = 0;
-		} else if((GetType() & MOD_TYPE_MOD))	// TODO does not always seem to work, especially with short chip samples?
-		{
-			pChn->nPos = pChn->nLoopStart;
-			pChn->nPosLo = 0;
 		}
 	}
 }
@@ -2177,7 +2173,7 @@ bool CSoundFile::ProcessEffects()
 			if(retrigEnv) //Case: instrument with no note data.
 			{
 				//IT compatibility: Instrument with no note.
-				if(IsCompatibleMode(TRK_IMPULSETRACKER) || m_SongFlags[SONG_PT1XMODE])
+				if(IsCompatibleMode(TRK_IMPULSETRACKER))
 				{
 					if(GetNumInstruments())
 					{

commit 1b479b4be1b1b6ff7e8687bc09a4f427064c36d9

[Fix] When handling ProTracker-compatible sample changes at loop ends, we also have to update the sample pointers dealing with loop wraparound. (broken since r4223) (fixes https://bugs.openmpt.org/view.php?id=626, archon_-_sextyfour.mod)
git-svn-id: https://source.openmpt.org/svn/openmpt@4728 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Fastmix.cpp
@@ -271,6 +271,34 @@ static forceinline int32 GetSampleCount(ModChannel &chn, int32 nSamples, bool IT
 	return nSmpCount;
 }
 
+
+// Calculate offset of loop wrap-around buffer for this sample.
+static inline void UpdateLookaheadPointers(const int8* &samplePointer, const int8* &lookaheadPointer, SmpLength &lookaheadStart, const ModChannel &chn, const MixFuncTable::ResamplingIndex resamplingMode)
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+{
+	samplePointer = static_cast<const int8 *>(chn.pCurrentSample);
+	lookaheadStart = chn.nLoopEnd - InterpolationMaxLookahead;
+	// We only need to apply the loop wrap-around logic if the sample is actually looping and if interpolation is applied.
+	// If there is no interpolation happening, there is no lookahead happening the sample read-out is exact.
+	if(chn.dwFlags[CHN_LOOP] && resamplingMode != MixFuncTable::ndxNoInterpolation)
+	{
+		const bool loopEndsAtSampleEnd = chn.pModSample->uFlags[CHN_LOOP] && chn.pModSample->nLoopEnd == chn.pModSample->nLength;
+		const bool inSustainLoop = chn.InSustainLoop();
+
+		// Do not enable wraparound magic if we're previewing a custom loop!
+		if(inSustainLoop || chn.nLoopEnd == chn.pModSample->nLoopEnd)
+		{
+			SmpLength lookaheadOffset = (loopEndsAtSampleEnd ? 0 : (3 * InterpolationMaxLookahead)) + chn.pModSample->nLength - chn.nLoopEnd;
+			if(inSustainLoop)
+			{
+				lookaheadOffset += 4 * InterpolationMaxLookahead;
+			}
+			lookaheadPointer = samplePointer + lookaheadOffset * chn.pModSample->GetBytesPerSample();
+		}
+	}
+}
+
+
 // Render count * number of channels samples
 void CSoundFile::CreateStereoMix(int count)
 //-----------------------------------------
@@ -342,28 +370,11 @@ void CSoundFile::CreateStereoMix(int count)
 			}
 		}
 
-		// Calculate offset of loop wrap-around buffer for this sample.
-		const int8 * const samplePointer = static_cast<const int8 *>(chn.pCurrentSample);
+		const int8 * samplePointer = nullptr;
 		const int8 * lookaheadPointer = nullptr;
-		const SmpLength lookaheadStart = chn.nLoopEnd - InterpolationMaxLookahead;
-		// We only need to apply the loop wrap-around logic if the sample is actually looping and if interpolation is applied.
-		// If there is no interpolation happening, there is no lookahead happening the sample read-out is exact.
-		if(chn.dwFlags[CHN_LOOP] && resamplingMode != MixFuncTable::ndxNoInterpolation)
-		{
-			const bool loopEndsAtSampleEnd = chn.pModSample->uFlags[CHN_LOOP] && chn.pModSample->nLoopEnd == chn.pModSample->nLength;
-			const bool inSustainLoop = chn.InSustainLoop();
+		SmpLength lookaheadStart = 0;
 
-			// Do not enable wraparound magic if we're previewing a custom loop!
-			if(inSustainLoop || chn.nLoopEnd == chn.pModSample->nLoopEnd)
-			{
-				SmpLength lookaheadOffset = (loopEndsAtSampleEnd ? 0 : (3 * InterpolationMaxLookahead)) + chn.pModSample->nLength - chn.nLoopEnd;
-				if(inSustainLoop)
-				{
-					lookaheadOffset += 4 * InterpolationMaxLookahead;
-				}
-				lookaheadPointer = samplePointer + lookaheadOffset * chn.pModSample->GetBytesPerSample();
-			}
-		}
+		UpdateLookaheadPointers(samplePointer, lookaheadPointer, lookaheadStart, chn, resamplingMode);
 
 		////////////////////////////////////////////////////
 		CHANNELINDEX naddmix = 0;
@@ -482,6 +493,7 @@ void CSoundFile::CreateStereoMix(int count)
 				chn.nLoopEnd = smp.nLoopEnd;
 				chn.nLength = smp.uFlags[CHN_LOOP] ? smp.nLoopEnd : smp.nLength;
 				chn.nPos = chn.nLoopStart;
+				UpdateLookaheadPointers(samplePointer, lookaheadPointer, lookaheadStart, chn, resamplingMode);
 				if(!chn.pCurrentSample)
 				{
 					break;