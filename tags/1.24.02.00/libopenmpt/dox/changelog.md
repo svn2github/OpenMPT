515a410f45593f8fb57c14cc71359666b714565c
saga-games <saga-games@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2015-02-16 22:40:26 +0000

    Created tag for OpenMPT 1.24.02.00
    git-svn-id: https://source.openmpt.org/svn/openmpt@4762 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ tags/1.24.02.00/libopenmpt/dox/changelog.md
@@ -0,0 +1,147 @@
+
+Changelog {#changelog}
+=========
+
+For fully detailed change log, please see the source repository directly. This
+is just a high-level summary.
+
+### libopenmpt-svn
+
+ *  Makefile configuration filenames changed from
+    `build/make/Makefile.config.*` to `build/make/config-*.mk`.
+ *  libopenmpt for Android now supports unmo3 from un4seen. See
+    `build/android_ndk/README.AndroidNDK.txt` for details.
+
+ *  [Bug] Fix out-of-bounds read in mixer code for ProTracker-compatible MOD
+    files which was introduced back in r4223 / beta6.
+
+ *  Vibrato effect was too weak in beta8 and beta9 in IT linear slide mode.
+ *  Very small fine portamento was wrong.
+ *  Tiny IT playback compatibility improvements.
+
+### 2014-12-21 - libopenmpt 0.2-beta9
+
+ *  [Bug] libopenmpt_ext.hpp was missing from the Windows binary zip files.
+
+### 2014-12-21 - libopenmpt 0.2-beta8
+
+ *  foo_openmpt: Settings are now accessable via foobar2000 advanced settings.
+ *  Autotools based build now supports libunmo3. Specify --enable-unmo3.
+ *  Support for dynamic loading of libunmo3 on MacOS X.
+ *  libopenmpt now uses libltld (from libtool) for dynamic loading of libunmo3
+    on all non-Windows platforms.
+ *  Support for older compilers:
+     *  GCC 4.1.x to 4.3.x (use `make ANCIENT=1`)
+     *  Microsoft Visual Studio 2008 (with latest Service Pack)
+        (see `build/vs2008`)
+ *  libopenmpt_ext.hpp is now distributed by default. The API is still
+    considered experimental and not guaranteed to stay API or ABI compatible.
+ *  xmp-openmpt / in_openmpt: No more libopenmpt_settings.dll.
+    The settings dialog now uses a statically linked copy of MFC.
+
+ *  [Bug] The -autotools tarballs were not working at all.
+
+ *  Vastly improved MT2 loader.
+ *  Improved S3M playback compatibility.
+ *  Added openmpt::ext::interactive, an extension which adds a whole bunch of
+    new functionality to change playback in some way or another.
+ *  Added possibility to sync sample playback when using
+    openmpt::module::set_position_* by setting the ctl value seek.sync_samples=1  
+ *  Support for "hidden" subsongs has been added.
+    They are accessible through the same interface as ordinary subsongs, i.e.
+    use openmpt::module::select_subsong to switch between any kind of subsongs.
+ *  All subsongs can now be played consecutively by passing -1 as the subsong
+    index in openmpt::module::select_subsong.
+ *  Added documentation for a couple of more functions.
+
+### 2014-09-07 - libopenmpt 0.2-beta7
+
+ *  libopenmpt now has an GNU Autotools based build system (in addition to all
+    previously supported ways of building libopenmpt). Autotools support is
+    packaged separately as tarballs ending in `-autotools.tar.gz`.
+
+ *  [Bug] The distributed windows .zip file did not include pugixml.
+
+ *  [Regression] openmpt123: Support for writing WavPack (.wv) files has been
+    removed.
+    
+    Reasoning:
+     1. WavPack support was incomplete and did not include support for writing
+        WavPack metadata at all.
+     2. openmpt123 already supports libSndFile which can be used to write
+        uncompressed lossless WAV files which can then be encoded to whatever
+        format the user desires with other tools.
+
+### 2014-09-06 - libopenmpt 0.2-beta6
+
+ *  openmpt123: SDL is now also used by default if availble, in addition to
+    PortAudio.
+ *  Support for emscripten is no longer experimental.
+ *  libopenmpt itself can now also be compiled with VS2008.
+
+ *  [Bug] Fix all known crashes on platforms that do not support unaligned
+    memory access.
+ *  [Bug] openmpt123: Effect column was always missing in pattern display.
+
+### 2014-06-15 - libopenmpt 0.2-beta5
+
+ *  Add unmo3 support for non-Windows builds.
+ *  Namespace all internal functions in order to allow statically linking
+    against libopenmpt without risking duplicate symbols.
+ *  Iconv is now completely optional and only used on Linux systems by default.
+ *  Added libopenmpt_example_c_stdout.c, an example without requiring
+    PortAudio.
+ *  Add experimental support for building libopenmpt with emscripten.
+
+ *  [Bug] Fix ping-pong loop behaviour which broke in 0.2-beta3.
+ *  [Bug] Fix crashs when accessing invalid patterns through libopenmpt API.
+ *  [Bug] Makefile: Support building with missing optional dependencies without
+    them being stated explicitely.
+ *  [Bug] openmpt123: Crash when quitting while playback is stopped.
+ *  [Bug] openmpt123: Crash when writing output to a file in interactive UI
+    mode.
+ *  [Bug] openmpt123: Wrong FLAC output filename in --render mode.
+
+ *  Various smaller playback accuracy improvements.
+
+### 2014-02-25 - libopenmpt 0.2-beta4
+
+ *  [Bug] Makefile: Dependency tracking for the test suite did not work.
+
+### 2014-02-21 - libopenmpt 0.2-beta3
+
+ *  [Change] The test suite is now built by default with Makefile based builds.
+    Use `TEST=0` to skip building the tests. `make check` runs the test suite.
+
+ *  [Bug] Crash in MOD and XM loaders on architectures not supporting unaligned
+    memory access.
+ *  [Bug] MMCMP, PP20 and XPK unpackers should now work on non-x86 hardware and
+    implement proper bounds checking.
+ *  [Bug] openmpt_module_get_num_samples() returned the wrong value.
+ *  [Bug] in_openmpt: DSP plugins did not work properly.
+ *  [Bug] in_openmpt/xmp-openmpt: Setting name for stereo separation was
+    mis-spelled. This version will revert your stereo separation settings to
+    default.
+ *  [Bug] Crash when loading some corrupted modules with stereo samples.
+
+ *  Support building on Android NDK.
+ *  Avoid clicks in sample loops when using interpolation.
+ *  IT filters are now done in integer instead of floating point. This improves
+    performances, especially on architectures with no or a slow FPU.
+ *  MOD pattern break handling fixes.
+ *  Various XM playback improvements.
+ *  Improved and switchable dithering when using 16bit integer API.
+
+### 2014-01-12 - libopenmpt 0.2-beta2
+
+ *  [Bug] MT2 loader crash.
+ *  [Bug] Saving settings in in_openmpt and xmp-openmpt did not work.
+ *  [Bug] Load libopenmpt_settings.dll also from below Plugins directory in
+    Winamp.
+
+ *  DBM playback improvements.
+
+### 2013-12-31 - libopenmpt 0.2-beta1
+
+ *  First release.
+
---
+++ tags/1.24.02.00/sounddev/SoundDeviceUtilities.cpp
@@ -0,0 +1,467 @@
+/*
+ * SoundDeviceUtilities.cpp
+ * ------------------------
+ * Purpose: Sound device utilities.
+ * Notes  : (currently none)
+ * Authors: Olivier Lapicque
+ *          OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
+
+
+#include "stdafx.h"
+
+#include "SoundDevice.h"
+#include "SoundDeviceUtilities.h"
+
+#include "../common/misc_util.h"
+
+#include <algorithm>
+
+#include <mmsystem.h>
+
+
+OPENMPT_NAMESPACE_BEGIN
+
+
+namespace SoundDevice {
+
+	
+bool FillWaveFormatExtensible(WAVEFORMATEXTENSIBLE &WaveFormat, const SoundDevice::Settings &m_Settings)
+//------------------------------------------------------------------------------------------------------
+{
+	MemsetZero(WaveFormat);
+	if(!m_Settings.sampleFormat.IsValid()) return false;
+	WaveFormat.Format.wFormatTag = m_Settings.sampleFormat.IsFloat() ? WAVE_FORMAT_IEEE_FLOAT : WAVE_FORMAT_PCM;
+	WaveFormat.Format.nChannels = (WORD)m_Settings.Channels;
+	WaveFormat.Format.nSamplesPerSec = m_Settings.Samplerate;
+	WaveFormat.Format.nAvgBytesPerSec = (DWORD)m_Settings.GetBytesPerSecond();
+	WaveFormat.Format.nBlockAlign = (WORD)m_Settings.GetBytesPerFrame();
+	WaveFormat.Format.wBitsPerSample = (WORD)m_Settings.sampleFormat.GetBitsPerSample();
+	WaveFormat.Format.cbSize = 0;
+	if((WaveFormat.Format.wBitsPerSample > 16 && m_Settings.sampleFormat.IsInt()) || (WaveFormat.Format.nChannels > 2))
+	{
+		WaveFormat.Format.wFormatTag = WAVE_FORMAT_EXTENSIBLE;
+		WaveFormat.Format.cbSize = sizeof(WAVEFORMATEXTENSIBLE) - sizeof(WAVEFORMATEX);
+		WaveFormat.Samples.wValidBitsPerSample = WaveFormat.Format.wBitsPerSample;
+		switch(WaveFormat.Format.nChannels)
+		{
+		case 1:  WaveFormat.dwChannelMask = SPEAKER_FRONT_CENTER; break;
+		case 2:  WaveFormat.dwChannelMask = SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT; break;
+		case 3:  WaveFormat.dwChannelMask = SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_BACK_CENTER; break;
+		case 4:  WaveFormat.dwChannelMask = SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT; break;
+		default: WaveFormat.dwChannelMask = 0; return false; break;
+		}
+		const GUID guid_MEDIASUBTYPE_PCM = {0x00000001, 0x0000, 0x0010, 0x80, 0x00, 0x0, 0xAA, 0x0, 0x38, 0x9B, 0x71};
+		const GUID guid_MEDIASUBTYPE_IEEE_FLOAT = {0x00000003, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71};
+		WaveFormat.SubFormat = m_Settings.sampleFormat.IsFloat() ? guid_MEDIASUBTYPE_IEEE_FLOAT : guid_MEDIASUBTYPE_PCM;
+	}
+	return true;
+}
+
+
+CAudioThread::CAudioThread(CSoundDeviceWithThread &SoundDevice) : m_SoundDevice(SoundDevice)
+//------------------------------------------------------------------------------------------
+{
+	MPT_TRACE();
+	m_WakeupInterval = 0.0;
+	m_hPlayThread = NULL;
+	m_dwPlayThreadId = 0;
+	m_hAudioWakeUp = NULL;
+	m_hAudioThreadTerminateRequest = NULL;
+	m_hAudioThreadGoneIdle = NULL;
+	m_hHardwareWakeupEvent = INVALID_HANDLE_VALUE;
+	m_AudioThreadActive = 0;
+	m_hAudioWakeUp = CreateEvent(NULL, FALSE, FALSE, NULL);
+	m_hAudioThreadTerminateRequest = CreateEvent(NULL, FALSE, FALSE, NULL);
+	m_hAudioThreadGoneIdle = CreateEvent(NULL, TRUE, FALSE, NULL);
+	m_hPlayThread = CreateThread(NULL, 0, AudioThreadWrapper, (LPVOID)this, 0, &m_dwPlayThreadId);
+}
+
+
+CAudioThread::~CAudioThread()
+//---------------------------
+{
+	MPT_TRACE();
+	if(m_hPlayThread != NULL)
+	{
+		SetEvent(m_hAudioThreadTerminateRequest);
+		WaitForSingleObject(m_hPlayThread, INFINITE);
+		m_dwPlayThreadId = 0;
+		m_hPlayThread = NULL;
+	}
+	if(m_hAudioThreadTerminateRequest)
+	{
+		CloseHandle(m_hAudioThreadTerminateRequest);
+		m_hAudioThreadTerminateRequest = 0;
+	}
+	if(m_hAudioThreadGoneIdle != NULL)
+	{
+		CloseHandle(m_hAudioThreadGoneIdle);
+		m_hAudioThreadGoneIdle = 0;
+	}
+	if(m_hAudioWakeUp != NULL)
+	{
+		CloseHandle(m_hAudioWakeUp);
+		m_hAudioWakeUp = NULL;
+	}
+}
+
+
+CPriorityBooster::CPriorityBooster(bool boostPriority)
+//----------------------------------------------------
+	: m_HasVista(false)
+	, pAvSetMmThreadCharacteristics(nullptr)
+	, pAvRevertMmThreadCharacteristics(nullptr)
+	, m_BoostPriority(boostPriority)
+	, task_idx(0)
+	, hTask(NULL)
+{
+
+	MPT_TRACE();
+
+	m_HasVista = mpt::Windows::Version::IsAtLeast(mpt::Windows::Version::WinVista);
+
+	if(m_HasVista)
+	{
+		m_AvRtDLL = mpt::Library(mpt::LibraryPath::System(MPT_PATHSTRING("avrt")));
+		if(m_AvRtDLL.IsValid())
+		{
+			if(!m_AvRtDLL.Bind(pAvSetMmThreadCharacteristics, "AvSetMmThreadCharacteristicsA"))
+			{
+				m_HasVista = false;
+			}
+			if(!m_AvRtDLL.Bind(pAvRevertMmThreadCharacteristics, "AvRevertMmThreadCharacteristics"))
+			{
+				m_HasVista = false;
+			}
+		} else
+		{
+			m_HasVista = false;
+		}
+	}
+
+	#ifdef _DEBUG
+		m_BoostPriority = false;
+	#endif
+
+	if(m_BoostPriority)
+	{
+		if(m_HasVista)
+		{
+			hTask = pAvSetMmThreadCharacteristics(TEXT("Pro Audio"), &task_idx);
+		} else
+		{
+			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+		}
+	}
+
+}
+
+
+CPriorityBooster::~CPriorityBooster()
+//-----------------------------------
+{
+
+	MPT_TRACE();
+
+	if(m_BoostPriority)
+	{
+		if(m_HasVista)
+		{
+			pAvRevertMmThreadCharacteristics(hTask);
+			hTask = NULL;
+			task_idx = 0;
+		} else
+		{
+			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_NORMAL);
+		}
+	}
+
+	pAvRevertMmThreadCharacteristics = nullptr;
+	pAvSetMmThreadCharacteristics = nullptr;
+
+}
+
+
+class CPeriodicWaker
+{
+private:
+	CAudioThread &self;
+
+	double sleepSeconds;
+	long sleepMilliseconds;
+	int64 sleep100Nanoseconds;
+
+	Util::MultimediaClock clock_noxp;
+
+	bool period_nont_set;
+	bool periodic_nt_timer;
+
+	HANDLE sleepEvent;
+
+public:
+
+	CPeriodicWaker(CAudioThread &self_, double sleepSeconds_) : self(self_), sleepSeconds(sleepSeconds_)
+	//--------------------------------------------------------------------------------------------------
+	{
+
+		MPT_TRACE();
+
+		sleepMilliseconds = static_cast<long>(sleepSeconds * 1000.0);
+		sleep100Nanoseconds = static_cast<int64>(sleepSeconds * 10000000.0);
+		if(sleepMilliseconds < 1) sleepMilliseconds = 1;
+		if(sleep100Nanoseconds < 1) sleep100Nanoseconds = 1;
+
+		period_nont_set = false;
+		periodic_nt_timer = (sleep100Nanoseconds >= 10000); // can be represented as a millisecond period, otherwise use non-periodic timers which allow higher precision but might me slower because we have to set them again in each period
+
+		sleepEvent = NULL;
+
+		if(mpt::Windows::Version::IsNT())
+		{
+			if(periodic_nt_timer)
+			{
+				sleepEvent = CreateWaitableTimer(NULL, FALSE, NULL);
+				LARGE_INTEGER dueTime;
+				dueTime.QuadPart = 0 - sleep100Nanoseconds; // negative time means relative
+				SetWaitableTimer(sleepEvent, &dueTime, sleepMilliseconds, NULL, NULL, FALSE);
+			} else
+			{
+				sleepEvent = CreateWaitableTimer(NULL, TRUE, NULL);
+			}
+		} else
+		{
+			sleepEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+			clock_noxp.SetResolution(1); // increase resolution of multimedia timer
+			period_nont_set = true;
+		}
+
+	}
+
+	long GetSleepMilliseconds() const
+	//-------------------------------
+	{
+		return sleepMilliseconds;
+	}
+
+	HANDLE GetWakeupEvent() const
+	//---------------------------
+	{
+		return sleepEvent;
+	}
+
+	void Retrigger()
+	//--------------
+	{
+		MPT_TRACE();
+		if(mpt::Windows::Version::IsNT())
+		{
+			if(!periodic_nt_timer)
+			{
+				LARGE_INTEGER dueTime;
+				dueTime.QuadPart = 0 - sleep100Nanoseconds; // negative time means relative
+				SetWaitableTimer(sleepEvent, &dueTime, 0, NULL, NULL, FALSE);
+			}
+		} else
+		{
+			timeSetEvent(sleepMilliseconds, 1, (LPTIMECALLBACK)sleepEvent, NULL, TIME_ONESHOT | TIME_CALLBACK_EVENT_SET);
+		}
+	}
+
+	CPeriodicWaker::~CPeriodicWaker()
+	//-------------------------------
+	{
+		MPT_TRACE();
+		if(mpt::Windows::Version::IsNT())
+		{
+			if(periodic_nt_timer)
+			{
+				CancelWaitableTimer(sleepEvent);
+			}
+			CloseHandle(sleepEvent);
+			sleepEvent = NULL;
+		} else
+		{
+			if(period_nont_set)
+			{
+				clock_noxp.SetResolution(0);
+				period_nont_set = false;
+			}
+			CloseHandle(sleepEvent);
+			sleepEvent = NULL;
+		}
+	}
+
+};
+
+
+DWORD WINAPI CAudioThread::AudioThreadWrapper(LPVOID user)
+{
+	return ((CAudioThread*)user)->AudioThread();
+}
+DWORD CAudioThread::AudioThread()
+//-------------------------------
+{
+	MPT_TRACE();
+
+	bool terminate = false;
+	while(!terminate)
+	{
+
+		bool idle = true;
+		while(!terminate && idle)
+		{
+			HANDLE waithandles[2] = {m_hAudioThreadTerminateRequest, m_hAudioWakeUp};
+			SetEvent(m_hAudioThreadGoneIdle);
+			switch(WaitForMultipleObjects(2, waithandles, FALSE, INFINITE))
+			{
+			case WAIT_OBJECT_0:
+				terminate = true;
+				break;
+			case WAIT_OBJECT_0+1:
+				idle = false;
+				break;
+			}
+		}
+
+		if(!terminate)
+		{
+
+			CPriorityBooster priorityBooster(m_SoundDevice.m_Settings.BoostThreadPriority);
+			CPeriodicWaker periodicWaker(*this, m_WakeupInterval);
+
+			m_SoundDevice.StartFromSoundThread();
+
+			while(!terminate && IsActive())
+			{
+
+				m_SoundDevice.FillAudioBufferLocked();
+
+				periodicWaker.Retrigger();
+
+				if(m_hHardwareWakeupEvent != INVALID_HANDLE_VALUE)
+				{
+					HANDLE waithandles[4] = {m_hAudioThreadTerminateRequest, m_hAudioWakeUp, m_hHardwareWakeupEvent, periodicWaker.GetWakeupEvent()};
+					switch(WaitForMultipleObjects(4, waithandles, FALSE, periodicWaker.GetSleepMilliseconds()))
+					{
+					case WAIT_OBJECT_0:
+						terminate = true;
+						break;
+					}
+				} else
+				{
+					HANDLE waithandles[3] = {m_hAudioThreadTerminateRequest, m_hAudioWakeUp, periodicWaker.GetWakeupEvent()};
+					switch(WaitForMultipleObjects(3, waithandles, FALSE, periodicWaker.GetSleepMilliseconds()))
+					{
+					case WAIT_OBJECT_0:
+						terminate = true;
+						break;
+					}
+				}
+
+			}
+
+			m_SoundDevice.StopFromSoundThread();
+
+		}
+
+	}
+
+	SetEvent(m_hAudioThreadGoneIdle);
+
+	return 0;
+
+}
+
+
+void CAudioThread::SetWakeupEvent(HANDLE ev)
+//------------------------------------------
+{
+	MPT_TRACE();
+	m_hHardwareWakeupEvent = ev;
+}
+
+
+void CAudioThread::SetWakeupInterval(double seconds)
+//--------------------------------------------------
+{
+	MPT_TRACE();
+	m_WakeupInterval = seconds;
+}
+
+
+void CAudioThread::Activate()
+//---------------------------
+{
+	MPT_TRACE();
+	if(InterlockedExchangeAdd(&m_AudioThreadActive, 0))
+	{
+		MPT_ASSERT_ALWAYS(false);
+		return;
+	}
+	ResetEvent(m_hAudioThreadGoneIdle);
+	InterlockedExchange(&m_AudioThreadActive, 1);
+	SetEvent(m_hAudioWakeUp);
+}
+
+
+void CAudioThread::Deactivate()
+//-----------------------------
+{
+	MPT_TRACE();
+	if(!InterlockedExchangeAdd(&m_AudioThreadActive, 0))
+	{
+		MPT_ASSERT_ALWAYS(false);
+		return;
+	}
+	InterlockedExchange(&m_AudioThreadActive, 0);
+	WaitForSingleObject(m_hAudioThreadGoneIdle, INFINITE);
+}
+
+
+void CSoundDeviceWithThread::FillAudioBufferLocked()
+//--------------------------------------------------
+{
+	MPT_TRACE();
+	SourceFillAudioBufferLocked();
+}
+
+
+void CSoundDeviceWithThread::SetWakeupEvent(HANDLE ev)
+//----------------------------------------------------
+{
+	MPT_TRACE();
+	m_AudioThread.SetWakeupEvent(ev);
+}
+
+
+void CSoundDeviceWithThread::SetWakeupInterval(double seconds)
+//------------------------------------------------------------
+{
+	MPT_TRACE();
+	m_AudioThread.SetWakeupInterval(seconds);
+}
+
+
+bool CSoundDeviceWithThread::InternalStart()
+//------------------------------------------
+{
+	MPT_TRACE();
+	m_AudioThread.Activate();
+	return true;
+}
+
+
+void CSoundDeviceWithThread::InternalStop()
+//-----------------------------------------
+{
+	MPT_TRACE();
+	m_AudioThread.Deactivate();
+}
+
+
+} // namespace SoundDevice
+
+
+OPENMPT_NAMESPACE_END
