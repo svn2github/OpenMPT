svn2github/OpenMPT

commit 38766303ae6b15c392d1990047bb79728675f498

[Imp] mptIO: Add proper FileReader wrapper support for seekable std::istream.
git-svn-id: https://source.openmpt.org/svn/openmpt@4879 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/mptIO.cpp
@@ -117,6 +117,115 @@ bool Flush(FILE* & f) { return fflush(f) == 0; }
 
 #if defined(MPT_FILEREADER_STD_ISTREAM)
 
+FileDataContainerStdStreamSeekable::FileDataContainerStdStreamSeekable(std::istream *s)
+	: stream(s)
+	, streamLength(0)
+	, cached(false)
+{
+	CacheLength();
+}
+
+FileDataContainerStdStreamSeekable::~FileDataContainerStdStreamSeekable()
+{
+	return;
+}
+
+bool FileDataContainerStdStreamSeekable::IsSeekable(std::istream *stream)
+{
+	stream->clear();
+	std::streampos oldpos = stream->tellg();
+	if(stream->fail() || oldpos == std::streampos(-1))
+	{
+		stream->clear();
+		return false;
+	}
+	stream->seekg(0, std::ios::beg);
+	if(stream->fail())
+	{
+		stream->clear();
+		stream->seekg(oldpos);
+		stream->clear();
+		return false;
+	}
+	stream->seekg(0, std::ios::end);
+	if(stream->fail())
+	{
+		stream->clear();
+		stream->seekg(oldpos);
+		stream->clear();
+		return false;
+	}
+	std::streampos length = stream->tellg();
+	if(stream->fail() || length == std::streampos(-1))
+	{
+		stream->clear();
+		stream->seekg(oldpos);
+		stream->clear();
+		return false;
+	}
+	stream->seekg(oldpos);
+	stream->clear();
+	return true;
+}
+
+void FileDataContainerStdStreamSeekable::CacheLength()
+{
+	stream->clear();
+	std::streampos oldpos = stream->tellg();
+	stream->seekg(0, std::ios::end);
+	std::streampos length = stream->tellg();
+	stream->seekg(oldpos);
+	streamLength = mpt::saturate_cast<IFileDataContainer::off_t>(static_cast<int64>(length));
+}
+
+void FileDataContainerStdStreamSeekable::CacheStream() const
+{
+	if(cached)
+	{
+		return;
+	}
+	cache.resize(streamLength);
+	stream->seekg(0);
+	stream->read(&cache[0], cache.size());
+	cached = true;
+}
+
+bool FileDataContainerStdStreamSeekable::IsValid() const
+{
+	return true;
+}
+
+const char *FileDataContainerStdStreamSeekable::GetRawData() const
+{
+	CacheStream();
+	return &cache[0];
+}
+
+IFileDataContainer::off_t FileDataContainerStdStreamSeekable::GetLength() const
+{
+	return streamLength;
+}
+
+IFileDataContainer::off_t FileDataContainerStdStreamSeekable::Read(char *dst, IFileDataContainer::off_t pos, IFileDataContainer::off_t count) const
+{
+	if(cached)
+	{
+		IFileDataContainer::off_t cache_avail = std::min<IFileDataContainer::off_t>(IFileDataContainer::off_t(cache.size()) - pos, count);
+		std::copy(cache.begin() + pos, cache.begin() + pos + cache_avail, dst);
+		return cache_avail;
+	} else
+	{
+		std::streampos currentpos = stream->tellg();
+		if(currentpos == std::streampos(-1) || static_cast<int64>(pos) != currentpos)
+		{ // inefficient istream implementations might invalidate their buffer when seeking, even when seeking to the current position
+			stream->seekg(pos);
+		}
+		stream->read(dst, count);
+		return static_cast<IFileDataContainer::off_t>(stream->gcount());
+	}
+}
+
+
 FileDataContainerStdStream::FileDataContainerStdStream(std::istream *s)
 	: cachesize(0), streamFullyCached(false), stream(s)
 {
@@ -204,34 +313,8 @@ const char *FileDataContainerStdStream::GetRawData() const
 
 IFileDataContainer::off_t FileDataContainerStdStream::GetLength() const
 {
-	if(streamFullyCached)
-	{
-		return cachesize;
-	} else
-	{
-		stream->clear();
-		std::streampos oldpos = stream->tellg();
-		if(!stream->fail() && oldpos != std::streampos(-1))
-		{
-			stream->seekg(0, std::ios::end);
-			if(!stream->fail())
-			{
-				std::streampos length = stream->tellg();
-				if(!stream->fail() && length != std::streampos(-1))
-				{
-					stream->seekg(oldpos);
-					stream->clear();
-					return static_cast<IFileDataContainer::off_t>(length);
-				}
-			}
-			stream->clear();
-			stream->seekg(oldpos);
-		}
-		// not seekable
-		stream->clear();
-		CacheStream();
-		return cachesize;
-	}
+	CacheStream();
+	return cachesize;
 }
 
 IFileDataContainer::off_t FileDataContainerStdStream::Read(char *dst, IFileDataContainer::off_t pos, IFileDataContainer::off_t count) const

trunk/OpenMPT/common/BuildSettings.h
@@ -97,6 +97,9 @@
 // Enable std::istream support in class FileReader (this is generally not needed for the tracker, local files can easily be mmapped as they have been before introducing std::istream support)
 //#define MPT_FILEREADER_STD_ISTREAM
 
+// Enable performance optimizations for seekable std::istream. Note that these probably will only payoff once FileReader::GetRawData() usage gets further reduced.
+//#define MPT_FILEREADER_STD_ISTREAM_SEEKABLE
+
 // Support for externally linked samples e.g. in MPTM files
 #define MPT_EXTERNAL_SAMPLES
 
@@ -171,6 +174,7 @@
 #endif
 //#define NO_LOGGING
 #define MPT_FILEREADER_STD_ISTREAM
+#define MPT_FILEREADER_STD_ISTREAM_SEEKABLE
 //#define MPT_EXTERNAL_SAMPLES
 #define NO_ARCHIVE_SUPPORT
 #define NO_REVERB

trunk/OpenMPT/common/FileReader.h
@@ -82,9 +82,35 @@ public:
 #endif // MPT_WITH_FILEIO
 
 	// Initialize file reader object with a std::istream.
-	FileReader(std::istream *s) : data(mpt::make_shared<FileDataContainerStdStream>(s)), streamPos(0) MPT_FILEREADER_INIT_FILENAME { }
+	FileReader(std::istream *s)
+		: data(
+#if defined(MPT_FILEREADER_STD_ISTREAM_SEEKABLE)
+				FileDataContainerStdStreamSeekable::IsSeekable(s) ?
+					MPT_STATIC_POINTER_CAST<IFileDataContainer>(mpt::make_shared<FileDataContainerStdStreamSeekable>(s))
+				:
+#endif // MPT_FILEREADER_STD_ISTREAM_SEEKABLE
+					MPT_STATIC_POINTER_CAST<IFileDataContainer>(mpt::make_shared<FileDataContainerStdStream>(s))
+			)
+		, streamPos(0)
+		MPT_FILEREADER_INIT_FILENAME
+	{
+		return;
+	}
 #if defined(MPT_WITH_FILEIO)
-	FileReader(std::istream *s, const mpt::PathString *filename) : data(mpt::make_shared<FileDataContainerStdStream>(s)), streamPos(0), fileName(filename) { }
+	FileReader(std::istream *s, const mpt::PathString *filename)
+		: data(
+#if defined(MPT_FILEREADER_STD_ISTREAM_SEEKABLE)
+				FileDataContainerStdStreamSeekable::IsSeekable(s) ?
+					MPT_STATIC_POINTER_CAST<IFileDataContainer>(mpt::make_shared<FileDataContainerStdStreamSeekable>(s))
+				:
+#endif // MPT_FILEREADER_STD_ISTREAM_SEEKABLE
+					MPT_STATIC_POINTER_CAST<IFileDataContainer>(mpt::make_shared<FileDataContainerStdStream>(s))
+			)
+		, streamPos(0)
+		, fileName(filename)
+	{
+		return;
+	}
 #endif // MPT_WITH_FILEIO
 
 	// Initialize file reader object based on an existing file reader object window.

trunk/OpenMPT/common/mptIO.h
@@ -534,6 +534,40 @@ public:
 };
 
 
+class FileDataContainerStdStreamSeekable : public IFileDataContainer {
+
+private:
+
+	std::istream *stream;
+
+	off_t streamLength;
+
+	mutable bool cached;
+	mutable std::vector<char> cache;
+
+public:
+
+	FileDataContainerStdStreamSeekable(std::istream *s);
+	virtual ~FileDataContainerStdStreamSeekable();
+
+	static bool IsSeekable(std::istream *stream);
+	static off_t GetLength(std::istream *stream);
+
+private:
+	
+	void CacheLength();
+	void CacheStream() const;
+
+public:
+
+	bool IsValid() const;
+	const char *GetRawData() const;
+	off_t GetLength() const;
+	off_t Read(char *dst, off_t pos, off_t count) const;
+
+};
+
+
 class FileDataContainerStdStream : public IFileDataContainer {
 
 private:

commit 091194f1e23af00a0860a1abcc4546ef0f6fbd38

[Fix] mptIO: std::istream::tellg() used by FileDataContainerStdStreamSeekable requres failbit and eofbit to be cleared. (broken since r4879)
git-svn-id: https://source.openmpt.org/svn/openmpt@4888 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/mptIO.cpp
@@ -215,6 +215,7 @@ IFileDataContainer::off_t FileDataContainerStdStreamSeekable::Read(char *dst, IF
 		return cache_avail;
 	} else
 	{
+		stream->clear(); // tellg needs eof and fail bits unset
 		std::streampos currentpos = stream->tellg();
 		if(currentpos == std::streampos(-1) || static_cast<int64>(pos) != currentpos)
 		{ // inefficient istream implementations might invalidate their buffer when seeking, even when seeking to the current position