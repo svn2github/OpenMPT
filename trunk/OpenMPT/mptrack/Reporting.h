svn2github/OpenMPT

commit e3fe8b3f9037b211cfca8bdf7e1f22fe6cd3f04e

[Ref] Add AnyString (and related) types. The AnyString types are meant to be used as function argument types only, and only during the transition phase to all-unicode strings in the whole codebase. Using an AnyString type as a function argument avoids the need to overload the function for all the different string types that we currently have (in particular, when taking multiple strings, this avoids the need to overload all possible permutations). [Ref] Add mpt::IsUTF8 heuristic function. [Ref] Convert logging to AnyString. [Ref] Convert class Reporting to AnyString. [Fix] mpt::ToUnicode(std::wstring) should depend on MPT_WSTRING_CONVERT. [Ref] Provide const wchar_t * overloads for string conversion functions even in non-MFC builds, because we handle nullptr there which would otherwise just crash in the std::wstring constructor.
git-svn-id: https://source.openmpt.org/svn/openmpt@4560 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Reporting.h
@@ -34,52 +34,34 @@ class Reporting
 //=============
 {
 
-protected:
-
-	static UINT ShowNotification(const char *text, const char *caption, UINT flags, const CWnd *parent);
-	static UINT ShowNotification(const std::wstring &text, const std::wstring &caption, UINT flags, const CWnd *parent);
-
 public:
 
 	// TODO Quick'n'dirty workaround for MsgBox(). Shouldn't be public.
-	static UINT CustomNotification(const char *text, const char *caption, UINT flags, const CWnd *parent) { return ShowNotification(text, caption, flags, parent); };
-	static UINT CustomNotification(const std::wstring &text, const std::wstring &caption, UINT flags, const CWnd *parent) { return ShowNotification(text, caption, flags, parent); };
+	static UINT CustomNotification(const AnyStringLocale &text, const AnyStringLocale &caption, UINT flags, const CWnd *parent);
 
 	// Show a simple notification
-	static void Notification(const char *text, const CWnd *parent = nullptr);
-	static void Notification(const char *text, const char *caption, const CWnd *parent = nullptr);
-	static void Notification(const std::wstring &text, const CWnd *parent = nullptr);
-	static void Notification(const std::wstring &text, const std::wstring &caption, const CWnd *parent = nullptr);
+	static void Notification(const AnyStringLocale &text, const CWnd *parent = nullptr);
+	static void Notification(const AnyStringLocale &text, const AnyStringLocale &caption, const CWnd *parent = nullptr);
 
 	// Show a simple information
-	static void Information(const char *text, const CWnd *parent = nullptr);
-	static void Information(const char *text, const char *caption, const CWnd *parent = nullptr);
-	static void Information(const std::wstring &text, const CWnd *parent = nullptr);
-	static void Information(const std::wstring &text, const std::wstring &caption, const CWnd *parent = nullptr);
+	static void Information(const AnyStringLocale &text, const CWnd *parent = nullptr);
+	static void Information(const AnyStringLocale &text, const AnyStringLocale &caption, const CWnd *parent = nullptr);
 
 	// Show a simple warning
-	static void Warning(const char *text, const CWnd *parent = nullptr);
-	static void Warning(const char *text, const char *caption, const CWnd *parent = nullptr);
-	static void Warning(const std::wstring &text, const CWnd *parent = nullptr);
-	static void Warning(const std::wstring &text, const std::wstring &caption, const CWnd *parent = nullptr);
+	static void Warning(const AnyStringLocale &text, const CWnd *parent = nullptr);
+	static void Warning(const AnyStringLocale &text, const AnyStringLocale &caption, const CWnd *parent = nullptr);
 
 	// Show an error box.
-	static void Error(const char *text, const CWnd *parent = nullptr);
-	static void Error(const char *text, const char *caption, const CWnd *parent = nullptr);
-	static void Error(const std::wstring &text, const CWnd *parent = nullptr);
-	static void Error(const std::wstring &text, const std::wstring &caption, const CWnd *parent = nullptr);
+	static void Error(const AnyStringLocale &text, const CWnd *parent = nullptr);
+	static void Error(const AnyStringLocale &text, const AnyStringLocale &caption, const CWnd *parent = nullptr);
 
 	// Show a confirmation dialog.
-	static ConfirmAnswer Confirm(const char *text, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
-	static ConfirmAnswer Confirm(const char *text, const char *caption, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
-	static ConfirmAnswer Confirm(const std::wstring &text, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
-	static ConfirmAnswer Confirm(const std::wstring &text, const std::wstring &caption, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
+	static ConfirmAnswer Confirm(const AnyStringLocale &text, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
+	static ConfirmAnswer Confirm(const AnyStringLocale &text, const AnyStringLocale &caption, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
 
 	// Show a confirmation dialog.
-	static RetryAnswer RetryCancel(const char *text, const CWnd *parent = nullptr);
-	static RetryAnswer RetryCancel(const char *text, const char *caption, const CWnd *parent = nullptr);
-	static RetryAnswer RetryCancel(const std::wstring &text, const CWnd *parent = nullptr);
-	static RetryAnswer RetryCancel(const std::wstring &text, const std::wstring &caption, const CWnd *parent = nullptr);
+	static RetryAnswer RetryCancel(const AnyStringLocale &text, const CWnd *parent = nullptr);
+	static RetryAnswer RetryCancel(const AnyStringLocale &text, const AnyStringLocale &caption, const CWnd *parent = nullptr);
 
 };
 

trunk/OpenMPT/common/Logging.cpp
@@ -163,8 +163,8 @@ void Logger::operator () (LogLevel level, const mpt::ustring &text)
 	DoLog(context, LogLevelToString(level) + MPT_USTRING(": ") + text);
 }
 
-void Logger::operator () (const mpt::ustring &text)
-//-------------------------------------------------
+void Logger::operator () (const AnyStringLocale &text)
+//----------------------------------------------------
 {
 	DoLog(context, text);
 }
@@ -177,24 +177,6 @@ void Logger::operator () (const char *format, ...)
 	DoLog(context, format, va);
 	va_end(va);
 }
-
-void Logger::operator () (const std::string &text)
-//------------------------------------------------
-{
-#if defined(MPT_WITH_CHARSET_LOCALE)
-	DoLog(context, mpt::ToUnicode(mpt::CharsetLocale, text));
-#else
-	DoLog(context, mpt::ToUnicode(mpt::CharsetUTF8, text));
-#endif
-}
-
-#if MPT_WSTRING_CONVERT && !(MPT_USTRING_MODE_WIDE)
-void Logger::operator () (const std::wstring &text)
-//-------------------------------------------------
-{
-	DoLog(context, mpt::ToUnicode(text));
-}
-#endif
 
 
 #endif // !NO_LOGGING

trunk/OpenMPT/common/Logging.h
@@ -84,11 +84,7 @@ public:
 	Logger(const Context &context) : context(context) {}
 	void MPT_PRINTF_FUNC(2,3) operator () (const char *format, ...);
 	void operator () (LogLevel level, const mpt::ustring &text);
-	void operator () (const mpt::ustring &text);
-	void operator () (const std::string &text);
-#if MPT_WSTRING_CONVERT && !(MPT_USTRING_MODE_WIDE)
-	void operator () (const std::wstring &text);
-#endif
+	void operator () (const AnyStringLocale &text);
 };
 
 #define Log mpt::log::Logger(MPT_LOG_CURRENTCONTEXT())
@@ -100,11 +96,7 @@ class Logger
 public:
 	inline void MPT_PRINTF_FUNC(2,3) operator () (const char * /*format*/, ...) {}
 	inline void operator () (LogLevel /*level*/ , const mpt::ustring & /*text*/ ) {}
-	inline void operator () (const mpt::ustring & /*text*/ ) {}
-	inline void operator () (const std::string & /*text*/ ) {}
-#if MPT_WSTRING_CONVERT && !(MPT_USTRING_MODE_WIDE)
-	inline void operator () (const std::wstring & /*text*/ ) {}
-#endif
+	inline void operator () (const AnyStringLocale & /*text*/ ) {}
 };
 
 #define Log if(true) {} else mpt::log::Logger() // completely compile out arguments to Log() so that they do not even get evaluated

trunk/OpenMPT/common/mptPathString.cpp
@@ -174,7 +174,7 @@ mpt::PathString PathString::TunnelOutofCString(const CString &path)
 		// that filenames we get from MFC are always encoded in our hacked UTF8-in-CString encoding.
 		// Instead, we use a rough heuristic: if the string is parseable as UTF8, we assume it is.
 		// This fails for CP_ACP strings, that are also valid UTF8. That's the trade-off here.
-		if(mpt::ToCharset(mpt::CharsetUTF8, mpt::ToWide(mpt::CharsetUTF8, path.GetString())) == path.GetString())
+		if(mpt::IsUTF8(path.GetString()))
 		{
 			// utf8
 			return mpt::PathString::FromUTF8(path.GetString());

trunk/OpenMPT/common/mptString.cpp
@@ -1010,6 +1010,13 @@ Tdststring ConvertImpl(Charset to, Charset from, const Tsrcstring &src)
 } // namespace String
 
 
+bool IsUTF8(const std::string &str)
+//---------------------------------
+{
+	return (str == String::EncodeImpl<std::string>(mpt::CharsetUTF8, String::DecodeImpl<std::string>(mpt::CharsetUTF8, str)));
+}
+
+
 #if MPT_WSTRING_CONVERT
 std::wstring ToWide(Charset from, const std::string &str)
 {
@@ -1103,10 +1110,12 @@ CString ToCString(const CStringW &str)
 #if MPT_USTRING_MODE_WIDE
 // inline
 #else // !MPT_USTRING_MODE_WIDE
+#if MPT_WSTRING_CONVERT
 mpt::ustring ToUnicode(const std::wstring &str)
 {
 	return String::EncodeImpl<mpt::ustring>(mpt::CharsetUTF8, str);
 }
+#endif
 mpt::ustring ToUnicode(Charset from, const std::string &str)
 {
 	return String::ConvertImpl<mpt::ustring>(mpt::CharsetUTF8, from, str);

trunk/OpenMPT/common/mptString.h
@@ -150,6 +150,16 @@ enum Charset {
 };
 
 
+// Checks if the std::string represents an UTF8 string.
+// This is currently implemented as converting to std::wstring and back assuming UTF8 both ways,
+// and comparing the result to the original string.
+// Caveats:
+//  - can give false negatives because of possible unicode normalization during conversion
+//  - can give false positives if the 8bit encoding contains high-ascii only in valid utf8 groups
+//  - slow because of double conversion
+bool IsUTF8(const std::string &str);
+
+
 #define MPT_CHAR_TYPE    char
 #define MPT_CHAR(x)      x
 #define MPT_LITERAL(x)   x
@@ -199,6 +209,7 @@ typedef MPT_ENCODED_STRING_TYPE(mpt::CharsetUTF8) u8string;
 // If str does not contain any invalid characters, this conversion is lossless.
 // Invalid source bytes will be replaced by some replacement character or string.
 static inline std::wstring ToWide(const std::wstring &str) { return str; }
+static inline std::wstring ToWide(const wchar_t * str) { return (str ? std::wstring(str) : std::wstring()); }
 std::wstring ToWide(Charset from, const std::string &str);
 #endif
 
@@ -210,6 +221,7 @@ std::wstring ToWide(Charset from, const std::string &str);
 // destination charset will be replaced by some replacement character or string.
 #if MPT_WSTRING_CONVERT
 std::string ToCharset(Charset to, const std::wstring &str);
+static inline std::string ToCharset(Charset to, const wchar_t * str) { return ToCharset(to, str ? std::wstring(str) : std::wstring()); }
 #endif
 std::string ToCharset(Charset to, Charset from, const std::string &str);
 
@@ -224,6 +236,7 @@ std::string ToCharset(Charset to, Charset from, const std::string &str);
 // If UNICODE is defined, this is a completely lossless operation.
 static inline CString ToCString(const CString &str) { return str; }
 CString ToCString(const std::wstring &str);
+static inline CString ToCString(const wchar_t * str) { return ToCString(str ? std::wstring(str) : std::wstring()); }
 CString ToCString(Charset from, const std::string &str);
 
 // Convert from a MFC CString. The CString encoding depends on UNICODE.
@@ -231,11 +244,6 @@ CString ToCString(Charset from, const std::string &str);
 // If UNICODE is defined, this is a completely lossless operation.
 std::wstring ToWide(const CString &str);
 std::string ToCharset(Charset to, const CString &str);
-
-// Provide un-ambiguous conversion from wide string literal.
-static inline std::wstring ToWide(const wchar_t * str) { return ToWide(str ? std::wstring(str) : std::wstring()); }
-static inline std::string ToCharset(Charset to, const wchar_t * str) { return ToCharset(to, str ? std::wstring(str) : std::wstring()); }
-static inline CString ToCString(const wchar_t * str) { return ToCString(str ? std::wstring(str) : std::wstring()); }
 
 #ifdef UNICODE
 MPT_DEPRECATED static inline CString ToCStringW(const CString &str) { return ToCString(str); }
@@ -320,21 +328,23 @@ typedef mpt::u8string    ustring;
 #error "MPT_USTRING_MODE_WIDE depends on MPT_WSTRING_CONVERT)"
 #endif
 static inline mpt::ustring ToUnicode(const std::wstring &str) { return str; }
+static inline mpt::ustring ToUnicode(const wchar_t * str) { return (str ? std::wstring(str) : std::wstring()); }
 static inline mpt::ustring ToUnicode(Charset from, const std::string &str) { return ToWide(from, str); }
 #if defined(_MFC_VER)
 static inline mpt::ustring ToUnicode(const CString &str) { return ToWide(str); }
-static inline mpt::ustring ToUnicode(const wchar_t * str) { return ToUnicode(str ? std::wstring(str) : std::wstring()); }
 #ifndef UNICODE
 static inline mpt::ustring ToUnicode(const CStringW &str) { return ToWide(str); }
 #endif // !UNICODE
 #endif // MFC
 #else // !MPT_USTRING_MODE_WIDE
 static inline mpt::ustring ToUnicode(const mpt::ustring &str) { return str; }
+#if MPT_WSTRING_CONVERT
 mpt::ustring ToUnicode(const std::wstring &str);
+static inline mpt::ustring ToUnicode(const wchar_t * str) { return ToUnicode(str ? std::wstring(str) : std::wstring()); }
+#endif
 mpt::ustring ToUnicode(Charset from, const std::string &str);
 #if defined(_MFC_VER)
 mpt::ustring ToUnicode(const CString &str);
-static inline mpt::ustring ToUnicode(const wchar_t * str) { return ToUnicode(str ? std::wstring(str) : std::wstring()); }
 #ifndef UNICODE
 mpt::ustring ToUnicode(const CStringW &str);
 #endif // !UNICODE
@@ -369,6 +379,137 @@ static inline CStringW ToCStringW(const mpt::ustring &str) { return ToCStringW(T
 #define MPT_UTF8(x) mpt::ToUnicode(mpt::CharsetUTF8, x )
 
 } // namespace mpt
+
+
+
+
+
+// The AnyString types are meant to be used as function argument types only,
+// and only during the transition phase to all-unicode strings in the whole codebase.
+// Using an AnyString type as function argument avoids the need to overload a function for all the
+// different string types that we currently have.
+// Warning: These types will silently do charset conversions. Only use them when this can be tolerated.
+
+// BasicAnyString is convertable to mpt::ustring and constructable from any string at all.
+template <mpt::Charset charset = mpt::CharsetUTF8, bool tryUTF8 = true>
+class BasicAnyString : public mpt::ustring
+{
+
+private:
+	
+	static mpt::ustring From8bit(const std::string &str)
+	{
+		if(charset == mpt::CharsetUTF8)
+		{
+			return mpt::ToUnicode(mpt::CharsetUTF8, str);
+		}
+		// auto utf8 detection
+		if(tryUTF8 && mpt::IsUTF8(str))
+		{
+			return mpt::ToUnicode(mpt::CharsetUTF8, str);
+		} else
+		{
+			return mpt::ToUnicode(charset, str);
+		}
+	}
+
+public:
+
+	// 8 bit
+	BasicAnyString(const char *str) : mpt::ustring(str ? mpt::ToUnicode(charset, str) : mpt::ustring()) { }
+	BasicAnyString(const std::string str) : mpt::ustring(mpt::ToUnicode(charset, str)) { }
+
+	// unicode
+	BasicAnyString(const mpt::ustring &str) : mpt::ustring(str) { }
+#if MPT_COMPILER_HAS_RVALUE_REF
+	BasicAnyString(mpt::ustring &&str) : mpt::ustring(std::move(str)) { }
+#endif
+#if MPT_USTRING_MODE_UTF8 && MPT_WSTRING_CONVERT
+	BasicAnyString(const std::wstring &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#endif
+#if MPT_WSTRING_CONVERT
+	BasicAnyString(const wchar_t *str) : mpt::ustring(str ? mpt::ToUnicode(str) : mpt::ustring()) { }
+#endif
+
+	// mfc
+#if defined(_MFC_VER)
+	BasicAnyString(const CString &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#ifndef UNICODE
+	BasicAnyString(const CStringW &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#endif
+#endif
+
+	// fallback for custom string types
+	template <typename Tstring> BasicAnyString(const Tstring &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#if MPT_COMPILER_HAS_RVALUE_REF
+	template <typename Tstring> BasicAnyString(Tstring &&str) : mpt::ustring(mpt::ToUnicode(std::forward<Tstring>(str))) { }
+#endif
+
+};
+
+// AnyUnicodeString is convertable to mpt::ustring and constructable from any unicode string,
+class AnyUnicodeString : public mpt::ustring
+{
+
+public:
+
+	// unicode
+	AnyUnicodeString(const mpt::ustring &str) : mpt::ustring(str) { }
+#if MPT_COMPILER_HAS_RVALUE_REF
+	AnyUnicodeString(mpt::ustring &&str) : mpt::ustring(std::move(str)) { }
+#endif
+#if MPT_USTRING_MODE_UTF8 && MPT_WSTRING_CONVERT
+	AnyUnicodeString(const std::wstring &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#endif
+#if MPT_WSTRING_CONVERT
+	AnyUnicodeString(const wchar_t *str) : mpt::ustring(str ? mpt::ToUnicode(str) : mpt::ustring()) { }
+#endif
+
+	// mfc
+#if defined(_MFC_VER)
+	AnyUnicodeString(const CString &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#ifndef UNICODE
+	AnyUnicodeString(const CStringW &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#endif
+#endif
+
+	// fallback for custom string types
+	template <typename Tstring> AnyUnicodeString(const Tstring &str) : mpt::ustring(mpt::ToUnicode(str)) { }
+#if MPT_COMPILER_HAS_RVALUE_REF
+	template <typename Tstring> AnyUnicodeString(Tstring &&str) : mpt::ustring(mpt::ToUnicode(std::forward<Tstring>(str))) { }
+#endif
+
+};
+
+// AnyString
+// Try to do the smartest auto-magic we can do.
+#if defined(MPT_WITH_CHARSET_LOCALE)
+typedef BasicAnyString<mpt::CharsetLocale, true> AnyString;
+#elif MPT_OS_WINDOWS
+typedef BasicAnyString<mpt::CharsetWindows1252, true> AnyString;
+#else
+typedef BasicAnyString<mpt::CharsetISO8859_1, true> AnyString;
+#endif
+
+// AnyStringLocale
+// char-based strings are assumed to be in locale encoding.
+#if defined(MPT_WITH_CHARSET_LOCALE)
+typedef BasicAnyString<mpt::CharsetLocale, false> AnyStringLocale;
+#else
+typedef BasicAnyString<mpt::CharsetUTF8, false> AnyStringLocale;
+#endif
+
+// AnyStringUTF8orLocale
+// char-based strings are tried in UTF8 first, if this fails, locale is used.
+#if defined(MPT_WITH_CHARSET_LOCALE)
+typedef BasicAnyString<mpt::CharsetLocale, true> AnyStringUTF8orLocale;
+#else
+typedef BasicAnyString<mpt::CharsetUTF8, false> AnyStringUTF8orLocale;
+#endif
+
+// AnyStringUTF8
+// char-based strings are assumed to be in UTF8.
+typedef BasicAnyString<mpt::CharsetUTF8, false> AnyStringUTF8;
 
 
 

trunk/OpenMPT/mptrack/Moddoc.cpp
@@ -863,11 +863,11 @@ void CModDoc::AddToLog(LogLevel level, const mpt::ustring &text) const
 	{
 		switch(level)
 		{
-		case LogError:        Reporting::Error(mpt::ToWide(text));        break;
-		case LogWarning:      Reporting::Warning(mpt::ToWide(text));      break;
-		case LogInformation:  Reporting::Information(mpt::ToWide(text));  break;
-		case LogNotification: Reporting::Notification(mpt::ToWide(text)); break;
-		default:              Reporting::Information(mpt::ToWide(text));  break;
+		case LogError:        Reporting::Error(text);        break;
+		case LogWarning:      Reporting::Warning(text);      break;
+		case LogInformation:  Reporting::Information(text);  break;
+		case LogNotification: Reporting::Notification(text); break;
+		default:              Reporting::Information(text);  break;
 		}
 	}
 }
@@ -922,11 +922,11 @@ UINT CModDoc::ShowLog(const std::wstring &preamble, const std::wstring &title, C
 		std::wstring actualTitle = (title.length() == 0) ? MAINFRAME_TITLEW : title;
 		switch(GetMaxLogLevel())
 		{
-		case LogError:        Reporting::Error(text, actualTitle.c_str(), parent); break;
-		case LogWarning:      Reporting::Warning(text, actualTitle.c_str(), parent); break;
-		case LogInformation:  Reporting::Information(text, actualTitle.c_str(), parent); break;
-		case LogNotification: Reporting::Notification(text, actualTitle.c_str(), parent); break;
-		default:              Reporting::Information(text, actualTitle.c_str(), parent); break;
+		case LogError:        Reporting::Error(text, actualTitle, parent); break;
+		case LogWarning:      Reporting::Warning(text, actualTitle, parent); break;
+		case LogInformation:  Reporting::Information(text, actualTitle, parent); break;
+		case LogNotification: Reporting::Notification(text, actualTitle, parent); break;
+		default:              Reporting::Information(text, actualTitle, parent); break;
 		}
 		return IDOK;
 	}

commit 21e4263bd26a95211af5a2977c4159fc95faf47e

[Fix] Avoid having string literals for caption in Reporting::Confirm decay to bool and catching the wrong overload instead of converting to a string (broken since r4560).
git-svn-id: https://source.openmpt.org/svn/openmpt@5195 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Reporting.h
@@ -62,6 +62,9 @@ public:
 	// Show a confirmation dialog.
 	static ConfirmAnswer Confirm(const AnyStringLocale &text, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
 	static ConfirmAnswer Confirm(const AnyStringLocale &text, const AnyStringLocale &caption, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr);
+	// work-around string literals for caption decaying to bool and catching the wrong overload instead of converting to a string.
+	static ConfirmAnswer Confirm(const AnyStringLocale &text, const char *caption, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr) { return Confirm(text, AnyStringLocale(caption), showCancel, defaultNo, parent); }
+	static ConfirmAnswer Confirm(const AnyStringLocale &text, const wchar_t *caption, bool showCancel = false, bool defaultNo = false, const CWnd *parent = nullptr) { return Confirm(text, AnyStringLocale(caption), showCancel, defaultNo, parent); }
 
 	// Show a confirmation dialog.
 	static RetryAnswer RetryCancel(const AnyStringLocale &text, const CWnd *parent = nullptr);