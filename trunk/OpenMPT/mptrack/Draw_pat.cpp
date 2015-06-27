svn2github/OpenMPT

commit 916670bb1a37923403278668100cf82cac3541ee

[Fix] Fix some alignment issues occuring with smooth pattern scrolling (e.g. when using step feature)
git-svn-id: https://source.openmpt.org/svn/openmpt@5367 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Draw_pat.cpp
@@ -160,7 +160,14 @@ UINT CViewPattern::GetColumnOffset(PatternCursor::Columns column) const
 int CViewPattern::GetSmoothScrollOffset() const
 //---------------------------------------------
 {
-	return Util::muldivr_unsigned(m_szCell.cy, m_nPlayTick, std::max(1u, m_nTicksOnRow));
+	if((TrackerSettings::Instance().m_dwPatternSetup & PATTERN_SMOOTHSCROLL) != 0	// Actually using the smooth scroll feature
+		&& (m_Status & (psFollowSong | psDragActive)) == psFollowSong	// Not drawing a selection during playback
+		&& (m_nMidRow != 0 || GetYScrollPos() > 0)	// If active row is not centered, only scroll when display position is actually not at the top
+		&& IsLiveRecord())	// Actually playing live (not paused or stepping)
+	{
+		return Util::muldivr_unsigned(m_szCell.cy, m_nPlayTick, std::max(1u, m_nTicksOnRow));
+	}
+	return 0;
 }
 
 
@@ -246,14 +253,9 @@ PatternCursor CViewPattern::GetPositionFromPoint(POINT pt)
 {
 	const PATTERNFONT *pfnt = PatternFont::currentFont;
 	int xofs = GetXScrollPos();
-	int yofs = GetYScrollPos();
+	int yofs = GetYScrollPos() + GetSmoothScrollOffset();
 	int x = xofs + (pt.x - m_szHeader.cx) / GetColumnWidth();
 	if (pt.x < m_szHeader.cx) x = (xofs) ? xofs - 1 : 0;
-
-	if((TrackerSettings::Instance().m_dwPatternSetup & PATTERN_SMOOTHSCROLL) != 0 && (m_Status & (psFollowSong | psDragActive)) == psFollowSong && IsLiveRecord())
-	{
-		pt.y += GetSmoothScrollOffset();
-	}
 
 	int y = yofs - m_nMidRow + (pt.y - m_szHeader.cy) / m_szCell.cy;
 	if (y < 0) y = 0;
@@ -513,13 +515,7 @@ void CViewPattern::OnDraw(CDC *pDC)
 	UINT nColumnWidth = m_szCell.cx;
 	UINT ncols = sndFile.GetNumChannels();
 	int xpaint = m_szHeader.cx;
-	int ypaint = rcClient.top + m_szHeader.cy;
-
-	// Scroll tick by tick (always if active row is centered, otherwise only if the top row isn't the first one)
-	if(doSmoothScroll && liveRecord && (m_Status & (psFollowSong | psDragActive)) == psFollowSong && (m_nMidRow != 0 || yofs > 0))
-	{
-		ypaint -= GetSmoothScrollOffset();
-	}
+	int ypaint = rcClient.top + m_szHeader.cy - GetSmoothScrollOffset();
 	
 	if (m_nMidRow)
 	{
@@ -1514,7 +1510,7 @@ void CViewPattern::InvalidateRow(ROWINDEX n)
 		CRect rect;
 		GetClientRect(&rect);
 		rect.left = m_szHeader.cx;
-		rect.top = m_szHeader.cy;
+		rect.top = m_szHeader.cy - GetSmoothScrollOffset();
 		rect.top += (n - yofs) * m_szCell.cy;
 		rect.bottom = rect.top + m_szCell.cy;
 		InvalidateRect(&rect, FALSE);

trunk/OpenMPT/mptrack/View_pat.cpp
@@ -99,10 +99,6 @@ BEGIN_MESSAGE_MAP(CViewPattern, CModScrollView)
 	ON_COMMAND(ID_NEXTINSTRUMENT,	OnNextInstrument)
 	ON_COMMAND(ID_PREVINSTRUMENT,	OnPrevInstrument)
 	ON_COMMAND(ID_PATTERN_PLAYROW,	OnPatternStep)
-	ON_COMMAND(ID_CONTROLENTER,		OnPatternStep)
-	ON_COMMAND(ID_CONTROLTAB,		OnSwitchToOrderList)
-	ON_COMMAND(ID_PREVORDER,		OnPrevOrder)
-	ON_COMMAND(ID_NEXTORDER,		OnNextOrder)
 	ON_COMMAND(IDC_PATTERN_RECORD,	OnPatternRecord)
 	ON_COMMAND(ID_RUN_SCRIPT,					OnRunScript)
 	ON_COMMAND(ID_TRANSPOSE_UP,					OnTransposeUp)
@@ -2487,6 +2483,10 @@ void CViewPattern::PatternStep(ROWINDEX row)
 			return;
 
 		CriticalSection cs;
+
+		// In case we were previously in smooth scrolling mode during live playback, the pattern might be misaligned.
+		if(GetSmoothScrollOffset() != 0)
+			InvalidatePattern(true);
 
 		// Cut instruments/samples in virtual channels
 		for(CHANNELINDEX i = pSndFile->GetNumChannels(); i < MAX_CHANNELS; i++)

commit 03550de4f05041a4ea15bc93192b765f97be56d1

[Fix] Pattern tab: Fix for partially broken stuff in r5367 [Imp] Allow sending incoming MIDI notes to plugins even if no instrument is assigned to them (pass data as-is)
git-svn-id: https://source.openmpt.org/svn/openmpt@5370 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Draw_pat.cpp
@@ -253,11 +253,11 @@ PatternCursor CViewPattern::GetPositionFromPoint(POINT pt)
 {
 	const PATTERNFONT *pfnt = PatternFont::currentFont;
 	int xofs = GetXScrollPos();
-	int yofs = GetYScrollPos() + GetSmoothScrollOffset();
+	int yofs = GetYScrollPos();
 	int x = xofs + (pt.x - m_szHeader.cx) / GetColumnWidth();
 	if (pt.x < m_szHeader.cx) x = (xofs) ? xofs - 1 : 0;
 
-	int y = yofs - m_nMidRow + (pt.y - m_szHeader.cy) / m_szCell.cy;
+	int y = yofs - m_nMidRow + (pt.y - m_szHeader.cy + GetSmoothScrollOffset()) / m_szCell.cy;
 	if (y < 0) y = 0;
 	int xx = (pt.x - m_szHeader.cx) % GetColumnWidth(), dx = 0;
 	int imax = 4;
@@ -484,7 +484,6 @@ void CViewPattern::OnDraw(CDC *pDC)
 	const int vuHeight = MulDiv(VUMETERS_HEIGHT, m_nDPIy, 96);
 	const int colHeight = MulDiv(COLHDR_HEIGHT, m_nDPIy, 96);
 	const int recordInsX = MulDiv(3, m_nDPIx, 96);
-	const bool liveRecord = IsLiveRecord();
 	const bool doSmoothScroll = (TrackerSettings::Instance().m_dwPatternSetup & PATTERN_SMOOTHSCROLL) != 0;
 
 	GetClientRect(&rcClient);

trunk/OpenMPT/mptrack/Moddoc.cpp
@@ -1018,6 +1018,9 @@ void CModDoc::ProcessMIDI(uint32 midiData, INSTRUMENTINDEX ins, IMixPlugin *plug
 				PlayNote(note, ins, 0, false, vol);
 			}
 			return;
+		} else
+		{
+			plugin->MidiSend(midiData);
 		}
 		break;
 