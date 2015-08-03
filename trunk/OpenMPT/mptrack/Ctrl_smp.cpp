svn2github/OpenMPT

commit a2570fa1a422dc3e9927c08b6998c54c8cd7455b

[Imp] Sample Editor: Add a dropdown menue to the open button in the sample editor toolbar which allows explicitely importing only known sample formats or only raw samples. This allows importing known formats as raw samples as well as allows avoiding the possibility of raw samples getting mis-interpreted as some known format (which, although with very low propability, can happen, especially when relying on overly eager external libraries to do the format parsing).
git-svn-id: https://source.openmpt.org/svn/openmpt@5429 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Ctrl_smp.cpp
@@ -67,8 +67,11 @@ struct PowerOf2Exponent<1> { enum { value = 0 }; };
 BEGIN_MESSAGE_MAP(CCtrlSamples, CModControlDlg)
 	//{{AFX_MSG_MAP(CCtrlSamples)
 	ON_WM_VSCROLL()
+	ON_NOTIFY(TBN_DROPDOWN, IDC_TOOLBAR1, &CCtrlSamples::OnTbnDropDownToolBar)
 	ON_COMMAND(IDC_SAMPLE_NEW,			OnSampleNew)
 	ON_COMMAND(IDC_SAMPLE_OPEN,			OnSampleOpen)
+	ON_COMMAND(IDC_SAMPLE_OPENKNOWN,	OnSampleOpenKnown)
+	ON_COMMAND(IDC_SAMPLE_OPENRAW,		OnSampleOpenRaw)
 	ON_COMMAND(IDC_SAMPLE_SAVEAS,		OnSampleSave)
 	ON_COMMAND(IDC_SAMPLE_PLAY,			OnSamplePlay)
 	ON_COMMAND(IDC_SAMPLE_NORMALIZE,	OnNormalize)
@@ -216,9 +219,10 @@ BOOL CCtrlSamples::OnInitDialog()
 	m_ComboZoom.SetItemData(m_ComboZoom.AddString("1:128"), 8);
 	m_ComboZoom.SetCurSel(0);
 	// File ToolBar
+	m_ToolBar1.SetExtendedStyle(m_ToolBar1.GetExtendedStyle() | TBSTYLE_EX_DRAWDDARROWS);
 	m_ToolBar1.Init(CMainFrame::GetMainFrame()->m_PatternIcons,CMainFrame::GetMainFrame()->m_PatternIconsDisabled);
 	m_ToolBar1.AddButton(IDC_SAMPLE_NEW, TIMAGE_SAMPLE_NEW);
-	m_ToolBar1.AddButton(IDC_SAMPLE_OPEN, TIMAGE_OPEN);
+	m_ToolBar1.AddButton(IDC_SAMPLE_OPEN, TIMAGE_OPEN, TBSTYLE_BUTTON | TBSTYLE_DROPDOWN);
 	m_ToolBar1.AddButton(IDC_SAMPLE_SAVEAS, TIMAGE_SAVE);
 	// Edit ToolBar
 	m_ToolBar2.Init(CMainFrame::GetMainFrame()->m_PatternIcons,CMainFrame::GetMainFrame()->m_PatternIconsDisabled);
@@ -807,8 +811,8 @@ void CCtrlSamples::SetModified(SampleHint hint, bool updateAll, bool waveformMod
 
 
 
-bool CCtrlSamples::OpenSample(const mpt::PathString &fileName)
-//------------------------------------------------------------
+bool CCtrlSamples::OpenSample(const mpt::PathString &fileName, FlagSet<OpenSampleTypes> types)
+//--------------------------------------------------------------------------------------------
 {
 	BeginWaitCursor();
 	InputFile f(fileName);
@@ -826,10 +830,14 @@ bool CCtrlSamples::OpenSample(const mpt::PathString &fileName)
 	}
 
 	m_modDoc.GetSampleUndo().PrepareUndo(m_nSample, sundo_replace, "Replace");
-	bool bOk = m_sndFile.ReadSampleFromFile(m_nSample, file, TrackerSettings::Instance().m_MayNormalizeSamplesOnLoad);
+	bool bOk = false;
+	if(!bOk && types[OpenSampleKnown])
+	{
+		bOk = m_sndFile.ReadSampleFromFile(m_nSample, file, TrackerSettings::Instance().m_MayNormalizeSamplesOnLoad);
+	}
 	ModSample &sample = m_sndFile.GetSample(m_nSample);
 
-	if (!bOk)
+	if(!bOk && types[OpenSampleRaw])
 	{
 		CRawSampleDlg dlg(this);
 		if(rememberRawFormat)
@@ -977,6 +985,27 @@ void CCtrlSamples::OnZoomChanged()
 }
 
 
+void CCtrlSamples::OnTbnDropDownToolBar(NMHDR* pNMHDR, LRESULT* pResult)
+//----------------------------------------------------------------------
+{
+	LPNMTOOLBAR pToolBar = reinterpret_cast<LPNMTOOLBAR>(pNMHDR);
+	ClientToScreen(&(pToolBar->rcButton)); // TrackPopupMenu uses screen coords
+	switch(pToolBar->iItem)
+	{
+	case IDC_SAMPLE_OPEN:
+		{
+			CMenu menu;
+			menu.CreatePopupMenu();
+			menu.AppendMenu(MF_STRING, IDC_SAMPLE_OPENKNOWN, _T("Import &sample ..."));
+			menu.AppendMenu(MF_STRING, IDC_SAMPLE_OPENRAW, _T("Import &RAW sample ..."));
+			menu.TrackPopupMenu(TPM_LEFTALIGN | TPM_RIGHTBUTTON, pToolBar->rcButton.left, pToolBar->rcButton.bottom, this);
+		}
+		break;
+	}
+	*pResult = 0;
+}
+
+
 void CCtrlSamples::OnSampleNew()
 //------------------------------
 {
@@ -1047,7 +1076,81 @@ void CCtrlSamples::OnSampleOpen()
 			OnSampleNew();
 		}
 
-		if(!OpenSample(files[counter]))
+		if(!OpenSample(files[counter]), OpenSampleKnown | OpenSampleRaw)
+			ErrorBox(IDS_ERR_FILEOPEN, this);
+	}
+	SwitchToView();
+}
+
+
+void CCtrlSamples::OnSampleOpenKnown()
+//------------------------------------
+{
+	static int nLastIndex = 0;
+	FileDialog dlg = OpenFileDialog()
+		.AllowMultiSelect()
+		.EnableAudioPreview()
+		.ExtensionFilter("All Samples|*.wav;*.flac;*.oga;*.pat;*.s3i;*.smp;*.xi;*.aif;*.aiff;*.its;*.iff;*.8sv;*.8svx;*.svx;*.mp1;*.mp2;*.mp3|"
+			"Wave Files (*.wav)|*.wav|"
+	#ifndef NO_FLAC
+			"FLAC Files (*.flac,*.oga)|*.flac;*.oga|"
+	#endif // NO_FLAC
+	#ifndef NO_MP3_SAMPLES
+			"MPEG Files (*.mp1,*.mp2,*.mp3)|*.mp1;*.mp2;*.mp3|"
+	#endif // NO_MP3_SAMPLES
+			"XI Samples (*.xi)|*.xi|"
+			"Impulse Tracker Samples (*.its)|*.its|"
+			"ScreamTracker Samples (*.s3i,*.smp)|*.s3i;*.smp|"
+			"GF1 Patches (*.pat)|*.pat|"
+			"AIFF Files (*.aiff;*.8svx)|*.aif;*.aiff;*.iff;*.8sv;*.8svx;*.svx|"
+			"All Files (*.*)|*.*||")
+		.WorkingDirectory(TrackerSettings::Instance().PathSamples.GetWorkingDir())
+		.FilterIndex(&nLastIndex);
+	if(!dlg.Show(this)) return;
+
+	TrackerSettings::Instance().PathSamples.SetWorkingDir(dlg.GetWorkingDirectory());
+
+	const FileDialog::PathList &files = dlg.GetFilenames();
+	for(size_t counter = 0; counter < files.size(); counter++)
+	{
+		// If loading multiple samples, create new slots for them
+		if(counter > 0)
+		{
+			OnSampleNew();
+		}
+
+		if(!OpenSample(files[counter], OpenSampleKnown))
+			ErrorBox(IDS_ERR_FILEOPEN, this);
+	}
+	SwitchToView();
+}
+
+
+void CCtrlSamples::OnSampleOpenRaw()
+//----------------------------------
+{
+	static int nLastIndex = 0;
+	FileDialog dlg = OpenFileDialog()
+		.AllowMultiSelect()
+		.EnableAudioPreview()
+		.ExtensionFilter("Raw Samples (*.raw,*.snd,*.pcm)|*.raw;*.snd;*.pcm|"
+		"All Files (*.*)|*.*||")
+		.WorkingDirectory(TrackerSettings::Instance().PathSamples.GetWorkingDir())
+		.FilterIndex(&nLastIndex);
+	if(!dlg.Show(this)) return;
+
+	TrackerSettings::Instance().PathSamples.SetWorkingDir(dlg.GetWorkingDirectory());
+
+	const FileDialog::PathList &files = dlg.GetFilenames();
+	for(size_t counter = 0; counter < files.size(); counter++)
+	{
+		// If loading multiple samples, create new slots for them
+		if(counter > 0)
+		{
+			OnSampleNew();
+		}
+
+		if(!OpenSample(files[counter], OpenSampleRaw))
 			ErrorBox(IDS_ERR_FILEOPEN, this);
 	}
 	SwitchToView();

trunk/OpenMPT/mptrack/Ctrl_smp.h
@@ -16,6 +16,13 @@
 
 OPENMPT_NAMESPACE_BEGIN
 
+enum OpenSampleTypes
+{
+	OpenSampleKnown = (1<<0),
+	OpenSampleRaw   = (1<<1),
+};
+MPT_DECLARE_ENUM(OpenSampleTypes)
+
 //=======================================
 class CCtrlSamples: public CModControlDlg
 //=======================================
@@ -69,7 +76,7 @@ public:
 	~CCtrlSamples();
 
 	bool SetCurrentSample(SAMPLEINDEX nSmp, LONG lZoom = -1, bool bUpdNum = true);
-	bool OpenSample(const mpt::PathString &fileName);
+	bool OpenSample(const mpt::PathString &fileName, FlagSet<OpenSampleTypes> types = OpenSampleKnown | OpenSampleRaw);
 	bool OpenSample(const CSoundFile &sndFile, SAMPLEINDEX nSample);
 	Setting<LONG> &GetSplitPosRef() {return TrackerSettings::Instance().glSampleWindowHeight;} 	//rewbs.varWindowSize
 
@@ -92,8 +99,11 @@ protected:
 	afx_msg void OnZoomChanged();
 	afx_msg void OnPrevInstrument();
 	afx_msg void OnNextInstrument();
+	afx_msg void OnTbnDropDownToolBar(NMHDR* pNMHDR, LRESULT* pResult);
 	afx_msg void OnSampleNew();
 	afx_msg void OnSampleOpen();
+	afx_msg void OnSampleOpenKnown();
+	afx_msg void OnSampleOpenRaw();
 	afx_msg void OnSampleSave();
 	afx_msg void OnSamplePlay();
 	afx_msg void OnNormalize();

trunk/OpenMPT/mptrack/mptrack.rc
@@ -952,11 +952,11 @@ IDD_CONTROL_SAMPLES DIALOGEX 0, 0, 544, 106
 STYLE DS_SETFONT | DS_FIXEDSYS | WS_CHILD
 FONT 8, "MS Shell Dlg", 0, 0, 0x1
 BEGIN
-    CONTROL         "Toolbar1",IDC_TOOLBAR1,"ToolbarWindow32",WS_GROUP | 0x4d,4,4,55,17
-    RTEXT           "Sample",IDC_STATIC,62,8,24,8
-    EDITTEXT        IDC_EDIT_SAMPLE,90,6,35,12,ES_AUTOHSCROLL | ES_NUMBER
-    CONTROL         "Spin6",IDC_SPIN_SAMPLE,"msctls_updown32",UDS_WRAP | UDS_SETBUDDYINT | UDS_ALIGNRIGHT | UDS_AUTOBUDDY | UDS_ARROWKEYS | UDS_NOTHOUSANDS,118,4,11,14
-    COMBOBOX        IDC_COMBO_ZOOM,131,6,41,89,CBS_DROPDOWNLIST | WS_VSCROLL
+    CONTROL         "Toolbar1",IDC_TOOLBAR1,"ToolbarWindow32",WS_GROUP | 0x4d,4,4,68,17
+    RTEXT           "Sample",IDC_STATIC,72,8,24,8
+    EDITTEXT        IDC_EDIT_SAMPLE,102,6,30,12,ES_AUTOHSCROLL | ES_NUMBER
+    CONTROL         "Spin6",IDC_SPIN_SAMPLE,"msctls_updown32",UDS_WRAP | UDS_SETBUDDYINT | UDS_ALIGNRIGHT | UDS_AUTOBUDDY | UDS_ARROWKEYS | UDS_NOTHOUSANDS,120,4,12,14
+    COMBOBOX        IDC_COMBO_ZOOM,138,6,34,89,CBS_DROPDOWNLIST | WS_VSCROLL
     CTEXT           "Length: 000000 (16-bit)",IDC_TEXT5,175,6,90,13,SS_CENTERIMAGE,WS_EX_STATICEDGE
     CONTROL         "Toolbar2",IDC_TOOLBAR2,"ToolbarWindow32",WS_GROUP | 0x4d,268,4,252,17
     CTEXT           "Name",IDC_STATIC,102,26,28,12,SS_CENTERIMAGE | NOT WS_GROUP,WS_EX_STATICEDGE

trunk/OpenMPT/mptrack/resource.h
@@ -962,6 +962,8 @@
 #define IDC_TABABOUT                    2483
 #define IDC_EDITABOUT                   2484
 #define IDC_CONTAINER                   2485
+#define IDC_SAMPLE_OPENKNOWN            2486
+#define IDC_SAMPLE_OPENRAW              2487
 #define ID_FILE_NEWMOD                  32771
 #define ID_FILE_NEWXM                   32772
 #define ID_FILE_NEWS3M                  32773
@@ -1260,7 +1262,7 @@
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        541
 #define _APS_NEXT_COMMAND_VALUE         44645
-#define _APS_NEXT_CONTROL_VALUE         2486
+#define _APS_NEXT_CONTROL_VALUE         2488
 #define _APS_NEXT_SYMED_VALUE           901
 #endif
 #endif

commit 6aa860812539e027b44806b246c3327ba2fba141

[Fix] Fix stupid typo introduced in r5429 that caused the sample editor to report failure even for successfully loaded samples. (fixes http://bugs.openmpt.org/view.php?id=695)
git-svn-id: https://source.openmpt.org/svn/openmpt@5432 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Ctrl_smp.cpp
@@ -1076,7 +1076,7 @@ void CCtrlSamples::OnSampleOpen()
 			OnSampleNew();
 		}
 
-		if(!OpenSample(files[counter]), OpenSampleKnown | OpenSampleRaw)
+		if(!OpenSample(files[counter], OpenSampleKnown | OpenSampleRaw))
 			ErrorBox(IDS_ERR_FILEOPEN, this);
 	}
 	SwitchToView();