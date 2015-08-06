svn2github/OpenMPT

commit 33b39884be5b076cbdfb091e8561f2ff8ba7356f

[Fix] Various UNICODE fixes [Ref] Settings: We know the internal structur of the settings map in the class itself, so we can just use map.clear() there to forget all settings.
git-svn-id: https://source.openmpt.org/svn/openmpt@5435 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Mpdlgs.cpp
@@ -299,11 +299,6 @@ void COptionsSoundcard::UpdateUpdateInterval()
 void COptionsSoundcard::UpdateGeneral()
 //-------------------------------------
 {
-	{
-		m_CbnStoppedMode.EnableWindow(TRUE);
-		CPropertySheet *sheet = dynamic_cast<CPropertySheet *>(GetParent());
-		if(sheet) sheet->GetDlgItem(IDOK)->EnableWindow(TRUE);
-	}
 	// General
 	{
 		if(m_CurrentDeviceCaps.CanKeepDeviceRunning)
@@ -325,12 +320,10 @@ void COptionsSoundcard::UpdateGeneral()
 		}
 		CheckDlgButton(IDC_CHECK7, TrackerSettings::Instance().m_SoundSettingsOpenDeviceAtStartup ? BST_CHECKED : BST_UNCHECKED);
 	}
-	if(theApp.GetSoundDevicesManager()->IsDeviceUnavailable(m_CurrentDeviceInfo.GetIdentifier()))
-	{
-		m_CbnStoppedMode.EnableWindow(FALSE);
-		CPropertySheet *sheet = dynamic_cast<CPropertySheet *>(GetParent());
-		if(sheet) sheet->GetDlgItem(IDOK)->EnableWindow(FALSE);
-	}
+	bool isUnavailble = theApp.GetSoundDevicesManager()->IsDeviceUnavailable(m_CurrentDeviceInfo.GetIdentifier());
+	m_CbnStoppedMode.EnableWindow(isUnavailble ? FALSE : TRUE);
+	CPropertySheet *sheet = dynamic_cast<CPropertySheet *>(GetParent());
+	if(sheet) sheet->GetDlgItem(IDOK)->EnableWindow(isUnavailble ? FALSE : TRUE);
 }
 
 
@@ -1617,22 +1610,22 @@ void COptionsPlayer::OnSavePreset()
 }
 
 
-static void f2s(UINT f, LPSTR s)
-//------------------------------
+static void f2s(UINT f, TCHAR *s)
+//-------------------------------
 {
 	if (f < 1000)
 	{
-		wsprintf(s, "%dHz", f);
+		wsprintf(s, _T("%uHz"), f);
 	} else
 	{
-		UINT fHi = f / 1000;
-		UINT fLo = f % 1000;
+		UINT fHi = f / 1000u;
+		UINT fLo = f % 1000u;
 		if (fLo)
 		{
-			wsprintf(s, "%d.%dkHz", fHi, fLo/100);
+			wsprintf(s, _T("%u.%ukHz"), fHi, fLo/100);
 		} else
 		{
-			wsprintf(s, "%dkHz", fHi);
+			wsprintf(s, _T("%ukHz"), fHi);
 		}
 	}
 }
@@ -1641,7 +1634,7 @@ static void f2s(UINT f, LPSTR s)
 void COptionsPlayer::UpdateDialog()
 //---------------------------------
 {
-	CHAR s[32];
+	TCHAR s[32];
 	for (UINT i=0; i<MAX_EQ_BANDS; i++)
 	{
 		int n = 32 - m_EQPreset.Gains[i];
@@ -1650,10 +1643,10 @@ void COptionsPlayer::UpdateDialog()
 		if (n != (m_Sliders[i].GetPos() & 0xFFFF)) m_Sliders[i].SetPos(n);
 		f2s(m_EQPreset.Freqs[i], s);
 		SetDlgItemText(IDC_TEXT1 + i, s);
-		for(int i = 0; i < CountOf(TrackerSettings::Instance().m_EqUserPresets); i++)
-		{
-			SetDlgItemText(IDC_BUTTON1 + i,	TrackerSettings::Instance().m_EqUserPresets[i].szName);
-		}
+	}
+	for(int i = 0; i < CountOf(TrackerSettings::Instance().m_EqUserPresets); i++)
+	{
+		SetDlgItemText(IDC_BUTTON1 + i, TrackerSettings::Instance().m_EqUserPresets[i].szName);
 	}
 }
 
@@ -1664,10 +1657,10 @@ void COptionsPlayer::OnSliderMenu(UINT nID)
 	UINT n = nID - ID_EQSLIDER_BASE;
 	if (n < MAX_EQ_BANDS)
 	{
-		CHAR s[32];
+		TCHAR s[32];
 		HMENU hMenu = ::CreatePopupMenu();
 		m_nSliderMenu = n;
-		if (!hMenu)	return;
+		if (!hMenu) return;
 		const UINT *pFreqs = gEqBandFreqs[m_nSliderMenu];
 		for (UINT i = 0; i < EQ_MAX_FREQS; i++)
 		{
@@ -1771,13 +1764,13 @@ BOOL CMidiSetupDlg::OnInitDialog()
 	m_ATBehaviour.ResetContent();
 	static const struct
 	{
-		const char *text;
+		const TCHAR *text;
 		RecordAftertouchOptions option;
 	} aftertouchOptions[] =
 	{
-		{ "Do not record Aftertouch", atDoNotRecord },
-		{ "Record as Volume Commands", atRecordAsVolume },
-		{ "Record as MIDI Macros", atRecordAsMacro },
+		{ _T("Do not record Aftertouch"), atDoNotRecord },
+		{ _T("Record as Volume Commands"), atRecordAsVolume },
+		{ _T("Record as MIDI Macros"), atRecordAsMacro },
 	};
 
 	for(int i = 0; i < CountOf(aftertouchOptions); i++)

trunk/OpenMPT/mptrack/MainFrm.cpp
@@ -2171,8 +2171,8 @@ void CMainFrame::SwitchToActiveView()
 void CMainFrame::OnUpdateTime(CCmdUI *)
 //-------------------------------------
 {
-	CHAR s[64];
-	wsprintf(s, "%d:%02d:%02d",
+	TCHAR s[64];
+	wsprintf(s, _T("%u:%02u:%02u"),
 		m_dwTimeSec / 3600, (m_dwTimeSec / 60) % 60, m_dwTimeSec % 60);
 
 	if(m_pSndFile != nullptr && m_pSndFile != &m_WaveFile && !m_pSndFile->IsPaused())
@@ -2180,11 +2180,11 @@ void CMainFrame::OnUpdateTime(CCmdUI *)
 		PATTERNINDEX nPat = m_pSndFile->m_PlayState.m_nPattern;
 		if(m_pSndFile->Patterns.IsValidIndex(nPat))
 		{
-			if(nPat < 10) strcat(s, " ");
-			if(nPat < 100) strcat(s, " ");
-			wsprintf(s + strlen(s), " [%d]", nPat);
+			if(nPat < 10) _tcscat(s, _T(" "));
+			if(nPat < 100) _tcscat(s, _T(" "));
+			wsprintf(s + _tcslen(s), _T(" [%d]"), nPat);
 		}
-		wsprintf(s + strlen(s), " %dch", m_nAvgMixChn);
+		wsprintf(s + _tcslen(s), _T(" %dch"), m_nAvgMixChn);
 	}
 	m_wndStatusBar.SetPaneText(m_wndStatusBar.CommandToIndex(ID_INDICATOR_TIME), s, TRUE);
 }

trunk/OpenMPT/mptrack/Moddoc.cpp
@@ -3033,16 +3033,15 @@ void CModDoc::PrepareUndoForAllPatterns(bool storeChannelInfo, const char *descr
 
 
 CString CModDoc::LinearToDecibels(double value, double valueAtZeroDB)
-//_------------------------------------------------------------------
+//-------------------------------------------------------------------
 {
 	if (value == 0) return _T("-inf");
 
 	double changeFactor = value / valueAtZeroDB;
 	double dB = 20.0 * std::log10(changeFactor);
 
-	char sign = (dB >= 0) ? '+' : ' ';
-	CString s;
-	s.Format(_T("%c%.2f dB"), sign, dB);
+	CString s = (dB >= 0) ? _T("+") : _T("");
+	s.AppendFormat(_T("%.2f dB"), dB);
 	return s;
 }
 

trunk/OpenMPT/mptrack/Settings.cpp
@@ -188,10 +188,9 @@ void SettingsContainer::ForgetSetting(const SettingPath &path)
 
 void SettingsContainer::ForgetAll()
 {
-	while(!map.empty())
-	{
-		Forget(map.begin()->first);
-	}
+	ASSERT(theApp.InGuiThread());
+	ASSERT(!CMainFrame::GetMainFrame() || (CMainFrame::GetMainFrame() && !CMainFrame::GetMainFrame()->InNotifyHandler())); // This is a slow path, use CachedSetting for stuff that is accessed in notify handler.
+	map.clear();
 }
 
 void SettingsContainer::RemoveSetting(const SettingPath &path)

commit 72a30532ce53d052a8e863fde2763ab57a189c6e

[Fix] sounddev: The stopmode combobox should be disabled for non-ASIO devices. (broke in r5435)
git-svn-id: https://source.openmpt.org/svn/openmpt@5441 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Mpdlgs.cpp
@@ -303,7 +303,6 @@ void COptionsSoundcard::UpdateGeneral()
 	{
 		if(m_CurrentDeviceCaps.CanKeepDeviceRunning)
 		{
-			m_CbnStoppedMode.EnableWindow(TRUE);
 			m_CbnStoppedMode.ResetContent();
 			m_CbnStoppedMode.AddString(_T("Close driver"));
 			m_CbnStoppedMode.AddString(_T("Pause driver"));
@@ -311,7 +310,6 @@ void COptionsSoundcard::UpdateGeneral()
 			m_CbnStoppedMode.SetCurSel(TrackerSettings::Instance().m_SoundSettingsStopMode);
 		} else
 		{
-			m_CbnStoppedMode.EnableWindow(FALSE);
 			m_CbnStoppedMode.ResetContent();
 			m_CbnStoppedMode.AddString(_T("Close driver"));
 			m_CbnStoppedMode.AddString(_T("Close driver"));
@@ -321,7 +319,7 @@ void COptionsSoundcard::UpdateGeneral()
 		CheckDlgButton(IDC_CHECK7, TrackerSettings::Instance().m_SoundSettingsOpenDeviceAtStartup ? BST_CHECKED : BST_UNCHECKED);
 	}
 	bool isUnavailble = theApp.GetSoundDevicesManager()->IsDeviceUnavailable(m_CurrentDeviceInfo.GetIdentifier());
-	m_CbnStoppedMode.EnableWindow(isUnavailble ? FALSE : TRUE);
+	m_CbnStoppedMode.EnableWindow(isUnavailble ? FALSE : (m_CurrentDeviceCaps.CanKeepDeviceRunning ? TRUE : FALSE));
 	CPropertySheet *sheet = dynamic_cast<CPropertySheet *>(GetParent());
 	if(sheet) sheet->GetDlgItem(IDOK)->EnableWindow(isUnavailble ? FALSE : TRUE);
 }