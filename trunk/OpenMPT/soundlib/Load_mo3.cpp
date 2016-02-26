svn2github/OpenMPT

commit 0bea2e42c7d5983e02372ff2a994408e48b667c5

[Mod] build: Add NO_LTDL=1 option to Makefile build. [Mod] build: Change USE_MO3 option to USE_UNMO3 in Makefile build. [Mod] build: Add --without-ltdl and change --enable-mo3 to --enable-unmo3 in autotools build system accordingly. [Doc] libopenmpt: Document the build system changes.
git-svn-id: https://source.openmpt.org/svn/openmpt@5963 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Load_mo3.cpp
@@ -74,7 +74,7 @@ public:
 	ComponentUnMO3() : ComponentLibrary(ComponentTypeForeign) { }
 	bool DoInitialize()
 	{
-#ifdef MPT_LINKED_UNMO3
+#if !defined(MPT_WITH_UNMO3_DYNBIND)
 		UNMO3_GetVersion = &(::UNMO3_GetVersion);
 		UNMO3_Free = &(::UNMO3_Free);
 		UNMO3_Decode_Old = nullptr;

trunk/OpenMPT/Makefile
@@ -57,8 +57,11 @@
 # Build flags for libopenmpt (provide on each `make` invocation)
 #  (defaults are 0):
 #
+#  NO_LTDL=1        Do not require libltdl
+#
 #  NO_ZLIB=1        Avoid using zlib, even if found
-#  USE_MO3=1        Support dynamic loading of unmo3 shared library
+#
+#  USE_UNMO3=1      Support dynamic loading of unmo3 shared library
 #
 #
 # Build flags for openmpt123 (provide on each `make` invocation)
@@ -326,6 +329,12 @@ endif
 
 endif
 
+ifeq ($(NO_LTDL),1)
+else
+CPPFLAGS_LTDL := -DMPT_WITH_LTDL
+LDLIBS_LTDL   := -lltdl
+endif
+
 ifeq ($(NO_ZLIB),1)
 else
 #LDLIBS   += -lz
@@ -338,9 +347,9 @@ NO_ZLIB:=1
 endif
 endif
 
-ifeq ($(USE_MO3),1)
-CPPFLAGS_MO3 := -DMPT_WITH_UNMO3
-LDLIBS_MO3  := -lltdl
+ifeq ($(USE_UNMO3),1)
+CPPFLAGS_UNMO3 := -DMPT_WITH_UNMO3_DYNBIND
+LDLIBS_UNMO3   := 
 else
 endif
 
@@ -417,9 +426,9 @@ NO_SNDFILE:=1
 endif
 endif
 
-CPPFLAGS += $(CPPFLAGS_ZLIB) $(CPPFLAGS_MO3)
-LDFLAGS += $(LDFLAGS_ZLIB) $(LDFLAGS_MO3)
-LDLIBS += $(LDLIBS_ZLIB) $(LDLIBS_MO3)
+CPPFLAGS += $(CPPFLAGS_LTDL) $(CPPFLAGS_ZLIB) $(CPPFLAGS_UNMO3)
+LDFLAGS += $(LDFLAGS_LTDL) $(LDFLAGS_ZLIB) $(LDFLAGS_UNMO3)
+LDLIBS += $(LDLIBS_LTDL) $(LDLIBS_ZLIB) $(LDLIBS_UNMO3)
 
 CPPFLAGS_OPENMPT123 += $(CPPFLAGS_SDL2) $(CPPFLAGS_SDL) $(CPPFLAGS_PORTAUDIO) $(CPPFLAGS_FLAC) $(CPPFLAGS_SNDFILE)
 LDFLAGS_OPENMPT123  += $(LDFLAGS_SDL2) $(LDFLAGS_SDL) $(LDFLAGS_PORTAUDIO) $(LDFLAGS_FLAC) $(LDFLAGS_SNDFILE)

trunk/OpenMPT/build/android_ndk/Android-unmo3.mk
@@ -33,8 +33,8 @@ LOCAL_CPP_FEATURES += rtti
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/common $(LOCAL_PATH)/build/svn_version
 
-LOCAL_CFLAGS   :=            -fvisibility=hidden -DLIBOPENMPT_BUILD -DMPT_WITH_ZLIB -DMPT_WITH_UNMO3 -DMPT_LINKED_UNMO3
-LOCAL_CPPFLAGS := -std=c++11 -fvisibility=hidden -DLIBOPENMPT_BUILD -DMPT_WITH_ZLIB -DMPT_WITH_UNMO3 -DMPT_LINKED_UNMO3
+LOCAL_CFLAGS   :=            -fvisibility=hidden -DLIBOPENMPT_BUILD -DMPT_WITH_ZLIB -DMPT_WITH_UNMO3 
+LOCAL_CPPFLAGS := -std=c++11 -fvisibility=hidden -DLIBOPENMPT_BUILD -DMPT_WITH_ZLIB -DMPT_WITH_UNMO3 
 LOCAL_LDLIBS := -lz
 
 ifeq ($(TARGET_ARCH_ABI),armeabi)

trunk/OpenMPT/build/autotools/Makefile.am
@@ -84,10 +84,10 @@ pkgconfig_DATA += libopenmpt/libopenmpt.pc
 lib_LTLIBRARIES += libopenmpt.la
 libopenmpt_la_LDFLAGS = -version-info 0:16:0
 nobase_include_HEADERS += libopenmpt/libopenmpt.h libopenmpt/libopenmpt.hpp libopenmpt/libopenmpt_version.h libopenmpt/libopenmpt_config.h libopenmpt/libopenmpt_stream_callbacks_fd.h libopenmpt/libopenmpt_stream_callbacks_file.h libopenmpt/libopenmpt_ext.hpp
-libopenmpt_la_CPPFLAGS = -DLIBOPENMPT_BUILD -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(LTDL_CPPFLAGS)
+libopenmpt_la_CPPFLAGS = -DLIBOPENMPT_BUILD -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(LTDL_CPPFLAGS) $(UNMO3_CPPFLAGS)
 libopenmpt_la_CXXFLAGS = $(ZLIB_CFLAGS)
 libopenmpt_la_CFLAGS = $(ZLIB_CFLAGS)
-libopenmpt_la_LIBADD = $(ZLIB_LIBS) $(LTDL_LIBS)
+libopenmpt_la_LIBADD = $(ZLIB_LIBS) $(LTDL_LIBS) $(UNMO3_LIBS)
 libopenmpt_la_SOURCES = 
 libopenmpt_la_SOURCES += build/svn_version/svn_version.h
 libopenmpt_la_SOURCES += common/AudioCriticalSection.cpp
@@ -301,10 +301,10 @@ libmodplug_la_SOURCES += libopenmpt/libopenmpt_modplug_cpp.cpp
 endif
 
 check_PROGRAMS += libopenmpttest
-libopenmpttest_CPPFLAGS = -DLIBOPENMPT_BUILD -DLIBOPENMPT_BUILD_TEST -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(LTDL_CPPFLAGS)
+libopenmpttest_CPPFLAGS = -DLIBOPENMPT_BUILD -DLIBOPENMPT_BUILD_TEST -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(LTDL_CPPFLAGS) $(UNMO3_CPPFLAGS)
 libopenmpttest_CXXFLAGS = $(ZLIB_CFLAGS)
 libopenmpttest_CFLAGS = $(ZLIB_CFLAGS)
-libopenmpttest_LDADD = $(ZLIB_LIBS) $(LTDL_LIBS)
+libopenmpttest_LDADD = $(ZLIB_LIBS) $(LTDL_LIBS) $(UNMO3_LIBS)
 libopenmpttest_SOURCES = 
 libopenmpttest_SOURCES += libopenmpt/libopenmpt_test.cpp
 libopenmpttest_SOURCES += test/test.cpp

trunk/OpenMPT/build/autotools/configure.ac
@@ -32,8 +32,8 @@ AS_IF([test "x$with_zlib" != "xno"],
 )
 
 # Optional libopenmpt dependency: libltdl
-AC_ARG_ENABLE([unmo3], AS_HELP_STRING([--enable-unmo3], [Enable the use of dynamically loaded libunmo3.]))
-AS_IF([test "x$enable_unmo3" = "xyes"],
+AC_ARG_WITH([ltdl], AS_HELP_STRING([--without-ltdl], [Disable the use of libtool libltdl for dynamic loading of third party libraries.]))
+AS_IF([test "x$with_ltdl" = "xno"],
  [
   AC_CHECK_HEADER([ltdl.h],
    [
@@ -62,7 +62,7 @@ AS_IF([test "x$enable_unmo3" = "xyes"],
 )
 AS_IF([test "x$have_ltdl" = "x1"],
  [
-  LTDL_CPPFLAGS=-DMPT_WITH_UNMO3
+  LTDL_CPPFLAGS=-DMPT_WITH_LTDL
   LTDL_LIBS=-lltdl
  ],
  [
@@ -72,6 +72,22 @@ AS_IF([test "x$have_ltdl" = "x1"],
 )
 AC_SUBST([LTDL_CPPFLAGS])
 AC_SUBST([LTDL_LIBS])
+
+
+# Optional libopenmpt feature: unmo3
+AC_ARG_ENABLE([unmo3], AS_HELP_STRING([--enable-unmo3], [Enable the use of dynamically loaded libunmo3.]))
+AS_IF([test "x$enable_unmo3" = "xyes"],
+ [
+  UNMO3_CPPFLAGS=-DMPT_WITH_UNMO3_DYNBIND
+  UNMO3_LIBS=
+ ],
+ [
+  UNMO3_CPPFLAGS=
+  UNMO3_LIBS=
+ ]
+)
+AC_SUBST([UNMO3_CPPFLAGS])
+AC_SUBST([UNMO3_LIBS])
 
 # Optional openmpt123 and examples dependency
 AC_ARG_WITH([portaudio], AS_HELP_STRING([--without-portaudio], [Disable use of libportaudio.]))

trunk/OpenMPT/common/BuildSettings.h
@@ -122,6 +122,7 @@
 //#define MPT_WITH_MINIZ
 #define MPT_WITH_OGG
 #define MPT_WITH_STBVORBIS
+#define MPT_WITH_UNMO3_DYNBIND
 #define MPT_WITH_ZLIB
 
 #endif
@@ -134,6 +135,7 @@
 #define MPT_WITH_MINIZ
 //#define MPT_WITH_OGG
 //#define MPT_WITH_STBVORBIS
+#define MPT_WITH_UNMO3_DYNBIND
 //#define MPT_WITH_ZLIB
 
 #endif
@@ -253,6 +255,11 @@
 #define NO_VST
 #define NO_DMO
 #define NO_PLUGINS
+#if defined(MPT_WITH_UNMO3_DYNBIND)
+#ifndef MPT_WITH_UNMO3
+#define MPT_WITH_UNMO3
+#endif
+#endif
 #if !defined(MPT_WITH_UNMO3) && !(MPT_COMPILER_MSVC)
 #ifndef NO_MO3
 #define NO_MO3

trunk/OpenMPT/common/mptLibrary.cpp
@@ -16,7 +16,7 @@
 #include <windows.h>
 #elif MPT_OS_ANDROID
 #include <dlfcn.h>
-#else
+#elif defined(MPT_WITH_LTDL)
 #include <ltdl.h>
 #endif
 #endif
@@ -231,7 +231,7 @@ public:
 
 
 
-#else // MPT_OS
+#elif defined(MPT_WITH_LTDL)
 
 
 class LibraryHandle
@@ -284,6 +284,46 @@ public:
 			return nullptr;
 		}
 		return reinterpret_cast<FuncPtr>(lt_dlsym(handle, symbol.c_str()));
+	}
+
+};
+
+
+#else // MPT_OS
+
+
+// dummy implementation
+
+class LibraryHandle
+{
+public:
+
+	LibraryHandle(const mpt::LibraryPath &path)
+	{
+		MPT_UNREFERENCED_PARAMETER(path);
+		return;
+	}
+
+	~LibraryHandle()
+	{
+		return;
+	}
+
+public:
+
+	bool IsValid() const
+	{
+		return false;
+	}
+
+	FuncPtr GetProcAddress(const std::string &symbol) const
+	{
+		MPT_UNREFERENCED_PARAMETER(symbol);
+		if(!IsValid())
+		{
+			return nullptr;
+		}
+		return nullptr;
 	}
 
 };

commit 169e0eae847311c67e5cd1d7439b2819b81c1c29

[Fix] mo3: Android build broke in r5963.
git-svn-id: https://source.openmpt.org/svn/openmpt@5964 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Load_mo3.cpp
@@ -38,13 +38,13 @@
 #else
 #define UNMO3_API
 #endif
-#ifdef MPT_LINKED_UNMO3
+#if !defined(MPT_WITH_UNMO3_DYNBIND)
 extern "C" {
 OPENMPT_NAMESPACE::uint32 UNMO3_API UNMO3_GetVersion(void);
 void UNMO3_API UNMO3_Free(const void *data);
 OPENMPT_NAMESPACE::int32 UNMO3_API UNMO3_Decode(const void **data, OPENMPT_NAMESPACE::uint32 *len, OPENMPT_NAMESPACE::uint32 flags);
 }
-#endif // MPT_LINKED_UNMO3
+#endif // !MPT_WITH_UNMO3_DYNBIND
 #endif // !NO_MO3
 
 