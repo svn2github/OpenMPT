svn2github/OpenMPT

commit 9dfa0c53de51404d365c24adf1ed29cd5cae32fb

[New] Added emulation of DMO Distortion effect for non-Windows platforms. [Fix] DigiBooster Echo and DMO emulation did not process plugin mixops. [Mod] OpenMPT: Version is now 1.26.00.21
git-svn-id: https://source.openmpt.org/svn/openmpt@6234 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/plugins/DigiBoosterEcho.cpp
@@ -39,8 +39,9 @@ void DigiBoosterEcho::Process(float *pOutL, float *pOutR, uint32 numFrames)
 //-------------------------------------------------------------------------
 {
 	const float *srcL = m_mixBuffer.GetInputBuffer(0), *srcR = m_mixBuffer.GetInputBuffer(1);
+	float *outL = m_mixBuffer.GetOutputBuffer(0), *outR = m_mixBuffer.GetOutputBuffer(1);
 
-	while(numFrames--)
+	for(uint32 i = numFrames; i != 0; i--)
 	{
 		int readPos = m_writePos - m_delayTime;
 		if(readPos < 0)
@@ -76,6 +77,8 @@ void DigiBoosterEcho::Process(float *pOutL, float *pOutR, uint32 numFrames)
 		*pOutL++ += (l * m_NMix + lDelay * m_PMix);
 		*pOutR++ += (r * m_NMix + rDelay * m_PMix);
 	}
+
+	ProcessMixOps(pOutL, pOutR, m_mixBuffer.GetOutputBuffer(0), m_mixBuffer.GetOutputBuffer(1), numFrames);
 }
 
 

trunk/OpenMPT/build/android_ndk/Android-minimp3-stbvorbis.mk
@@ -115,6 +115,7 @@ LOCAL_SRC_FILES := \
 	soundlib/XMTools.cpp \
 	soundlib/plugins/DigiBoosterEcho.cpp \
 	soundlib/plugins/dmo/DMOPlugin.cpp \
+	soundlib/plugins/dmo/Distortion.cpp \
 	soundlib/plugins/dmo/Echo.cpp \
 	soundlib/plugins/dmo/Gargle.cpp \
 	soundlib/plugins/dmo/ParamEq.cpp \

trunk/OpenMPT/build/android_ndk/Android-mpg123-vorbis.mk
@@ -113,6 +113,7 @@ LOCAL_SRC_FILES := \
 	soundlib/XMTools.cpp \
 	soundlib/plugins/DigiBoosterEcho.cpp \
 	soundlib/plugins/dmo/DMOPlugin.cpp \
+	soundlib/plugins/dmo/Distortion.cpp \
 	soundlib/plugins/dmo/Echo.cpp \
 	soundlib/plugins/dmo/Gargle.cpp \
 	soundlib/plugins/dmo/ParamEq.cpp \

trunk/OpenMPT/build/android_ndk/Android-unmo3.mk
@@ -146,6 +146,7 @@ LOCAL_SRC_FILES := \
 	soundlib/XMTools.cpp \
 	soundlib/plugins/DigiBoosterEcho.cpp \
 	soundlib/plugins/dmo/DMOPlugin.cpp \
+	soundlib/plugins/dmo/Distortion.cpp \
 	soundlib/plugins/dmo/Echo.cpp \
 	soundlib/plugins/dmo/Gargle.cpp \
 	soundlib/plugins/dmo/ParamEq.cpp \

trunk/OpenMPT/build/android_ndk/Android.mk
@@ -114,6 +114,7 @@ LOCAL_SRC_FILES := \
 	soundlib/XMTools.cpp \
 	soundlib/plugins/DigiBoosterEcho.cpp \
 	soundlib/plugins/dmo/DMOPlugin.cpp \
+	soundlib/plugins/dmo/Distortion.cpp \
 	soundlib/plugins/dmo/Echo.cpp \
 	soundlib/plugins/dmo/Gargle.cpp \
 	soundlib/plugins/dmo/ParamEq.cpp \

trunk/OpenMPT/build/autotools/Makefile.am
@@ -261,6 +261,8 @@ libopenmpt_la_SOURCES += soundlib/XMTools.cpp
 libopenmpt_la_SOURCES += soundlib/XMTools.h
 libopenmpt_la_SOURCES += soundlib/plugins/dmo/DMOPlugin.cpp
 libopenmpt_la_SOURCES += soundlib/plugins/dmo/DMOPlugin.h
+libopenmpt_la_SOURCES += soundlib/plugins/dmo/Distortion.cpp
+libopenmpt_la_SOURCES += soundlib/plugins/dmo/Distortion.h
 libopenmpt_la_SOURCES += soundlib/plugins/dmo/Echo.cpp
 libopenmpt_la_SOURCES += soundlib/plugins/dmo/Echo.h
 libopenmpt_la_SOURCES += soundlib/plugins/dmo/Gargle.cpp
@@ -501,6 +503,9 @@ libopenmpttest_SOURCES += soundlib/XMTools.h
 libopenmpttest_SOURCES += soundlib/plugins/PlugInterface.h
 libopenmpttest_SOURCES += soundlib/plugins/dmo/DMOPlugin.cpp
 libopenmpttest_SOURCES += soundlib/plugins/dmo/DMOPlugin.h
+libopenmpttest_SOURCES += soundlib/plugins/dmo/Distortion.cpp
+libopenmpttest_SOURCES += soundlib/plugins/dmo/Distortion.h
+libopenmpttest_SOURCES += soundlib/plugins/dmo/Echo.h
 libopenmpttest_SOURCES += soundlib/plugins/dmo/Echo.cpp
 libopenmpttest_SOURCES += soundlib/plugins/dmo/Echo.h
 libopenmpttest_SOURCES += soundlib/plugins/dmo/Gargle.cpp

trunk/OpenMPT/build/vs2008/OpenMPT.vcproj
@@ -2808,6 +2808,14 @@
 					>
 				</File>
 				<File
+					RelativePath="..\..\soundlib\plugins\Distortion.cpp"
+					>
+				</File>
+				<File
+					RelativePath="..\..\soundlib\plugins\Distortion.h"
+					>
+				</File>
+				<File
 					RelativePath="..\..\soundlib\plugins\OpCodes.h"
 					>
 				</File>
@@ -2848,6 +2856,110 @@
 					</File>
 					<File
 						RelativePath="..\..\soundlib\plugins\dmo\DMOPlugin.h"
+						>
+					</File>
+					<File
+						RelativePath="..\..\soundlib\plugins\dmo\Distortion.cpp"
+						>
+						<FileConfiguration
+							Name="Debug|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="Debug|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="Release|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="Release|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="DebugMDd|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="DebugMDd|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="ReleaseLTCG|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="ReleaseLTCG|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="DebugShared|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="DebugShared|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="ReleaseShared|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="ReleaseShared|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+					</File>
+					<File
+						RelativePath="..\..\soundlib\plugins\dmo\Distortion.h"
 						>
 					</File>
 					<File

trunk/OpenMPT/build/vs2008/libopenmpt-small.vcproj
@@ -1378,6 +1378,14 @@
 					>
 				</File>
 				<File
+					RelativePath="..\..\soundlib\plugins\Distortion.cpp"
+					>
+				</File>
+				<File
+					RelativePath="..\..\soundlib\plugins\Distortion.h"
+					>
+				</File>
+				<File
 					RelativePath="..\..\soundlib\plugins\OpCodes.h"
 					>
 				</File>
@@ -1418,6 +1426,78 @@
 					</File>
 					<File
 						RelativePath="..\..\soundlib\plugins\dmo\DMOPlugin.h"
+						>
+					</File>
+					<File
+						RelativePath="..\..\soundlib\plugins\dmo\Distortion.cpp"
+						>
+						<FileConfiguration
+							Name="Debug|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="Debug|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="Release|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="Release|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="DebugShared|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="DebugShared|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="ReleaseShared|Win32"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+						<FileConfiguration
+							Name="ReleaseShared|x64"
+							>
+							<Tool
+								Name="VCCLCompilerTool"
+								ObjectFile="$(IntDir)\Distortion1.obj"
+							/>
+						</FileConfiguration>
+					</File>
+					<File
+						RelativePath="..\..\soundlib\plugins\dmo\Distortion.h"
 						>
 					</File>
 					<File

commit 00c4b0488b35fb8dc49ce4c1332022d491cb8e59

[Fix] Crash in DigiBooster Echo introduced in r6234
git-svn-id: https://source.openmpt.org/svn/openmpt@6241 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/plugins/DigiBoosterEcho.cpp
@@ -31,6 +31,11 @@ DigiBoosterEcho::DigiBoosterEcho(VSTPluginLib &factory, CSoundFile &sndFile, SND
 //---------------------------------------------------------------------------------------------------
 {
 	RecalculateEchoParams();
+
+	m_mixBuffer.Initialize(2, 2);
+	m_MixState.pOutBufferL = m_mixBuffer.GetInputBuffer(0);
+	m_MixState.pOutBufferR = m_mixBuffer.GetInputBuffer(1);
+
 	InsertIntoFactoryList();
 }
 