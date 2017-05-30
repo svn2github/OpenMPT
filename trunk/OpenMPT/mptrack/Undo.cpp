svn2github/OpenMPT

commit 30845d9c66d0896d2ef0f0aee94d218ae6860d6e

[Imp] Pattern tab: Show undo/redo location in menu entries.
git-svn-id: https://source.openmpt.org/svn/openmpt@8242 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Undo.cpp
@@ -233,25 +233,17 @@ void CPatternUndo::RemoveLastUndoStep()
 }
 
 
-const char *CPatternUndo::GetUndoName() const
-//-------------------------------------------
+CString CPatternUndo::GetName(const undobuf_t &buffer) const
+//----------------------------------------------------------
 {
-	if(!CanUndo())
-	{
-		return "";
-	}
-	return UndoBuffer.back().description;
-}
-
-
-const char *CPatternUndo::GetRedoName() const
-//-------------------------------------------
-{
-	if(!CanRedo())
-	{
-		return "";
-	}
-	return RedoBuffer.back().description;
+	if(buffer.empty())
+		return CString();
+	
+	const UndoInfo &info = buffer.back();
+	if(info.linkToPrevious)
+		return UndoBuffer.back().description + CString(_T(" (Multiple Patterns)"));
+	else
+		return (UndoBuffer.back().description + mpt::String::Print(" (Pat %1 Row %2 Chn %3)", info.pattern, info.firstRow, info.firstChannel + 1)).c_str();
 }
 
 

trunk/OpenMPT/mptrack/Undo.h
@@ -52,6 +52,8 @@ protected:
 	void DeleteStep(undobuf_t &buffer, size_t step);
 	PATTERNINDEX Undo(undobuf_t &fromBuf, undobuf_t &toBuf, bool linkedFromPrevious);
 
+	CString GetName(const undobuf_t &buffer) const;
+
 	bool PrepareBuffer(undobuf_t &buffer, PATTERNINDEX pattern, CHANNELINDEX firstChn, ROWINDEX firstRow, CHANNELINDEX numChns, ROWINDEX numRows, const char *description, bool linkToPrevious, bool storeChannelInfo);
 
 public:
@@ -71,9 +73,9 @@ public:
 	// Remove the latest added undo step from the undo buffer
 	void RemoveLastUndoStep();
 	// Get name of next undo item
-	const char *GetUndoName() const;
+	CString GetUndoName() const { return GetName(UndoBuffer); }
 	// Get name of next redo item
-	const char *GetRedoName() const;
+	CString GetRedoName() const { return GetName(RedoBuffer); }
 
 	CPatternUndo(CModDoc &parent) : modDoc(parent) { }
 

trunk/OpenMPT/mptrack/View_pat.cpp
@@ -2974,8 +2974,7 @@ void CViewPattern::OnUpdateUndo(CCmdUI *pCmdUI)
 	if ((pCmdUI) && (pModDoc))
 	{
 		pCmdUI->Enable(pModDoc->GetPatternUndo().CanUndo());
-		pCmdUI->SetText(CString("Undo ") + CString(pModDoc->GetPatternUndo().GetUndoName())
-			+ CString("\t") + CMainFrame::GetInputHandler()->GetKeyTextFromCommand(kcEditUndo));
+		pCmdUI->SetText(CMainFrame::GetInputHandler()->GetKeyTextFromCommand(kcEditUndo, _T("Undo ") + pModDoc->GetPatternUndo().GetUndoName()));
 	}
 }
 
@@ -2987,8 +2986,7 @@ void CViewPattern::OnUpdateRedo(CCmdUI *pCmdUI)
 	if ((pCmdUI) && (pModDoc))
 	{
 		pCmdUI->Enable(pModDoc->GetPatternUndo().CanRedo());
-		pCmdUI->SetText(CString("Redo ") + CString(pModDoc->GetPatternUndo().GetRedoName())
-			+ CString("\t") + CMainFrame::GetInputHandler()->GetKeyTextFromCommand(kcEditRedo));
+		pCmdUI->SetText(CMainFrame::GetInputHandler()->GetKeyTextFromCommand(kcEditRedo, _T("Redo ") + pModDoc->GetPatternUndo().GetRedoName()));
 	}
 }
 

commit 04ca8f98529f1ef3b67bec8c1dfc1e78571a75c3

[Fix] Crash introduced in r8242.
git-svn-id: https://source.openmpt.org/svn/openmpt@8243 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/Undo.cpp
@@ -241,9 +241,9 @@ CString CPatternUndo::GetName(const undobuf_t &buffer) const
 	
 	const UndoInfo &info = buffer.back();
 	if(info.linkToPrevious)
-		return UndoBuffer.back().description + CString(_T(" (Multiple Patterns)"));
+		return info.description + CString(_T(" (Multiple Patterns)"));
 	else
-		return (UndoBuffer.back().description + mpt::String::Print(" (Pat %1 Row %2 Chn %3)", info.pattern, info.firstRow, info.firstChannel + 1)).c_str();
+		return (info.description + mpt::String::Print(" (Pat %1 Row %2 Chn %3)", info.pattern, info.firstRow, info.firstChannel + 1)).c_str();
 }
 
 