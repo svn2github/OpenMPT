3beab3ef636ae5c789ff5c7031dab01a65bc3fa5
manx <manx@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2018-01-28 15:59:10 +0000

    tag libopenmpt-0.3.5
    git-svn-id: https://source.openmpt.org/svn/openmpt@9540 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ tags/libopenmpt-0.3.5/build/download_and_compile_premake.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $MY_DIR
+
+cd "${MY_DIR}"
+cd ..
+
+
+function download_and_unpack () {
+ set -e
+ MPT_GET_DESTDIR="$1"
+ MPT_GET_URL="$2"
+ MPT_GET_FILE="$3"
+ MPT_GET_SUBDIR="$4"
+ if [ ! -f "build/externals/$3" ]; then
+  wget "$2" -O "build/externals/$3"
+ fi
+ cd include
+  if [ -d "$1" ]; then
+   rm -rf "$1"
+  fi
+  if [ "$4" = "." ]; then
+   mkdir "$1"
+   cd "$1"
+    unzip -x "../../build/externals/$3"
+   cd ..
+  else
+   unzip -x "../build/externals/$3"
+   if [ ! "$4" = "$1" ]; then
+    mv "$4" "$1"
+   fi
+  fi
+ cd ..
+ return 0
+}
+
+if [ ! -d "build/externals" ]; then
+ mkdir build/externals
+fi
+
+
+
+download_and_unpack "genie" "https://github.com/bkaradzic/GENie/archive/b0582481bebe3372eb05504768ff76c45f14607d.zip" "genie-b0582481bebe3372eb05504768ff76c45f14607d.zip" "GENie-b0582481bebe3372eb05504768ff76c45f14607d"
+
+cd include/genie
+
+make
+
+mkdir -p build/vs2015
+./bin/linux/genie --to=../build/vs2015 vs2015
+
+mkdir -p build/vs2017
+./bin/linux/genie --to=../build/vs2017 vs2015
+
+cd ../..
+
+cp -ar include/genie/build/vs* build/genie/genie/build/
+
+
+
+download_and_unpack "premake" "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha12/premake-5.0.0-alpha12-src.zip" "premake-5.0-alpha12-src.zip" "premake-5.0.0-alpha12"
+
+cd include/premake
+
+cd build/gmake.unix
+make
+cd ../..
+echo "Test suite is broken in premake-5.0.0-alpha12."
+#bin/release/premake5 test
---
+++ tags/libopenmpt-0.3.5/build/premake/mpt-OpenMPT.lua
@@ -0,0 +1,218 @@
+
+if layout == "custom" then
+  project "OpenMPT-custom"
+  mpt_projectname = "OpenMPT-custom"
+   uuid "6b9af880-af37-4268-bb91-2b982ff6499a"
+else
+if charset == "Unicode" then
+  project "OpenMPT"
+  mpt_projectname = "OpenMPT"
+   uuid "37FC32A4-8DDC-4A9C-A30C-62989DD8ACE9"
+else
+	project "OpenMPT-ANSI"
+	mpt_projectname = "OpenMPT-ANSI"
+	uuid "ba66db50-e2f0-4c9e-b650-0cca6c66e1c1"
+end
+end
+  language "C++"
+  location ( "../../build/" .. mpt_projectpathname )
+if layout == "custom" then
+  vpaths {
+		["*"] = "../../",
+		["Sources/*"] = "../../**.cpp",
+		["Headers/*"] = "../../**.h",
+	}
+else
+  vpaths { ["*"] = "../../" }
+end
+  dofile "../../build/premake/premake-defaults-EXEGUI.lua"
+  dofile "../../build/premake/premake-defaults.lua"
+if charset == "MBCS" then
+  filter { "configurations:*Shared" }
+   targetname "OpenMPT-ANSI"
+  filter { "not configurations:*Shared" }
+   targetname "mptrack-ANSI"
+else
+  filter { "configurations:*Shared" }
+   targetname "OpenMPT"
+  filter { "not configurations:*Shared" }
+   targetname "mptrack"
+end
+  filter {}
+  local extincludedirs = {
+   "../../include",
+   "../../include/vstsdk2.4",
+   "../../include/ASIOSDK2/common",
+   "../../include/flac/include",
+   "../../include/lame/include",
+   "../../include/lhasa/lib/public",
+   "../../include/mpg123/ports/MSVC++",
+   "../../include/mpg123/src/libmpg123",
+   "../../include/ogg/include",
+   "../../include/opus/include",
+   "../../include/opusenc/include",
+   "../../include/opusfile/include",
+   "../../include/portaudio/include",
+   "../../include/vorbis/include",
+   "../../include/zlib",
+  }
+	filter { "action:vs*" }
+		includedirs ( extincludedirs )
+	filter { "not action:vs*" }
+		sysincludedirs ( extincludedirs )
+	filter {}
+  includedirs {
+   "../../common",
+   "../../soundlib",
+   "$(IntDir)/svn_version",
+   "../../build/svn_version",
+  }
+  files {
+   "../../mptrack/res/OpenMPT.manifest",
+  }
+  files {
+   "../../common/*.cpp",
+   "../../common/*.h",
+   "../../soundbase/*.cpp",
+   "../../soundbase/*.h",
+   "../../soundlib/*.cpp",
+   "../../soundlib/*.h",
+   "../../soundlib/plugins/*.cpp",
+   "../../soundlib/plugins/*.h",
+   "../../soundlib/plugins/dmo/*.cpp",
+   "../../soundlib/plugins/dmo/*.h",
+   "../../sounddsp/*.cpp",
+   "../../sounddsp/*.h",
+   "../../sounddev/*.cpp",
+   "../../sounddev/*.h",
+   "../../unarchiver/*.cpp",
+   "../../unarchiver/*.h",
+   "../../mptrack/*.cpp",
+   "../../mptrack/*.h",
+   "../../mptrack/plugins/*.cpp",
+   "../../mptrack/plugins/*.h",
+   "../../test/*.cpp",
+   "../../test/*.h",
+   "../../pluginBridge/BridgeCommon.h",
+   "../../pluginBridge/BridgeWrapper.cpp",
+   "../../pluginBridge/BridgeWrapper.h",
+  }
+  files {
+   "../../mptrack/mptrack.rc",
+   "../../mptrack/res/*.*", -- resource data files
+  }
+	pchheader "stdafx.h"
+	pchsource "../../common/stdafx.cpp"
+  defines { "MODPLUG_TRACKER" }
+  largeaddressaware ( true )
+  characterset(charset)
+  flags { "MFC" }
+  warnings "Extra"
+  links {
+   "UnRAR",
+   "zlib",
+   "minizip",
+   "smbPitchShift",
+   "lhasa",
+   "flac",
+   "mpg123",
+   "ogg",
+   "opus",
+   "opusenc",
+   "opusfile",
+   "portaudio",
+   "r8brain",
+   "rtmidi",
+   "soundtouch",
+   "vorbis",
+  }
+  filter { "configurations:*Shared" }
+  filter { "not configurations:*Shared" }
+   linkoptions {
+    "/DELAYLOAD:openmpt-mpg123.dll",
+    "/DELAYLOAD:OpenMPT_SoundTouch_f32.dll",
+   }
+  filter {}
+	if not _OPTIONS["xp"] then
+  linkoptions {
+   "/DELAYLOAD:mf.dll",
+   "/DELAYLOAD:mfplat.dll",
+   "/DELAYLOAD:mfreadwrite.dll",
+--   "/DELAYLOAD:mfuuid.dll", -- static library
+   "/DELAYLOAD:propsys.dll",
+  }
+	end
+  filter { "action:vs*" }
+    files {
+      "../../build/vs/debug/openmpt.natvis",
+    }
+  filter {}
+  prebuildcommands { "..\\..\\build\\svn_version\\update_svn_version_vs_premake.cmd $(IntDir)" }
+
+ project "OpenMPT-NativeSupport"
+  uuid "563a631d-fe07-47bc-a98f-9fe5b3ebabfa"
+  language "C++"
+  location ( "../../build/" .. mpt_projectpathname )
+  vpaths { ["*"] = "../../" }
+  mpt_projectname = "OpenMPT-NativeSupport"
+  dofile "../../build/premake/premake-defaults-DLL.lua"
+  dofile "../../build/premake/premake-defaults.lua"
+  includedirs {
+   "../../common",
+   "../../include",
+   "../../include/msinttypes/inttypes",
+   "../../include/ASIOSDK2/common",
+   "../../include/portaudio/include",
+   "$(IntDir)/svn_version",
+   "../../build/svn_version",
+  }
+  files {
+   "../../common/*.cpp",
+   "../../common/*.h",
+   "../../soundbase/*.cpp",
+   "../../soundbase/*.h",
+   "../../sounddev/*.cpp",
+   "../../sounddev/*.h",
+   "../../mptrack/wine/*.cpp",
+   "../../mptrack/wine/*.h",
+  }
+  excludes {
+   "../../mptrack/wine/WineWrapper.cpp",
+  }
+  defines { "MODPLUG_TRACKER", "MPT_BUILD_WINESUPPORT" }
+  largeaddressaware ( true )
+  characterset "Unicode"
+  warnings "Extra"
+  links {
+   "portaudio",
+  }
+  filter {}
+  prebuildcommands { "..\\..\\build\\svn_version\\update_svn_version_vs_premake.cmd $(IntDir)" }
+
+ project "OpenMPT-WineWrapper"
+  uuid "f3da2bf5-e84a-4f71-80ab-884594863d3a"
+  language "C"
+  location ( "../../build/" .. mpt_projectpathname )
+  vpaths { ["*"] = "../../" }
+  mpt_projectname = "OpenMPT-WineWrapper"
+  dofile "../../build/premake/premake-defaults-DLL.lua"
+  dofile "../../build/premake/premake-defaults.lua"
+  includedirs {
+   "../../common",
+   "../../include",
+   "../../include/msinttypes/inttypes",
+   "$(IntDir)/svn_version",
+   "../../build/svn_version",
+  }
+  files {
+   "../../mptrack/wine/WineWrapper.c",
+  }
+  defines { "MODPLUG_TRACKER", "MPT_BUILD_WINESUPPORT_WRAPPER" }
+  largeaddressaware ( true )
+  characterset "Unicode"
+  warnings "Extra"
+  links {
+   "OpenMPT-NativeSupport",
+  }
+  filter {}
+  postbuildcommands { "..\\..\\build\\wine\\build_wine_support.cmd $(IntDir) $(OutDir)" }
