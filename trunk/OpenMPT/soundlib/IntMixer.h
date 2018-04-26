svn2github/OpenMPT

commit 6ae4f177393c429184b648492dac151bcad41722

[Mod] Filter: Bump resonant filter constants precision from 16 to 24 fractional bits. [Fix] Filter: Add 8 extra fractional bits to the resonant filter state variables to avoid weird issues at low cutoff and high mix rates (fixes electroazure movement ii.it when mixed at 88kHz or higher)
git-svn-id: https://source.openmpt.org/svn/openmpt@10083 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/IntMixer.h
@@ -361,8 +361,10 @@ struct ResonantFilter
 		}
 	}
 
+	// To avoid a precision loss especially with quiet samples at low cutoff and high mix rate, we pre-amplify the sample.
+#define MIXING_FILTER_PREAMP 256
 	// Filter values are clipped to double the input range
-#define ClipFilter(x) Clamp<typename Traits::output_t, typename Traits::output_t>(x, int16_min * 2, int16_max * 2)
+#define ClipFilter(x) Clamp<typename Traits::output_t, typename Traits::output_t>(x, int16_min * 2 * MIXING_FILTER_PREAMP, int16_max * 2 * MIXING_FILTER_PREAMP)
 
 	MPT_FORCEINLINE void operator() (typename Traits::outbuf_t &outSample, const ModChannel &chn)
 	{
@@ -371,13 +373,13 @@ struct ResonantFilter
 		for(int i = 0; i < Traits::numChannelsIn; i++)
 		{
 			typename Traits::output_t val = static_cast<typename Traits::output_t>((
-				Util::mul32to64(outSample[i], chn.nFilter_A0) +
+				Util::mul32to64(outSample[i] * MIXING_FILTER_PREAMP, chn.nFilter_A0) +
 				Util::mul32to64(ClipFilter(fy[i][0]), chn.nFilter_B0) +
 				Util::mul32to64(ClipFilter(fy[i][1]), chn.nFilter_B1) +
 				(1 << (MIXING_FILTER_PRECISION - 1))) / (1 << MIXING_FILTER_PRECISION));
 			fy[i][1] = fy[i][0];
 			fy[i][0] = val - (outSample[i] & chn.nFilter_HP);
-			outSample[i] = val;
+			outSample[i] = val / MIXING_FILTER_PREAMP;
 		}
 	}
 

trunk/OpenMPT/soundlib/Mixer.h
@@ -16,7 +16,7 @@ OPENMPT_NAMESPACE_BEGIN
 
 #ifdef MPT_INTMIXER
 typedef int32 mixsample_t;
-enum { MIXING_FILTER_PRECISION = 16 };	// Fixed point resonant filter bits
+enum { MIXING_FILTER_PRECISION = 24 };	// Fixed point resonant filter bits
 #else
 typedef float mixsample_t;
 #endif

commit 205ae03adbfef640461a1efa05080d3920ad869a

[Fix] Highpass filter broke in r10083.
git-svn-id: https://source.openmpt.org/svn/openmpt@10155 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/IntMixer.h
@@ -361,7 +361,7 @@ struct ResonantFilter
 		}
 	}
 
-	// To avoid a precision loss especially with quiet samples at low cutoff and high mix rate, we pre-amplify the sample.
+	// To avoid a precision loss in the state variables especially with quiet samples at low cutoff and high mix rate, we pre-amplify the sample.
 #define MIXING_FILTER_PREAMP 256
 	// Filter values are clipped to double the input range
 #define ClipFilter(x) Clamp<typename Traits::output_t, typename Traits::output_t>(x, int16_min * 2 * MIXING_FILTER_PREAMP, int16_max * 2 * MIXING_FILTER_PREAMP)
@@ -372,13 +372,14 @@ struct ResonantFilter
 
 		for(int i = 0; i < Traits::numChannelsIn; i++)
 		{
+			const auto inputAmp = outSample[i] * MIXING_FILTER_PREAMP;
 			typename Traits::output_t val = static_cast<typename Traits::output_t>(mpt::rshift_signed(
-				Util::mul32to64(outSample[i] * MIXING_FILTER_PREAMP, chn.nFilter_A0) +
+				Util::mul32to64(inputAmp, chn.nFilter_A0) +
 				Util::mul32to64(ClipFilter(fy[i][0]), chn.nFilter_B0) +
 				Util::mul32to64(ClipFilter(fy[i][1]), chn.nFilter_B1) +
 				(1 << (MIXING_FILTER_PRECISION - 1)), MIXING_FILTER_PRECISION));
 			fy[i][1] = fy[i][0];
-			fy[i][0] = val - (outSample[i] & chn.nFilter_HP);
+			fy[i][0] = val - (inputAmp & chn.nFilter_HP);
 			outSample[i] = val / MIXING_FILTER_PREAMP;
 		}
 	}