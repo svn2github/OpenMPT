svn2github/OpenMPT

commit 373dffec154725ae32db03221df05d28308ecbe7

[Ref] Replace almost all accesses of union ModSample::pSample by accessor functions sample16() (int16), sample8() (int8), sampleb() (mpt::byte), samplev() (void). sample16() and sample8() assert the correct sample format, thus some cases which handle that in other ways need to use sampleb() instead. Also make accessor functions const-correct. Also replace most tests of pSample != nullptr by HasSampleMem(). This is a preliminary step to making the sample data use some proper RAII abstraction and exception safety.
git-svn-id: https://source.openmpt.org/svn/openmpt@9769 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/SampleIO.cpp
@@ -126,7 +126,7 @@ size_t SampleIO::ReadSample(ModSample &sample, FileReader &file) const
 			LimitMax(readLength, file.BytesLeft());
 
 			const uint8 *inBuf = mpt::byte_cast<const uint8*>(sourceBuf) + sizeof(compressionTable);
-			int8 *outBuf = sample.pSample8;
+			int8 *outBuf = sample.sample8();
 			int8 delta = 0;
 
 			for(size_t i = readLength; i != 0; i--)
@@ -158,7 +158,7 @@ size_t SampleIO::ReadSample(ModSample &sample, FileReader &file) const
 			LimitMax(sourceSize, mpt::saturate_cast<uint32>(packedDataView.size()));
 			bytesRead += sourceSize;
 
-			AMSUnpack(reinterpret_cast<const int8 *>(packedDataView.data()), packedDataView.size(), sample.pSample, sample.GetSampleSizeInBytes(), packCharacter);
+			AMSUnpack(reinterpret_cast<const int8 *>(packedDataView.data()), packedDataView.size(), sample.samplev(), sample.GetSampleSizeInBytes(), packCharacter);
 		}
 	} else if(GetEncoding() == PTM8Dto16 && GetChannelFormat() == mono && GetBitDepth() == 16)
 	{
@@ -211,11 +211,11 @@ size_t SampleIO::ReadSample(ModSample &sample, FileReader &file) const
 					dlt += hibyte;
 					if(!is16bit)
 					{
-						sample.pSample8[j] = dlt;
+						sample.sample8()[j] = dlt;
 					}
 					else
 					{
-						sample.pSample16[j] = lowbyte | (dlt << 8);
+						sample.sample16()[j] = lowbyte | (dlt << 8);
 					}
 				}
 			} catch(const std::range_error &)
@@ -231,7 +231,7 @@ size_t SampleIO::ReadSample(ModSample &sample, FileReader &file) const
 		{
 			const uint8 *inBuf = mpt::byte_cast<const uint8*>(sourceBuf);
 			const uint8 *inBufMax = inBuf + fileSize;
-			uint8 *outBuf = static_cast<uint8 *>(sample.pSample);
+			uint8 *outBuf = mpt::byte_cast<uint8 *>(sample.sampleb());
 			bytesRead = DMFUnpack(outBuf, inBuf, inBufMax, sample.GetSampleSizeInBytes());
 
 			// This assertion ensures that, when using variable length samples,
@@ -324,7 +324,7 @@ size_t SampleIO::ReadSample(ModSample &sample, FileReader &file) const
 		bytesRead = readLength;
 
 		const uint8 *inBuf = mpt::byte_cast<const uint8*>(sourceBuf);
-		int16 *outBuf = sample.pSample16;
+		int16 *outBuf = sample.sample16();
 
 		while(readLength--)
 		{
@@ -381,7 +381,7 @@ size_t SampleIO::ReadSample(ModSample &sample, FileReader &file) const
 			bytesRead = CopyStereoSplitSample<SC::DecodeInt8Delta>(sample, sourceBuf, fileSize);
 			for(SmpLength i = 0; i < sample.nLength * 2; i += 2)
 			{
-				sample.pSample8[i + 1] = static_cast<int8>(static_cast<uint8>(sample.pSample8[i + 1]) + static_cast<uint8>(sample.pSample8[i]));
+				sample.sample8()[i + 1] = static_cast<int8>(static_cast<uint8>(sample.sample8()[i + 1]) + static_cast<uint8>(sample.sample8()[i]));
 			}
 			break;
 		default:
@@ -473,7 +473,7 @@ size_t SampleIO::ReadSample(ModSample &sample, FileReader &file) const
 			bytesRead = CopyStereoSplitSample<SC::DecodeInt16Delta<littleEndian16> >(sample, sourceBuf, fileSize);
 			for(SmpLength i = 0; i < sample.nLength * 2; i += 2)
 			{
-				sample.pSample16[i + 1] = static_cast<int16>(static_cast<uint16>(sample.pSample16[i + 1]) + static_cast<uint16>(sample.pSample16[i]));
+				sample.sample16()[i + 1] = static_cast<int16>(static_cast<uint16>(sample.sample16()[i + 1]) + static_cast<uint16>(sample.sample16()[i]));
 			}
 			break;
 		default:
@@ -869,9 +869,6 @@ size_t SampleIO::WriteSample(std::ostream *f, const ModSample &sample, SmpLength
 		int8 buffer8[8192];
 		int16 buffer16[4096];
 	};
-	const void *const pSampleVoid = sample.pSample;
-	const int8 *const pSample8 = sample.pSample8;
-	const int16 *const pSample16 = sample.pSample16;
 	SmpLength numSamples = sample.nLength;
 
 	if(maxSamples && numSamples > maxSamples) numSamples = maxSamples;
@@ -883,6 +880,7 @@ size_t SampleIO::WriteSample(std::ostream *f, const ModSample &sample, SmpLength
 	{
 		// 16-bit little-endian mono samples
 		MPT_ASSERT(len == numSamples * 2);
+		const int16 *const pSample16 = sample.sample16();
 		if(!f) return len;
 		const int16 *p = pSample16;
 		int s_old = 0;
@@ -920,6 +918,7 @@ size_t SampleIO::WriteSample(std::ostream *f, const ModSample &sample, SmpLength
 	{
 		// 8-bit Stereo samples (not interleaved)
 		MPT_ASSERT(len == numSamples * 2);
+		const int8 *const pSample8 = sample.sample8();
 		if(!f) return len;
 		const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
 		for (uint32 iCh=0; iCh<2; iCh++)
@@ -955,6 +954,7 @@ size_t SampleIO::WriteSample(std::ostream *f, const ModSample &sample, SmpLength
 	{
 		// 16-bit little-endian Stereo samples (not interleaved)
 		MPT_ASSERT(len == numSamples * 4);
+		const int16 *const pSample16 = sample.sample16();
 		if(!f) return len;
 		const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x8000 : 0;
 		for (uint32 iCh=0; iCh<2; iCh++)
@@ -989,6 +989,7 @@ size_t SampleIO::WriteSample(std::ostream *f, const ModSample &sample, SmpLength
 	else if((GetBitDepth() == 8 || (GetBitDepth() == 16 && GetEndianness() == GetNativeEndianness())) && GetChannelFormat() == stereoInterleaved && GetEncoding() == signedPCM)
 	{
 		// Stereo signed interleaved
+		const void *const pSampleVoid = sample.samplev();
 		if(f) mpt::IO::WriteRaw(*f, mpt::void_cast<const mpt::byte*>(pSampleVoid), len);
 	}
 
@@ -996,6 +997,7 @@ size_t SampleIO::WriteSample(std::ostream *f, const ModSample &sample, SmpLength
 	{
 		// Stereo unsigned interleaved
 		MPT_ASSERT(len == numSamples * 2);
+		const int8 *const pSample8 = sample.sample8();
 		if(!f) return len;
 		for(SmpLength j = 0; j < len; j++)
 		{
@@ -1022,6 +1024,7 @@ size_t SampleIO::WriteSample(std::ostream *f, const ModSample &sample, SmpLength
 	{
 		MPT_ASSERT(GetBitDepth() == 8);
 		MPT_ASSERT(len == numSamples);
+		const int8 *const pSample8 = sample.sample8();
 		if(!f) return len;
 		const int8 *p = pSample8;
 		int sinc = sample.GetElementarySampleSize();

trunk/OpenMPT/mptrack/Autotune.cpp
@@ -126,11 +126,11 @@ bool Autotune::PrepareSample(SmpLength maxShift)
 	switch(sample.GetElementarySampleSize())
 	{
 	case 1:
-		CopySamples(sample.pSample8 + sampleOffset * sample.GetNumChannels(), sampleLoopStart, sampleLoopEnd);
+		CopySamples(sample.sample8() + sampleOffset * sample.GetNumChannels(), sampleLoopStart, sampleLoopEnd);
 		return true;
 
 	case 2:
-		CopySamples(sample.pSample16 + sampleOffset * sample.GetNumChannels(), sampleLoopStart, sampleLoopEnd);
+		CopySamples(sample.sample16() + sampleOffset * sample.GetNumChannels(), sampleLoopStart, sampleLoopEnd);
 		return true;
 	}
 
@@ -141,7 +141,7 @@ bool Autotune::PrepareSample(SmpLength maxShift)
 
 bool Autotune::CanApply() const
 {
-	return (sample.pSample != nullptr && sample.nLength >= MIN_SAMPLE_LENGTH);
+	return (sample.HasSampleMem() && sample.nLength >= MIN_SAMPLE_LENGTH);
 }
 
 

trunk/OpenMPT/mptrack/CleanupSong.cpp
@@ -512,7 +512,7 @@ bool CModCleanupDlg::RemoveUnusedSamples()
 
 	// Check if any samples are not referenced in the patterns (sample mode) or by an instrument (instrument mode).
 	// This doesn't check yet if a sample is referenced by an instrument, but actually unused in the patterns.
-	for(SAMPLEINDEX smp = 1; smp <= sndFile.GetNumSamples(); smp++) if (sndFile.GetSample(smp).pSample)
+	for(SAMPLEINDEX smp = 1; smp <= sndFile.GetNumSamples(); smp++) if (sndFile.GetSample(smp).HasSampleMem())
 	{
 		if(!modDoc.IsSampleUsed(smp))
 		{
@@ -591,10 +591,10 @@ bool CModCleanupDlg::OptimizeSamples()
 			bool identicalChannels = false;
 			if(sample.GetElementarySampleSize() == 1)
 			{
-				identicalChannels = ComapreStereoChannels(loopLength, sample.pSample8);
+				identicalChannels = ComapreStereoChannels(loopLength, sample.sample8());
 			} else if(sample.GetElementarySampleSize() == 2)
 			{
-				identicalChannels = ComapreStereoChannels(loopLength, sample.pSample16);
+				identicalChannels = ComapreStereoChannels(loopLength, sample.sample16());
 			}
 			if(identicalChannels)
 			{
@@ -603,7 +603,7 @@ bool CModCleanupDlg::OptimizeSamples()
 			}
 		}
 
-		if(sample.pSample && sample.nLength > loopLength + 2) numLoopOpt++;
+		if(sample.HasSampleMem() && sample.nLength > loopLength + 2) numLoopOpt++;
 	}
 	if(!numLoopOpt && !numStereoOpt) return false;
 
@@ -678,7 +678,7 @@ bool CModCleanupDlg::RearrangeSamples()
 	// First, find out which sample slots are unused and create the new sample map only with used samples
 	for(SAMPLEINDEX i = 1; i <= sndFile.GetNumSamples(); i++)
 	{
-		if(sndFile.GetSample(i).pSample != nullptr)
+		if(sndFile.GetSample(i).HasSampleMem())
 		{
 			sampleMap.push_back(i);
 		}

trunk/OpenMPT/mptrack/Ctrl_ins.cpp
@@ -2450,7 +2450,7 @@ void CCtrlInstruments::OnMixPlugChanged()
 						bool hasSamples = false;
 						for(auto sample : referencedSamples)
 						{
-							if(sample > 0 && sample <= m_sndFile.GetNumSamples() && m_sndFile.GetSample(sample).pSample != nullptr)
+							if(sample > 0 && sample <= m_sndFile.GetNumSamples() && m_sndFile.GetSample(sample).HasSampleMem())
 							{
 								hasSamples = true;
 								break;

trunk/OpenMPT/mptrack/Ctrl_pat.cpp
@@ -303,7 +303,7 @@ void CCtrlPatterns::UpdateView(UpdateHint hint, CObject *pObj)
 			{
 				// Show sample names
 				SAMPLEINDEX nmax = m_sndFile.GetNumSamples();
-				for(SAMPLEINDEX i = 1; i <= nmax; i++) if (m_sndFile.GetSample(i).pSample)
+				for(SAMPLEINDEX i = 1; i <= nmax; i++) if (m_sndFile.GetSample(i).HasSampleMem())
 				{
 					if (m_modDoc.GetSplitKeyboardSettings().IsSplitActive())
 						s.Format(szSplitFormat,

trunk/OpenMPT/mptrack/Ctrl_smp.cpp
@@ -1318,7 +1318,7 @@ void CCtrlSamples::SaveSample(bool doBatchSave)
 	{
 		// Save this sample
 		const ModSample &sample = m_sndFile.GetSample(m_nSample);
-		if((!m_nSample) || (sample.pSample == nullptr))
+		if((!m_nSample) || (!sample.HasSampleMem()))
 		{
 			SwitchToView();
 			return;
@@ -1393,7 +1393,7 @@ void CCtrlSamples::SaveSample(bool doBatchSave)
 	for(SAMPLEINDEX smp = minSmp; smp <= maxSmp; smp++)
 	{
 		ModSample &sample = m_sndFile.GetSample(smp);
-		if (sample.pSample)
+		if (sample.HasSampleMem())
 		{
 			fileName = dlg.GetFirstFile();
 			if(doBatchSave)
@@ -1507,7 +1507,7 @@ void CCtrlSamples::Normalize(bool allSamples)
 
 	for(SAMPLEINDEX smp = minSample; smp <= maxSample; smp++)
 	{
-		if(m_sndFile.GetSample(smp).pSample)
+		if(m_sndFile.GetSample(smp).HasSampleMem())
 		{
 			ModSample &sample = m_sndFile.GetSample(smp);
 
@@ -1535,10 +1535,10 @@ void CCtrlSamples::Normalize(bool allSamples)
 
 			if(sample.uFlags[CHN_16BIT])
 			{
-				modified |= DoNormalize(sample.pSample16, selStart, selEnd);
+				modified |= DoNormalize(sample.sample16(), selStart, selEnd);
 			} else
 			{
-				modified |= DoNormalize(sample.pSample8, selStart, selEnd);
+				modified |= DoNormalize(sample.sample8(), selStart, selEnd);
 			}
 
 			if(modified)
@@ -1587,7 +1587,7 @@ void CCtrlSamples::RemoveDCOffset(bool allSamples)
 	{
 		SmpLength selStart, selEnd;
 
-		if(m_sndFile.GetSample(smp).pSample == nullptr)
+		if(!m_sndFile.GetSample(smp).HasSampleMem())
 			continue;
 
 		if (minSample != maxSample)
@@ -1685,7 +1685,7 @@ static void ApplyAmplifyImpl(T * MPT_RESTRICT pSample, SmpLength start, SmpLengt
 
 void CCtrlSamples::ApplyAmplify(int32 lAmp, int32 fadeIn, int32 fadeOut, Fade::Law fadeLaw)
 {
-	if((!m_sndFile.GetSample(m_nSample).pSample)) return;
+	if(!m_sndFile.GetSample(m_nSample).HasSampleMem()) return;
 
 	BeginWaitCursor();
 	ModSample &sample = m_sndFile.GetSample(m_nSample);
@@ -1698,10 +1698,10 @@ void CCtrlSamples::ApplyAmplify(int32 lAmp, int32 fadeIn, int32 fadeOut, Fade::L
 	selection.nEnd *= sample.GetNumChannels();
 	if (sample.uFlags[CHN_16BIT])
 	{
-		ApplyAmplifyImpl(sample.pSample16, selection.nStart, selection.nEnd, lAmp, fadeIn, fadeOut, fadeLaw);
+		ApplyAmplifyImpl(sample.sample16(), selection.nStart, selection.nEnd, lAmp, fadeIn, fadeOut, fadeLaw);
 	} else
 	{
-		ApplyAmplifyImpl(sample.pSample8, selection.nStart, selection.nEnd, lAmp, fadeIn, fadeOut, fadeLaw);
+		ApplyAmplifyImpl(sample.sample8(), selection.nStart, selection.nEnd, lAmp, fadeIn, fadeOut, fadeLaw);
 	}
 	sample.PrecomputeLoops(m_sndFile, false);
 	SetModified(SampleHint().Data(), false, true);
@@ -1725,7 +1725,7 @@ void CCtrlSamples::OnAmplify()
 // Fade-Out is applied if the selection ends and the end of the sample.
 void CCtrlSamples::OnQuickFade()
 {
-	if((m_sndFile.GetSample(m_nSample).pSample == nullptr)) return;
+	if(!m_sndFile.GetSample(m_nSample).HasSampleMem()) return;
 
 	SampleSelectionPoints sel = GetSelectionPoints();
 	if(sel.selectionActive && (sel.nStart == 0 || sel.nEnd == m_sndFile.GetSample(m_nSample).nLength))
@@ -1742,7 +1742,7 @@ void CCtrlSamples::OnQuickFade()
 void CCtrlSamples::OnResample()
 {
 	ModSample &sample = m_sndFile.GetSample(m_nSample);
-	if(sample.pSample == nullptr) return;
+	if(!sample.HasSampleMem()) return;
 
 	const uint32 oldRate = sample.GetSampleRate(m_sndFile.GetType());
 	CResamplingDlg dlg(this, oldRate, TrackerSettings::Instance().sampleEditorDefaultResampler);
@@ -1760,7 +1760,7 @@ void CCtrlSamples::ApplyResample(uint32_t newRate, ResamplingMode mode)
 	BeginWaitCursor();
 
 	ModSample &sample = m_sndFile.GetSample(m_nSample);
-	if(sample.pSample == nullptr) return;
+	if(!sample.HasSampleMem()) return;
 
 	SampleSelectionPoints selection = GetSelectionPoints();
 	LimitMax(selection.nEnd, sample.nLength);
@@ -1789,8 +1789,8 @@ void CCtrlSamples::ApplyResample(uint32_t newRate, ResamplingMode mode)
 	{
 		// First, copy parts of the sample that are not affected by partial upsampling
 		const SmpLength bps = sample.GetBytesPerSample();
-		std::memcpy(newSample, sample.pSample, selection.nStart * bps);
-		std::memcpy(static_cast<char *>(newSample) + newSelEnd * bps, static_cast<char *>(sample.pSample) + selection.nEnd * bps, (sample.nLength - selection.nEnd) * bps);
+		std::memcpy(newSample, sample.sampleb(), selection.nStart * bps);
+		std::memcpy(static_cast<char *>(newSample) + newSelEnd * bps, sample.sampleb() + selection.nEnd * bps, (sample.nLength - selection.nEnd) * bps);
 
 		if(mode == SRCMODE_DEFAULT)
 		{
@@ -1816,12 +1816,12 @@ void CCtrlSamples::ApplyResample(uint32_t newRate, ResamplingMode mode)
 					switch(sample.GetElementarySampleSize())
 					{
 					case 1:
-						firstVal = SC::Convert<double, int8>()(sample.pSample8[readOffset]);
-						lastVal = SC::Convert<double, int8>()(sample.pSample8[readOffset + selLength - numChannels]);
+						firstVal = SC::Convert<double, int8>()(sample.sample8()[readOffset]);
+						lastVal = SC::Convert<double, int8>()(sample.sample8()[readOffset + selLength - numChannels]);
 						break;
 					case 2:
-						firstVal = SC::Convert<double, int16>()(sample.pSample16[readOffset]);
-						lastVal = SC::Convert<double, int16>()(sample.pSample16[readOffset + selLength - numChannels]);
+						firstVal = SC::Convert<double, int16>()(sample.sample16()[readOffset]);
+						lastVal = SC::Convert<double, int16>()(sample.sample16()[readOffset + selLength - numChannels]);
 						break;
 					default:
 						// When higher bit depth is added, feel free to also replace CDSPResampler16 by CDSPResampler24 above.
@@ -1854,10 +1854,10 @@ void CCtrlSamples::ApplyResample(uint32_t newRate, ResamplingMode mode)
 						switch(sample.GetElementarySampleSize())
 						{
 						case 1:
-							CopySample<SC::ConversionChain<SC::Convert<double, int8>, SC::DecodeIdentity<int8> > >(convBuffer.data(), smpCount, 1, sample.pSample8 + readOffset, sample.GetSampleSizeInBytes(), sample.GetNumChannels());
+							CopySample<SC::ConversionChain<SC::Convert<double, int8>, SC::DecodeIdentity<int8> > >(convBuffer.data(), smpCount, 1, sample.sample8() + readOffset, sample.GetSampleSizeInBytes(), sample.GetNumChannels());
 							break;
 						case 2:
-							CopySample<SC::ConversionChain<SC::Convert<double, int16>, SC::DecodeIdentity<int16> > >(convBuffer.data(), smpCount, 1, sample.pSample16 + readOffset, sample.GetSampleSizeInBytes(), sample.GetNumChannels());
+							CopySample<SC::ConversionChain<SC::Convert<double, int16>, SC::DecodeIdentity<int16> > >(convBuffer.data(), smpCount, 1, sample.sample16() + readOffset, sample.GetSampleSizeInBytes(), sample.GetNumChannels());
 							break;
 						}
 						readOffset += smpCount * numChannels;
@@ -1893,7 +1893,7 @@ void CCtrlSamples::ApplyResample(uint32_t newRate, ResamplingMode mode)
 			if(sample.uFlags[CHN_16BIT]) functionNdx |= MixFuncTable::ndx16Bit;
 			if(sample.uFlags[CHN_STEREO]) functionNdx |= MixFuncTable::ndxStereo;
 			ModChannel chn{};
-			chn.pCurrentSample = sample.pSample;
+			chn.pCurrentSample = sample.samplev();
 			chn.increment = SamplePosition::Ratio(oldRate, newRate);
 			chn.position.Set(selection.nStart);
 			chn.leftVol = chn.rightVol = (1 << 8);
@@ -2010,7 +2010,7 @@ void CCtrlSamples::OnEnableStretchToSize()
 
 void CCtrlSamples::OnEstimateSampleSize()
 {
-	if((m_sndFile.GetSample(m_nSample).pSample == nullptr)) return;
+	if(!m_sndFile.GetSample(m_nSample).HasSampleMem()) return;
 
 	//rewbs.timeStretchMods
 	//Ensure m_dTimeStretchRatio is up-to-date with textbox content
@@ -2239,10 +2239,10 @@ public:
 			switch(smpsize)
 			{
 			case 1:
-				CopyInterleavedSampleStreams(buffer.data(), sample.pSample8 + inPos * nChn, inChunkSize, nChn, conv8f32);
+				CopyInterleavedSampleStreams(buffer.data(), sample.sample8() + inPos * nChn, inChunkSize, nChn, conv8f32);
 				break;
 			case 2:
-				CopyInterleavedSampleStreams(buffer.data(), sample.pSample16 + inPos * nChn, inChunkSize, nChn, convf32);
+				CopyInterleavedSampleStreams(buffer.data(), sample.sample16() + inPos * nChn, inChunkSize, nChn, convf32);
 				break;
 			}
 			soundtouch_putSamples(handleSt, buffer.data(), inChunkSize);
@@ -2426,7 +2426,7 @@ public:
 				}
 
 				// Convert current channel's data chunk to float
-				int8 *ptr = sample.pSample8 + pos * smpsize * nChn + i * smpsize;
+				int8 *ptr = sample.sample8() + pos * smpsize * nChn + i * smpsize;
 
 				switch(smpsize)
 				{
@@ -2587,7 +2587,7 @@ void CCtrlSamples::OnInvert()
 
 void CCtrlSamples::OnSignUnSign()
 {
-	if((m_sndFile.GetSample(m_nSample).pSample == nullptr)) return;
+	if(!m_sndFile.GetSample(m_nSample).HasSampleMem()) return;
 
 	if(m_modDoc.IsNotePlaying(0, m_nSample, 0))
 		MsgBoxHidable(ConfirmSignUnsignWhenPlaying);
@@ -2611,7 +2611,7 @@ void CCtrlSamples::OnSignUnSign()
 
 void CCtrlSamples::OnSilence()
 {
-	if((m_sndFile.GetSample(m_nSample).pSample == nullptr)) return;
+	if(!m_sndFile.GetSample(m_nSample).HasSampleMem()) return;
 	BeginWaitCursor();
 	SampleSelectionPoints selection = GetSelectionPoints();
 
@@ -3070,7 +3070,7 @@ void CCtrlSamples::OnVScroll(UINT nCode, UINT, CScrollBar *scrollBar)
 	TCHAR s[256];
 	if(IsLocked()) return;
 	ModSample &sample = m_sndFile.GetSample(m_nSample);
-	const uint8 *pSample = static_cast<const uint8 *>(sample.pSample);
+	const uint8 *pSample = mpt::byte_cast<const uint8 *>(sample.sampleb());
 	const uint32 inc = sample.GetBytesPerSample();
 	SmpLength i;
 	int pos;
@@ -3407,7 +3407,7 @@ void CCtrlSamples::OnXFade()
 {
 	ModSample &sample = m_sndFile.GetSample(m_nSample);
 
-	if(sample.pSample == nullptr || sample.nLength == 0)
+	if(!sample.HasSampleMem() || sample.nLength == 0)
 	{
 		MessageBeep(MB_ICONWARNING);
 		SwitchToView();
@@ -3457,7 +3457,7 @@ void CCtrlSamples::OnStereoSeparation()
 {
 	ModSample &sample = m_sndFile.GetSample(m_nSample);
 
-	if(sample.pSample == nullptr
+	if(!sample.HasSampleMem()
 		|| !sample.nLength
 		|| sample.GetNumChannels() != 2)
 	{

trunk/OpenMPT/mptrack/Draw_pat.cpp
@@ -1118,7 +1118,7 @@ void CViewPattern::DrawChannelVUMeter(HDC hdc, int x, int y, UINT nChn)
 		if (vur > 8) vur = 8;
 		x += (m_szCell.cx / 2);
 		auto &channel = GetSoundFile()->m_PlayState.Chn[nChn];
-		auto bmp = ((channel.pModSample != nullptr && channel.pModSample->pSample != nullptr) || !channel.HasMIDIOutput()) ? CMainFrame::bmpVUMeters : CMainFrame::bmpPluginVUMeters;
+		auto bmp = ((channel.pModSample != nullptr && channel.pModSample->HasSampleMem()) || !channel.HasMIDIOutput()) ? CMainFrame::bmpVUMeters : CMainFrame::bmpPluginVUMeters;
 		if (m_nDetailLevel <= PatternCursor::instrColumn)
 		{
 			DibBlt(hdc, x-VUMETERS_LOWIDTH-1, y, VUMETERS_LOWIDTH, VUMETERS_BMPHEIGHT,

trunk/OpenMPT/mptrack/MainFrm.cpp
@@ -959,7 +959,7 @@ bool CMainFrame::DoNotification(DWORD dwSamplesRead, int64 streamPosition)
 	{
 		// Sample positions
 		const SAMPLEINDEX smp = notifyItem;
-		if(smp > 0 && smp <= m_pSndFile->GetNumSamples() && m_pSndFile->GetSample(smp).pSample != nullptr)
+		if(smp > 0 && smp <= m_pSndFile->GetNumSamples() && m_pSndFile->GetSample(smp).HasSampleMem())
 		{
 			for(CHANNELINDEX k = 0; k < MAX_CHANNELS; k++)
 			{

commit 7ddf8db155339f88fdc5b357e12fb799af92f187

[Fix] SampleIO: Due to assertion in ModSample::sample8(), conversion to 8bit output failed in WriteSample() when the source sample was 16bit. Split into 2 separate loops depending on source sample type. (broken since r9769).
git-svn-id: https://source.openmpt.org/svn/openmpt@10290 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/SampleIO.cpp
@@ -1008,31 +1008,53 @@ size_t SampleIO::WriteSample(std::ostream &f, const ModSample &sample, SmpLength
 	{
 		MPT_ASSERT(GetBitDepth() == 8);
 		MPT_ASSERT(len == numSamples);
-		const int8 *const pSample8 = sample.sample8();
-		const int8 *p = pSample8;
-		int sinc = sample.GetElementarySampleSize();
-		int s_old = 0;
-		const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
-		MPT_MAYBE_CONSTANT_IF(mpt::endian_is_little())
+		if(sample.uFlags[CHN_16BIT])
 		{
-			if (sample.uFlags[CHN_16BIT]) p++;
-		}
-		for (SmpLength j = 0; j < numSamples; j++)
-		{
-			int s_new = *p;
-			p += sinc;
-			if (sample.uFlags[CHN_STEREO])
+			const int16 *const pSample16 = sample.sample16();
+			const int16 *p = pSample16;
+			int s_old = 0;
+			const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
+			for(SmpLength j = 0; j < numSamples; j++)
 			{
-				s_new = (s_new + (static_cast<int>(*p)) + 1) / 2;
-				p += sinc;
+				int s_new = mpt::rshift_signed(*p, 8);
+				p++;
+				if(sample.uFlags[CHN_STEREO])
+				{
+					s_new = (s_new + (static_cast<int>(*p)) + 1) / 2;
+					p++;
+				}
+				if(GetEncoding() == deltaPCM)
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new - s_old));
+					s_old = s_new;
+				} else
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new + s_ofs));
+				}
 			}
-			if (GetEncoding() == deltaPCM)
+		} else
+		{
+			const int8 *const pSample8 = sample.sample8();
+			const int8 *p = pSample8;
+			int s_old = 0;
+			const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
+			for(SmpLength j = 0; j < numSamples; j++)
 			{
-				mpt::IO::Write(fb, static_cast<int8>(s_new - s_old));
-				s_old = s_new;
-			} else
-			{
-				mpt::IO::Write(fb, static_cast<int8>(s_new + s_ofs));
+				int s_new = *p;
+				p++;
+				if(sample.uFlags[CHN_STEREO])
+				{
+					s_new = (s_new + (static_cast<int>(*p)) + 1) / 2;
+					p++;
+				}
+				if(GetEncoding() == deltaPCM)
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new - s_old));
+					s_old = s_new;
+				} else
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new + s_ofs));
+				}
 			}
 		}
 	}