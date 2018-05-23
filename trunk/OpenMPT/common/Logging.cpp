svn2github/OpenMPT

commit 853c069d6ae28e1fbefd722e770c65d574344fdb

[Ref] Replace all FILE* by mpt::ifstream or mpt::ofstream, and disable all C stdio helper code.
git-svn-id: https://source.openmpt.org/svn/openmpt@10283 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/Logging.cpp
@@ -125,22 +125,22 @@ void Logger::SendLogMessage(const Context &context, LogLevel level, const char *
 #endif
 		if(mpt::log::FileEnabled)
 		{
-			static FILE * s_logfile = nullptr;
+			static mpt::ofstream s_logfile;
 			if(!s_logfile)
 			{
-				s_logfile = mpt_fopen(MPT_PATHSTRING("mptrack.log"), "a");
+				s_logfile = s_logfile.open(MPT_PATHSTRING("mptrack.log"), std::ios::app);
 			}
 			if(s_logfile)
 			{
-				fprintf(s_logfile, mpt::ToCharset(mpt::CharsetUTF8, mpt::format(MPT_USTRING("%1+%2 %3(%4): %5 [%6]\n"))
+				mpt::IO::WriteText(s_logfile, mpt::ToCharset(mpt::CharsetUTF8, mpt::format(MPT_USTRING("%1+%2 %3(%4): %5 [%6]\n"))
 					( mpt::Date::ANSI::ToUString(cur)
 					, mpt::ufmt::dec<6>(diff)
 					, file
 					, line
 					, message
 					, function
-					)).c_str());
-				fflush(s_logfile);
+					)));
+				mpt::IO::Flush(s_logfile);
 			}
 		}
 		if(mpt::log::DebuggerEnabled)

trunk/OpenMPT/common/BuildSettings.h
@@ -552,10 +552,6 @@
 #define MPT_ENABLE_FILEIO // External samples require disk file io
 #endif
 
-#if !defined(MODPLUG_NO_FILESAVE) && !defined(MPT_ENABLE_FILEIO_STDIO)
-#define MPT_ENABLE_FILEIO_STDIO // file saving requires FILE*
-#endif
-
 #if defined(NO_PLUGINS)
 // Any plugin type requires NO_PLUGINS to not be defined.
 #define NO_VST

trunk/OpenMPT/common/FileReader.cpp
@@ -41,7 +41,7 @@ OnDiskFileWrapper::OnDiskFileWrapper(FileReader &file, const mpt::PathString &fi
 
 #ifdef MPT_ONDISKFILEWRAPPER_NO_CREATEFILE
 
-			FILE * f = mpt_fopen(tempName, "wb");
+			mpt::ofstream f(tempName, std::ios::binary);
 			if(!f)
 			{
 				throw std::runtime_error("");
@@ -58,16 +58,13 @@ OnDiskFileWrapper::OnDiskFileWrapper(FileReader &file, const mpt::PathString &fi
 					chunkOk = mpt::IO::WriteRaw(f, mpt::const_byte_span(view.data() + written, chunkSize));
 					if(!chunkOk)
 					{
-						fclose(f);
-						f = NULL;
 						throw std::runtime_error("");
 					}
 					towrite -= chunkSize;
 					written += chunkSize;
 				} while(towrite > 0);
 			}
-			fclose(f);
-			f = NULL;
+			f.close();
 
 #else // !MPT_ONDISKFILEWRAPPER_NO_CREATEFILE
 

trunk/OpenMPT/mptrack/MIDIMapping.cpp
@@ -20,7 +20,7 @@
 OPENMPT_NAMESPACE_BEGIN
 
 
-size_t CMIDIMapper::Serialize(FILE *f) const
+size_t CMIDIMapper::Serialize(std::ostream *file) const
 {
 	//Bytes: 1 Flags, 2 key, 1 plugindex, 1,2,4,8 plug/etc.
 	size_t size = 0;
@@ -45,8 +45,9 @@ size_t CMIDIMapper::Serialize(FILE *f) const
 		}
 		else temp8 |= (2 << 6);
 
-		if(f)
+		if(file)
 		{
+			std::ostream & f = *file;
 			mpt::IO::WriteIntLE<uint8>(f, temp8);
 			mpt::IO::WriteIntLE<uint16>(f, temp16);
 			mpt::IO::WriteIntLE<uint8>(f, d.GetPlugIndex());

trunk/OpenMPT/mptrack/MIDIMapping.h
@@ -106,7 +106,7 @@ public:
 	size_t GetCount() const {return m_Directives.size();}
 
 	// Serialize to file, or just return the serialization size if no file handle is provided.
-	size_t Serialize(FILE *f = nullptr) const;
+	size_t Serialize(std::ostream *file = nullptr) const;
 	// Deserialize MIDI Mappings from file. Returns true if no errors were encountered.
 	bool Deserialize(FileReader &file);
 

trunk/OpenMPT/mptrack/Modedit.cpp
@@ -1107,14 +1107,14 @@ bool CModDoc::SaveEnvelope(INSTRUMENTINDEX nIns, EnvelopeType nEnv, const mpt::P
 	CStringA s;
 	EnvelopeToString(s, pIns->GetEnvelope(nEnv));
 
-	FILE *f = mpt_fopen(fileName, "wb");
-	if(f == nullptr)
+	mpt::ofstream f(fileName, std::ios::binary);
+	if(!f)
 	{
 		EndWaitCursor();
 		return false;
 	}
 	mpt::IO::WriteText(f, std::string(s.GetString(), s.GetString() + s.GetLength()));
-	fclose(f);
+	f.close();
 	EndWaitCursor();
 	return true;
 }

trunk/OpenMPT/soundlib/Dlsbank.cpp
@@ -512,9 +512,12 @@ CDLSBank::CDLSBank()
 bool CDLSBank::IsDLSBank(const mpt::PathString &filename)
 {
 	RIFFCHUNKID riff;
-	FILE *f;
 	if(filename.empty()) return false;
-	if((f = mpt_fopen(filename, "rb")) == nullptr) return false;
+	mpt::ifstream f(filename, std::ios::binary);
+	if(!f)
+	{
+		return false;
+	}
 	MemsetZero(riff);
 	mpt::IO::Read(f, riff);
 	// Check for embedded DLS sections
@@ -549,7 +552,6 @@ bool CDLSBank::IsDLSBank(const mpt::PathString &filename)
 			if ((len <= 4) || !mpt::IO::SeekRelative(f, len-4)) break;
 		}
 	}
-	fclose(f);
 	return ((riff.id_RIFF == IFFID_RIFF)
 		&& ((riff.id_DLS == IFFID_DLS) || (riff.id_DLS == IFFID_MLS) || (riff.id_DLS == IFFID_sfbk))
 		&& (riff.riff_len >= 256));
@@ -1441,8 +1443,11 @@ bool CDLSBank::ExtractWaveForm(uint32 nIns, uint32 nRgn, std::vector<uint8> &wav
 	}
 
 	long dwOffset = mpt::saturate_cast<long>(m_WaveForms[nWaveLink] + m_dwWavePoolOffset);
-	FILE *f = mpt_fopen(m_szFileName, "rb");
-	if(f == nullptr) return false;
+	mpt::ifstream f(m_szFileName, std::ios::binary);
+	if(!f)
+	{
+		return false;
+	}
 	if (mpt::IO::SeekAbsolute(f, dwOffset))
 	{
 		if (m_nType & SOUNDBANK_TYPE_SF2)
@@ -1483,7 +1488,6 @@ bool CDLSBank::ExtractWaveForm(uint32 nIns, uint32 nRgn, std::vector<uint8> &wav
 			}
 		}
 	}
-	fclose(f);
 	return !waveData.empty();
 }
 

trunk/OpenMPT/soundlib/InstrumentExtensions.cpp
@@ -26,7 +26,7 @@ MODULAR (in/out) ModInstrument :
 
 - both following functions need to be updated when adding a new member in ModInstrument :
 
-void WriteInstrumentHeaderStructOrField(ModInstrument * input, FILE * file, uint32 only_this_code, int16 fixedsize);
+void WriteInstrumentHeaderStructOrField(ModInstrument * input, std::ostream &file, uint32 only_this_code, int16 fixedsize);
 bool ReadInstrumentHeaderField(ModInstrument * input, uint32 fcode, int16 fsize, FileReader &file);
 
 - see below for body declaration.
@@ -285,7 +285,7 @@ bool IsNegative(const T &val)
 
 
 // Write (in 'file') 'input' ModInstrument with 'code' & 'size' extra field infos for each member
-void WriteInstrumentHeaderStructOrField(ModInstrument * input, FILE * file, uint32 only_this_code, uint16 fixedsize)
+void WriteInstrumentHeaderStructOrField(ModInstrument * input, std::ostream &file, uint32 only_this_code, uint16 fixedsize)
 {
 uint32 fcode;
 uint16 fsize;
@@ -346,7 +346,7 @@ static bool IsPropertyNeeded(const TIns &Instruments, PropType ModInstrument::*P
 
 
 template<typename PropType>
-static void WritePropertyIfNeeded(const CSoundFile &sndFile, PropType ModInstrument::*Prop, uint32 code, uint16 size, FILE *f, INSTRUMENTINDEX numInstruments)
+static void WritePropertyIfNeeded(const CSoundFile &sndFile, PropType ModInstrument::*Prop, uint32 code, uint16 size, std::ostream &f, INSTRUMENTINDEX numInstruments)
 {
 	if(IsPropertyNeeded(sndFile.Instruments, Prop))
 	{
@@ -360,7 +360,7 @@ static void WritePropertyIfNeeded(const CSoundFile &sndFile, PropType ModInstrum
 // The reason is that ITs and XMs save [code][size][ins1.Value][ins2.Value]...
 // whereas ITP saves [code][size][ins1.Value][code][size][ins2.Value]...
 // too late to turn back....
-void CSoundFile::SaveExtendedInstrumentProperties(INSTRUMENTINDEX numInstruments, FILE *f) const
+void CSoundFile::SaveExtendedInstrumentProperties(INSTRUMENTINDEX numInstruments, std::ostream &f) const
 {
 	uint32 code = MagicBE("MPTX");	// write extension header code
 	mpt::IO::WriteIntLE<uint32>(f, code);
@@ -436,7 +436,7 @@ void CSoundFile::SaveExtendedInstrumentProperties(INSTRUMENTINDEX numInstruments
 	}
 }
 
-void CSoundFile::WriteInstrumentPropertyForAllInstruments(uint32 code, uint16 size, FILE *f, INSTRUMENTINDEX nInstruments) const
+void CSoundFile::WriteInstrumentPropertyForAllInstruments(uint32 code, uint16 size, std::ostream &f, INSTRUMENTINDEX nInstruments) const
 {
 	mpt::IO::WriteIntLE<uint32>(f, code);		//write code
 	mpt::IO::WriteIntLE<uint16>(f, size);		//write size

commit 17bb49d0508651b393976159c41dd5b1ec7e458e

[Fix] Logging: Build fix (broken since r10283).
git-svn-id: https://source.openmpt.org/svn/openmpt@10287 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/Logging.cpp
@@ -128,7 +128,7 @@ void Logger::SendLogMessage(const Context &context, LogLevel level, const char *
 			static mpt::ofstream s_logfile;
 			if(!s_logfile)
 			{
-				s_logfile = s_logfile.open(MPT_PATHSTRING("mptrack.log"), std::ios::app);
+				s_logfile.open(MPT_PATHSTRING("mptrack.log"), std::ios::app);
 			}
 			if(s_logfile)
 			{