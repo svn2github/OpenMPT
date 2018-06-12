svn2github/OpenMPT

commit 5510e03df066540dd8c78be9a89bb3d44c380f2c

[Fix] Do not check for _DEBUG throughout the code (unless in mptrack/). _DEBUG has a very specific meaning with MSVC CRT (i.e.: it is the debug crt version) and we should not overload its meaning with various other things. [Ref] Use MPT_BUILD_DEBUG instead of DEBUG. [Ref] Also avoid NDEBUG checks in inappropriate places because this in turn has very specific semantics with regard to the global runtime assert() macro.
git-svn-id: https://source.openmpt.org/svn/openmpt@10442 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/version.cpp
@@ -274,7 +274,7 @@ bool IsReleasedBuild()
 
 bool IsDebugBuild()
 {
-	#ifdef _DEBUG
+	#if defined(MPT_BUILD_DEBUG) || !defined(NDEBUG) || defined(DEBUG) || defined(_DEBUG)
 		return true;
 	#else
 		return false;

trunk/OpenMPT/common/typedefs.h
@@ -264,7 +264,7 @@ OPENMPT_NAMESPACE_BEGIN
 
 #if MPT_CHECKER_ASSUME_ASSERTIONS
 #ifdef NDEBUG
-#error "Builds for static analyzers depend on std::assert being enabled, but the current build has #define NDEBUG. This makes no sense."
+#error "Builds for static analyzers depend on assert() being enabled, but the current build has #define NDEBUG. This makes no sense."
 #endif
 OPENMPT_NAMESPACE_END
 #include <cassert>

trunk/OpenMPT/mptrack/MainFrm.cpp
@@ -215,9 +215,10 @@ void CMainFrame::Initialize()
 	//Adding version number to the frame title
 	CString title = GetTitle();
 	title += _T(" ") + mpt::cfmt::val(Version::Current());
-	#ifdef DEBUG
+	if(Build::IsDebugBuild())
+	{
 		title += _T(" DEBUG");
-	#endif
+	}
 	#ifdef NO_VST
 		title += _T(" NO_VST");
 	#endif

trunk/OpenMPT/sounddev/SoundDeviceUtilities.cpp
@@ -173,7 +173,7 @@ CPriorityBooster::CPriorityBooster(SoundDevice::SysInfo sysInfo, ComponentHandle
 	, oldPriority(THREAD_PRIORITY_NORMAL)
 {
 	MPT_TRACE_SCOPE();
-	#ifdef _DEBUG
+	#ifdef MPT_BUILD_DEBUG
 		m_BoostPriority = false;
 	#endif
 	if(m_BoostPriority)

trunk/OpenMPT/sounddev/SoundDeviceWaveout.cpp
@@ -225,7 +225,7 @@ bool CWaveDevice::InternalClose()
 		waveOutClose(m_hWaveOut);
 		m_hWaveOut = NULL;
 	}
-	#ifdef _DEBUG
+	#ifdef MPT_BUILD_DEBUG
 		if(m_DriverBugs.load())
 		{
 				SendDeviceMessage(LogError, MPT_USTRING("Errors were detected while playing sound:\n") + GetStatistics().text);

trunk/OpenMPT/soundlib/Fastmix.cpp
@@ -254,7 +254,7 @@ struct MixLoopState
 
 		Limit(nSmpCount, 1u, nSamples);
 
-#ifdef _DEBUG
+#ifdef MPT_BUILD_DEBUG
 		{
 			SmpLength posDest = (nPos + nInc * (nSmpCount - 1)).GetUInt();
 			if (posDest < 0 || posDest > chn.nLength)
@@ -393,11 +393,11 @@ void CSoundFile::CreateStereoMix(int count)
 				chn.nROfs = -*(pbufmax - 2);
 				chn.nLOfs = -*(pbufmax - 1);
 
-#ifdef _DEBUG
+#ifdef MPT_BUILD_DEBUG
 				SamplePosition targetpos = chn.position + chn.increment * nSmpCount;
 #endif
 				MixFuncTable::Functions[functionNdx | (chn.nRampLength ? MixFuncTable::ndxRamp : 0)](chn, m_Resampler, pbuffer, nSmpCount);
-#ifdef _DEBUG
+#ifdef MPT_BUILD_DEBUG
 				MPT_ASSERT(chn.position.GetUInt() == targetpos.GetUInt());
 #endif
 

trunk/OpenMPT/soundlib/Load_mod.cpp
@@ -587,7 +587,7 @@ static PATTERNINDEX GetNumPatterns(FileReader &file, ModSequence &Order, ORDERIN
 		file.Seek(patternStartOffset);
 	}
 
-#ifdef _DEBUG
+#ifdef MPT_BUILD_DEBUG
 	// Check if the "hidden" patterns in the order list are actually real, i.e. if they are saved in the file.
 	// OpenMPT did this check in the past, but no other tracker appears to do this.
 	// Interestingly, (broken) variants of the ProTracker modules

trunk/OpenMPT/soundlib/load_j2b.cpp
@@ -22,6 +22,9 @@
 #endif
 
 
+//#define J2B_LOG
+
+
 OPENMPT_NAMESPACE_BEGIN
 
 
@@ -528,9 +531,9 @@ static bool ConvertAMPattern(FileReader chunk, PATTERNINDEX pat, bool isAM, CSou
 					m.command = amEffTrans[command];
 				} else
 				{
-#ifdef DEBUG
+#ifdef J2B_LOG
 					Log(mpt::format("J2B: Unknown command: 0x%1, param 0x%2")(mpt::fmt::HEX0<2>(command), mpt::fmt::HEX0<2>(m.param)));
-#endif // DEBUG
+#endif
 					m.command = CMD_NONE;
 				}
 

commit 1b78b2a56b3c8dbe4143d2b03298b59429a58fed

[Fix] Do not flag builds without NDEBUG as IsDebugBuild(). libopenmpt generally does not define NDEBUG, and this in fact only even matters if we actually use assert(), which is already guarded by MPT_BUILD_DEBUG and MPT_BUILD_CHECKED (broken since r10442).
git-svn-id: https://source.openmpt.org/svn/openmpt@10446 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/common/version.cpp
@@ -274,7 +274,7 @@ bool IsReleasedBuild()
 
 bool IsDebugBuild()
 {
-	#if defined(MPT_BUILD_DEBUG) || !defined(NDEBUG) || defined(DEBUG) || defined(_DEBUG)
+	#if defined(MPT_BUILD_DEBUG) || defined(DEBUG) || defined(_DEBUG)
 		return true;
 	#else
 		return false;