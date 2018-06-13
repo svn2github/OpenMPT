svn2github/OpenMPT

commit 8affc8a1f458a41dcb3395bd5e78bff3a92665c6

[New] Add code to support loading pixel and vector images using GDI+ as an alternative to our internal PNG loader. This code is disabled for now as we are not requiering it yet. [Ref] Rename raw GDI bitmap handling types and conversion functions. In particular, rename misnomer ToDIB() to CopyToCompatibleBitmap() because it in fact does the complete opposite of creating a DIB: it creates a compatible DDB. [Ref] Use std::unique_ptr to pass images around. [Ref] Use exceptions to signal errors during image loading.
git-svn-id: https://source.openmpt.org/svn/openmpt@10067 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/CImageListEx.cpp
@@ -19,8 +19,11 @@ OPENMPT_NAMESPACE_BEGIN
 
 bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGrow, CDC *dc, bool disabled)
 {
-	PNG::Bitmap *bitmap = PNG::ReadPNG(MAKEINTRESOURCE(resourceID));
-	if(bitmap == nullptr)
+	std::unique_ptr<RawGDIDIB> bitmap;
+	try
+	{
+		bitmap = LoadPixelImage(GetResource(MAKEINTRESOURCE(resourceID), _T("PNG")));
+	} catch(...)
 	{
 		return false;
 	}
@@ -28,14 +31,13 @@ bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGr
 	if(disabled)
 	{
 		// Grayed out icons
-		PNG::Pixel *pixel = bitmap->GetPixels();
-		for(size_t i = bitmap->GetNumPixels(); i != 0; i--, pixel++)
+		for(auto &pixel : bitmap->Pixels())
 		{
-			if(pixel->a != 0)
+			if(pixel.a != 0)
 			{
-				uint8_t y = static_cast<uint8_t>(pixel->r * 0.299f + pixel->g * 0.587f + pixel->b * 0.114f);
-				pixel->r = pixel->g = pixel->b = y;
-				pixel->a /= 2;
+				uint8 y = static_cast<uint8>(pixel.r * 0.299f + pixel.g * 0.587f + pixel.b * 0.114f);
+				pixel.r = pixel.g = pixel.b = y;
+				pixel.a /= 2;
 			}
 		}
 	}
@@ -45,17 +47,17 @@ bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGr
 	// Use 1-bit transperency when there is no alpha channel.
 	if(GetDeviceCaps(dc->GetSafeHdc(), BITSPIXEL) * GetDeviceCaps(dc->GetSafeHdc(), PLANES) < 32)
 	{
-		uint32 rowSize = (bitmap->width + 31u) / 32u * 4u;
-		std::vector<uint8> bitmapMask(rowSize * bitmap->height);
+		uint32 rowSize = (bitmap->Width() + 31u) / 32u * 4u;
+		std::vector<uint8> bitmapMask(rowSize * bitmap->Height());
 
 		const COLORREF buttonColor = GetSysColor(COLOR_BTNFACE);
-		const uint8_t r = GetRValue(buttonColor), g = GetGValue(buttonColor), b = GetBValue(buttonColor);
+		const uint8 r = GetRValue(buttonColor), g = GetGValue(buttonColor), b = GetBValue(buttonColor);
 
-		PNG::Pixel *pixel = bitmap->GetPixels();
-		for(uint32 y = 0; y < bitmap->height; y++)
+		RawGDIDIB::Pixel *pixel = bitmap->Pixels().data();
+		for(uint32 y = 0; y < bitmap->Height(); y++)
 		{
 			uint8 *mask = bitmapMask.data() + rowSize * y;
-			for(uint32 x = 0; x < bitmap->width; x++, pixel++)
+			for(uint32 x = 0; x < bitmap->Width(); x++, pixel++)
 			{
 				if(pixel->a != 0)
 				{
@@ -73,8 +75,8 @@ bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGr
 			}
 		}
 
-		CBitmap dib, dibMask;
-		bitmap->ToDIB(dib, dc);
+		CBitmap ddb, dibMask;
+		CopyToCompatibleBitmap(ddb, *dc, *bitmap);
 
 		struct
 		{
@@ -83,8 +85,8 @@ bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGr
 		} bi;
 		MemsetZero(bi);
 		bi.bi.biSize = sizeof(BITMAPINFOHEADER);
-		bi.bi.biWidth = bitmap->width;
-		bi.bi.biHeight = -(int32)bitmap->height;
+		bi.bi.biWidth = bitmap->Width();
+		bi.bi.biHeight = -(int32)bitmap->Height();
 		bi.bi.biPlanes = 1;
 		bi.bi.biBitCount = 1;
 		bi.bi.biCompression = BI_RGB;
@@ -92,35 +94,34 @@ bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGr
 		bi.col[1].rgbBlue = bi.col[1].rgbGreen = bi.col[1].rgbRed = 255;
 
 		if(dc == nullptr) dc = CDC::FromHandle(GetDC(NULL));
-		dibMask.CreateCompatibleBitmap(dc, bitmap->width, bitmap->height);
-		SetDIBits(dc->GetSafeHdc(), dibMask, 0, bitmap->height, bitmapMask.data(), reinterpret_cast<BITMAPINFO *>(&bi), DIB_RGB_COLORS);
+		dibMask.CreateCompatibleBitmap(dc, bitmap->Width(), bitmap->Height());
+		SetDIBits(dc->GetSafeHdc(), dibMask, 0, bitmap->Height(), bitmapMask.data(), reinterpret_cast<BITMAPINFO *>(&bi), DIB_RGB_COLORS);
 
 		result = CImageList::Create(cx, cy, ILC_COLOR24 | ILC_MASK, nInitial, nGrow)
-			&& CImageList::Add(&dib, &dibMask);
+			&& CImageList::Add(&ddb, &dibMask);
 	} else
 	{
 		// 32-bit image on modern system
 
 		// Make fully transparent pixels use the mask color. This should hopefully make the icons look "somewhat" okay
-		// on system where the alpha channel is magically missing in 32-bit mode (http://bugs.openmpt.org/view.php?id=520)
-		PNG::Pixel *pixel = bitmap->GetPixels();
-		for(size_t i = bitmap->GetNumPixels(); i != 0; i--, pixel++)
+		// on system where the alpha channel is magically missing in 32-bit mode (https://bugs.openmpt.org/view.php?id=520)
+		for(auto &pixel : bitmap->Pixels())
 		{
-			if(pixel->a == 0)
+			if(pixel.a == 0)
 			{
-				pixel->r = 255;
-				pixel->g = 0;
-				pixel->b = 255;
+				pixel.r = 255;
+				pixel.g = 0;
+				pixel.b = 255;
 			}
 		}
 
-		CBitmap dib;
-		bitmap->ToDIB(dib, dc);
+		CBitmap ddb;
+		CopyToCompatibleBitmap(ddb, *dc, *bitmap);
 		result = CImageList::Create(cx, cy, ILC_COLOR32 | ILC_MASK, nInitial, nGrow)
-			&& CImageList::Add(&dib, RGB(255, 0, 255));
+			&& CImageList::Add(&ddb, RGB(255, 0, 255));
 	}
-	delete bitmap;
-	return true;
+
+	return result;
 }
 
 

trunk/OpenMPT/common/BuildSettings.h
@@ -101,6 +101,7 @@
 // OpenMPT-only dependencies
 #define MPT_WITH_ASIO
 #define MPT_WITH_DSOUND
+//#define MPT_WITH_GDIPLUS
 #define MPT_WITH_LHASA
 #define MPT_WITH_MINIZIP
 #define MPT_WITH_OPUS

trunk/OpenMPT/mptrack/AboutDialog.cpp
@@ -25,17 +25,17 @@ END_MESSAGE_MAP()
 
 CRippleBitmap::CRippleBitmap()
 {
-	bitmapSrc = PNG::ReadPNG(MAKEINTRESOURCE(IDB_MPTRACK));
-	bitmapTarget = new PNG::Bitmap(bitmapSrc->width, bitmapSrc->height);
-	offset1.assign(bitmapSrc->GetNumPixels(), 0);
-	offset2.assign(bitmapSrc->GetNumPixels(), 0);
+	bitmapSrc = LoadPixelImage(GetResource(MAKEINTRESOURCE(IDB_MPTRACK), _T("PNG")));
+	bitmapTarget = mpt::make_unique<RawGDIDIB>(bitmapSrc->Width(), bitmapSrc->Height());
+	offset1.assign(bitmapSrc->Pixels().size(), 0);
+	offset2.assign(bitmapSrc->Pixels().size(), 0);
 	frontBuf = offset2.data();
 	backBuf = offset1.data();
 
 	// Pre-fill first and last row of output bitmap, since those won't be touched.
-	const PNG::Pixel *in1 = bitmapSrc->GetPixels(), *in2 = bitmapSrc->GetPixels() + (bitmapSrc->height - 1) * bitmapSrc->width;
-	PNG::Pixel *out1 = bitmapTarget->GetPixels(), *out2 = bitmapTarget->GetPixels() + (bitmapSrc->height - 1) * bitmapSrc->width;
-	for(uint32 i = 0; i < bitmapSrc->width; i++)
+	const RawGDIDIB::Pixel *in1 = bitmapSrc->Pixels().data(), *in2 = bitmapSrc->Pixels().data() + (bitmapSrc->Height() - 1) * bitmapSrc->Width();
+	RawGDIDIB::Pixel *out1 = bitmapTarget->Pixels().data(), *out2 = bitmapTarget->Pixels().data() + (bitmapSrc->Height() - 1) * bitmapSrc->Width();
+	for(uint32 i = 0; i < bitmapSrc->Width(); i++)
 	{
 		*(out1++) = *(in1++);
 		*(out2++) = *(in2++);
@@ -43,12 +43,12 @@ CRippleBitmap::CRippleBitmap()
 
 	MemsetZero(bi);
 	bi.biSize = sizeof(BITMAPINFOHEADER);
-	bi.biWidth = bitmapSrc->width;
-	bi.biHeight = -(int32)bitmapSrc->height;
+	bi.biWidth = bitmapSrc->Width();
+	bi.biHeight = -(int32)bitmapSrc->Height();
 	bi.biPlanes = 1;
 	bi.biBitCount = 32;
 	bi.biCompression = BI_RGB;
-	bi.biSizeImage = bitmapSrc->width * bitmapSrc->height * 4;
+	bi.biSizeImage = bitmapSrc->Width() * bitmapSrc->Height() * 4;
 
 }
 
@@ -59,8 +59,6 @@ CRippleBitmap::~CRippleBitmap()
 	{
 		ShowCursor(TRUE);
 	}
-	delete bitmapSrc;
-	delete bitmapTarget;
 }
 
 
@@ -74,13 +72,13 @@ void CRippleBitmap::OnMouseMove(UINT nFlags, CPoint point)
 	lastRipple = now;
 
 	// Initiate ripples at cursor location
-	Limit(point.x, 1, int(bitmapSrc->width) - 2);
-	Limit(point.y, 2, int(bitmapSrc->height) - 3);
-	int32 *p = backBuf + point.x + point.y * bitmapSrc->width;
+	Limit(point.x, 1, int(bitmapSrc->Width()) - 2);
+	Limit(point.y, 2, int(bitmapSrc->Height()) - 3);
+	int32 *p = backBuf + point.x + point.y * bitmapSrc->Width();
 	p[0] += (nFlags & MK_LBUTTON) ? 50 : 150;
 	p[0] += (nFlags & MK_MBUTTON) ? 150 : 0;
 
-	int32 w = bitmapSrc->width;
+	int32 w = bitmapSrc->Width();
 	// Make the initial point of this ripple a bit "fatter".
 	p[-1] += p[0] / 2;     p[1] += p[0] / 2;
 	p[-w] += p[0] / 2;     p[w] += p[0] / 2;
@@ -133,8 +131,8 @@ void CRippleBitmap::OnPaint()
 	GetClientRect(rect);
 	StretchDIBits(dc.m_hDC,
 		0, 0, rect.Width(), rect.Height(),
-		0, 0, bitmapTarget->width, bitmapTarget->height,
-		bitmapTarget->GetPixels(),
+		0, 0, bitmapTarget->Width(), bitmapTarget->Height(),
+		bitmapTarget->Pixels().data(),
 		reinterpret_cast<BITMAPINFO *>(&bi), DIB_RGB_COLORS, SRCCOPY);
 }
 
@@ -155,7 +153,7 @@ bool CRippleBitmap::Animate()
 	backBuf = (frame ? offset1 : offset2).data();
 
 	// Spread the ripples...
-	const int32 w = bitmapSrc->width, h = bitmapSrc->height;
+	const int32 w = bitmapSrc->Width(), h = bitmapSrc->Height();
 	const int32 numPixels = w * (h - 2);
 	const int32 *back = backBuf + w;
 	int32 *front = frontBuf + w;
@@ -167,9 +165,9 @@ bool CRippleBitmap::Animate()
 
 	// ...and compute the final picture.
 	const int32 *offset = frontBuf + w;
-	const PNG::Pixel *pixelIn = bitmapSrc->GetPixels() + w;
-	PNG::Pixel *pixelOut = bitmapTarget->GetPixels() + w;
-	PNG::Pixel *limitMin = bitmapSrc->GetPixels(), *limitMax = bitmapSrc->GetPixels() + bitmapSrc->GetNumPixels() - 1;
+	const RawGDIDIB::Pixel *pixelIn = bitmapSrc->Pixels().data() + w;
+	RawGDIDIB::Pixel *pixelOut = bitmapTarget->Pixels().data() + w;
+	RawGDIDIB::Pixel *limitMin = bitmapSrc->Pixels().data(), *limitMax = bitmapSrc->Pixels().data() + bitmapSrc->Pixels().size() - 1;
 	for(int32 i = numPixels; i != 0; i--, pixelIn++, pixelOut++, offset++)
 	{
 		// Compute pixel displacement
@@ -178,7 +176,7 @@ bool CRippleBitmap::Animate()
 
 		if(xOff | yOff)
 		{
-			const PNG::Pixel *p = pixelIn + xOff + yOff * w;
+			const RawGDIDIB::Pixel *p = pixelIn + xOff + yOff * w;
 			Limit(p, limitMin, limitMax);
 			// Add a bit of shading depending on how far we're displacing the pixel...
 			pixelOut->r = mpt::saturate_cast<uint8>(p->r + (p->r * xOff) / 32);

trunk/OpenMPT/mptrack/AboutDialog.h
@@ -2,7 +2,7 @@
 
 OPENMPT_NAMESPACE_BEGIN
 
-namespace PNG { struct Bitmap; }
+class RawGDIDIB;
 
 class CRippleBitmap: public CWnd
 {
@@ -14,7 +14,7 @@ public:
 protected:
 
 	BITMAPINFOHEADER bi;
-	PNG::Bitmap *bitmapSrc, *bitmapTarget;
+	std::unique_ptr<RawGDIDIB> bitmapSrc, bitmapTarget;
 	std::vector<int32> offset1, offset2;
 	int32 *frontBuf, *backBuf;
 	DWORD lastFrame = 0;	// Time of last frame

trunk/OpenMPT/mptrack/MPTrackLink.cpp
@@ -24,6 +24,10 @@ OPENMPT_NAMESPACE_BEGIN
 #pragma comment(lib, "htmlhelp.lib")
 #pragma comment(lib, "uxtheme.lib")
 
+#ifdef MPT_WITH_GDIPLUS
+#pragma comment(lib, "gdiplus.lib")
+#endif
+
 #pragma comment(lib, "dmoguids.lib")
 #pragma comment(lib, "strmiids.lib")
 

trunk/OpenMPT/mptrack/Mptrack.cpp
@@ -34,6 +34,15 @@
 #include "../soundlib/plugins/PluginManager.h"
 #include "MPTrackWine.h"
 
+#ifdef MPT_WITH_GDIPLUS
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#pragma warning(push)
+#pragma warning(disable:4458) // declaration of 'x' hides class member
+#include <gdiplus.h>
+#pragma warning(pop)
+#endif // MPT_WITH_GDIPLUS
+
 // rewbs.memLeak
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
@@ -851,6 +860,10 @@ BOOL CTrackApp::InitInstanceImpl(CMPTCommandLineInfo &cmdInfo)
 	// additionally, seed the C rand() PRNG, just in case any third party library calls rand()
 	mpt::rng::crand::reseed(RandomDevice());
 
+	#ifdef MPT_WITH_GDIPLUS
+		m_Gdiplus = mpt::make_unique<GdiplusRAII>();
+	#endif // MPT_WITH_GDIPLUS
+
 	if(cmdInfo.m_bNoWine)
 	{
 		mpt::Windows::PreventWineDetection();
@@ -1215,6 +1228,10 @@ int CTrackApp::ExitInstanceImpl()
 		SetWineVersion(nullptr);
 	}
 
+	#ifdef MPT_WITH_GDIPLUS
+		m_Gdiplus.reset();
+	#endif // MPT_WITH_GDIPLUS
+
 	m_PRNG.reset();
 	mpt::set_global_prng(nullptr);
 	m_BestPRNG.reset();
@@ -1337,8 +1354,13 @@ void CTrackApp::OnAppAbout()
 class CSplashScreen: public CDialog
 {
 protected:
+#ifdef MPT_WITH_GDIPLUS
+	std::unique_ptr<Gdiplus::Image> m_Image;
+#else
 	CBitmap m_Bitmap;
-	PNG::Bitmap *bitmap;
+	uint32 m_BitmapWidth;
+	uint32 m_BitmapHeight;
+#endif
 
 public:
 	CSplashScreen();
@@ -1364,48 +1386,95 @@ static DWORD gSplashScreenStartTime = 0;
 
 CSplashScreen::CSplashScreen()
 {
-	bitmap = PNG::ReadPNG(MAKEINTRESOURCE(IDB_SPLASHNOFOLDFIN));
 }
 
 
 CSplashScreen::~CSplashScreen()
 {
 	gpSplashScreen = nullptr;
-	delete bitmap;
 }
 
 
 void CSplashScreen::OnPaint()
 {
-	CPaintDC dc(this);
+	#ifdef MPT_WITH_GDIPLUS
 
-	CDC hdcMem;
-	hdcMem.CreateCompatibleDC(&dc);
-	CBitmap *oldBitmap = hdcMem.SelectObject(&m_Bitmap);
-	dc.BitBlt(0, 0, bitmap->width, bitmap->height, &hdcMem, 0, 0, SRCCOPY);
-	hdcMem.SelectObject(oldBitmap);
-	hdcMem.DeleteDC();
+		CPaintDC dc(this);
+		Gdiplus::Graphics gfx(dc);
 
-	CDialog::OnPaint();
+		gfx.DrawImage(m_Image.get(), 0, 0);
+
+		CDialog::OnPaint();
+
+	#else // !MPT_WITH_GDIPLUS
+
+		CPaintDC dc(this);
+
+		CDC hdcMem;
+		hdcMem.CreateCompatibleDC(&dc);
+		CBitmap *oldBitmap = hdcMem.SelectObject(&m_Bitmap);
+		dc.BitBlt(0, 0, m_BitmapWidth, m_BitmapHeight, &hdcMem, 0, 0, SRCCOPY);
+		hdcMem.SelectObject(oldBitmap);
+		hdcMem.DeleteDC();
+
+		CDialog::OnPaint();
+
+	#endif // MPT_WITH_GDIPLUS
 }
 
 
 BOOL CSplashScreen::OnInitDialog()
 {
-	CDialog::OnInitDialog();
+	if(!CDialog::OnInitDialog())
+	{
+		return FALSE;
+	}
 
-	CDC *dc = GetDC();
-	bitmap->ToDIB(m_Bitmap, dc);
-	ReleaseDC(dc);
+	#ifdef MPT_WITH_GDIPLUS
 
-	CRect rect;
-	GetWindowRect(&rect);
-	SetWindowPos(nullptr,
-		rect.left - ((static_cast<int32>(bitmap->width) - rect.Width()) / 2),
-		rect.top - ((static_cast<int32>(bitmap->height) - rect.Height()) / 2),
-		bitmap->width,
-		bitmap->height,
-		SWP_NOZORDER | SWP_NOCOPYBITS);
+		try
+		{
+			m_Image = GDIP::LoadPixelImage(GetResource(MAKEINTRESOURCE(IDB_SPLASHNOFOLDFIN), _T("PNG")));
+		} catch(const bad_image &)
+		{
+			return FALSE;
+		}
+
+		CRect rect;
+		GetWindowRect(&rect);
+		SetWindowPos(nullptr,
+			rect.left - ((static_cast<int32>(m_Image->GetWidth()) - rect.Width()) / 2),
+			rect.top - ((static_cast<int32>(m_Image->GetHeight()) - rect.Height()) / 2),
+			m_Image->GetWidth(),
+			m_Image->GetHeight(),
+			SWP_NOZORDER | SWP_NOCOPYBITS);
+
+	#else // !MPT_WITH_GDIPLUS
+	
+		std::unique_ptr<RawGDIDIB> bitmap;
+		try
+		{
+			bitmap = LoadPixelImage(GetResource(MAKEINTRESOURCE(IDB_SPLASHNOFOLDFIN), _T("PNG")));
+		} catch(const bad_image &)
+		{
+			return FALSE;
+		}
+		m_BitmapWidth = bitmap->Width();
+		m_BitmapHeight = bitmap->Height();
+		CDC *dc = GetDC();
+		CopyToCompatibleBitmap(m_Bitmap, *dc, *bitmap);
+		ReleaseDC(dc);
+	
+		CRect rect;
+		GetWindowRect(&rect);
+		SetWindowPos(nullptr,
+			rect.left - ((static_cast<int32>(m_BitmapWidth) - rect.Width()) / 2),
+			rect.top - ((static_cast<int32>(m_BitmapHeight) - rect.Height()) / 2),
+			m_BitmapWidth,
+			m_BitmapHeight,
+			SWP_NOZORDER | SWP_NOCOPYBITS);
+
+	#endif // MPT_WITH_GDIPLUS
 
 	return TRUE;
 }

trunk/OpenMPT/mptrack/Mptrack.h
@@ -36,6 +36,9 @@ namespace mpt { namespace Wine {
 class VersionContext;
 class Context;
 } } // namespace mpt::Wine
+#ifdef MPT_WITH_GDIPLUS
+class GdiplusRAII;
+#endif // MPT_WITH_GDIPLUS
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -121,6 +124,10 @@ protected:
 	std::unique_ptr<mpt::random_device> m_RD;
 	std::unique_ptr<mpt::thread_safe_prng<mpt::best_prng> > m_BestPRNG;
 	std::unique_ptr<mpt::thread_safe_prng<mpt::prng> > m_PRNG;
+
+#ifdef MPT_WITH_GDIPLUS
+	std::unique_ptr<GdiplusRAII> m_Gdiplus;
+#endif // MPT_WITH_GDIPLUS
 
 	std::shared_ptr<mpt::Wine::VersionContext> m_WineVersion;
 

trunk/OpenMPT/mptrack/PNG.cpp
@@ -1,9 +1,8 @@
 /*
- * PNG.cpp
- * -------
- * Purpose: Extremely minimalistic PNG loader (only for internal data resources)
- * Notes  : Currently implemented: 8-bit, 24-bit and 32-bit images (8 bits per component), no filters, no interlaced pictures.
- *          Paletted pictures are automatically depalettized.
+ * Image.cpp
+ * ---------
+ * Purpose: Bitmap and Vector image file handling.
+ * Notes  : Either uses GDI+ or a simple custom PNG loader.
  * Authors: OpenMPT Devs
  * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
  */
@@ -13,22 +12,36 @@
 #include "MPTrackUtil.h"
 #include "PNG.h"
 #include "../common/FileReader.h"
+#include "../common/ComponentManager.h"
+
+#ifdef MPT_WITH_GDIPLUS
+#include <atlbase.h>
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#pragma warning(push)
+#pragma warning(disable:4458) // declaration of 'x' hides class member
+#include <gdiplus.h>
+#pragma warning(pop)
+#else // !MPT_WITH_GDIPLUS
 #if defined(MPT_WITH_ZLIB)
 #include <zlib.h>
 #elif defined(MPT_WITH_MINIZ)
 #include <miniz/miniz.h>
 #endif
+#endif // MPT_WITH_GDIPLUS
 
 
 OPENMPT_NAMESPACE_BEGIN
 
 
-PNG::Bitmap *PNG::ReadPNG(FileReader &file)
+#ifndef MPT_WITH_GDIPLUS
+
+static std::unique_ptr<RawGDIDIB> ReadPNG(FileReader file)
 {
 	file.Rewind();
 	if(!file.ReadMagic("\211PNG\r\n\032\n"))
 	{
-		return nullptr;
+		throw bad_image();
 	}
 
 	uint32_t width = 0;
@@ -40,7 +53,7 @@ PNG::Bitmap *PNG::ReadPNG(FileReader &file)
 	uint8_t interlaceMethod;
 
 	std::vector<uint8_t> dataIn;
-	std::vector<Pixel> palette;
+	std::vector<RawGDIDIB::Pixel> palette;
 
 	while(file.CanRead(12))
 	{
@@ -91,7 +104,7 @@ PNG::Bitmap *PNG::ReadPNG(FileReader &file)
 			{
 				uint8_t p[3];
 				chunk.ReadArray(p);
-				palette[i] = Pixel(p[0], p[1], p[2], 255);
+				palette[i] = RawGDIDIB::Pixel(p[0], p[1], p[2], 255);
 			}
 		}
 	}
@@ -115,12 +128,12 @@ PNG::Bitmap *PNG::ReadPNG(FileReader &file)
 		|| (colorType == 3 && palette.empty())
 		|| dataIn.size() < (bitsPerPixel * width * height) / 8 + height)			// Enough data present?
 	{
-		return nullptr;
+		throw bad_image();
 	}
 
-	Bitmap *bitmap = new (std::nothrow) Bitmap(width, height);
+	std::unique_ptr<RawGDIDIB> bitmap = mpt::make_unique<RawGDIDIB>(width, height);
 
-	Pixel *pixelOut = bitmap->GetPixels();
+	RawGDIDIB::Pixel *pixelOut = bitmap->Pixels().data();
 	uint32_t x = 0, y = 0;
 	size_t offset = 0;
 	while(y < height)
@@ -163,35 +176,272 @@ PNG::Bitmap *PNG::ReadPNG(FileReader &file)
 	return bitmap;
 }
 
+#endif // !MPT_WITH_GDIPLUS
 
-PNG::Bitmap *PNG::ReadPNG(const TCHAR *resource)
+
+#ifdef MPT_WITH_GDIPLUS
+
+
+GdiplusRAII::GdiplusRAII()
+	: gdiplusToken(0)
 {
-	mpt::const_byte_span data = GetResource(resource, TEXT("PNG"));
-	if(!data.data())
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+}
+
+
+GdiplusRAII::~GdiplusRAII()
+{
+	Gdiplus::GdiplusShutdown(gdiplusToken);
+	gdiplusToken = 0;
+}
+
+
+#endif // MPT_WITH_GDIPLUS
+
+
+RawGDIDIB::RawGDIDIB(uint32 width, uint32 height)
+	: width(width)
+	, height(height)
+	, pixels(width * height)
+{
+	MPT_ASSERT(width > 0);
+	MPT_ASSERT(height > 0);
+}
+
+
+#ifdef MPT_WITH_GDIPLUS
+
+
+namespace GDIP
+{
+
+
+static CComPtr<IStream> GetStream(mpt::const_byte_span data)
+{
+	CComPtr<IStream> stream;
+#if (_WIN32_WINNT >= _WIN32_WINNT_VISTA)
+	stream.Attach(SHCreateMemStream(data.data(), mpt::saturate_cast<UINT>(data.size())));
+#else
+	HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE | GMEM_ZEROINIT, data.size());
+	if(hGlobal == NULL)
 	{
-		return nullptr;
+		throw bad_image();
 	}
-	FileReader file(data);
-	return ReadPNG(file);
+	void * mem = GlobalLock(hGlobal);
+	if(!mem)
+	{
+		hGlobal = GlobalFree(hGlobal);
+		throw bad_image();
+	}
+	std::memcpy(mem, data.data(), data.size());
+	GlobalUnlock(hGlobal);
+	if(CreateStreamOnHGlobal(NULL, TRUE, &stream) != S_OK)
+	{
+		hGlobal = GlobalFree(hGlobal);
+		throw bad_image();
+	}
+	hGlobal = NULL;
+#endif
+	if(!stream)
+	{
+		throw bad_image();
+	}
+	return stream;
+}
+
+
+std::unique_ptr<Gdiplus::Bitmap> LoadPixelImage(mpt::const_byte_span file)
+{
+	CComPtr<IStream> stream = GetStream(file);
+	std::unique_ptr<Gdiplus::Bitmap> result = mpt::make_unique<Gdiplus::Bitmap>(stream, FALSE);
+	if(result->GetLastStatus() != Gdiplus::Ok)
+	{
+		throw bad_image();
+	}
+	if(result->GetWidth() == 0 || result->GetHeight() == 0)
+	{
+		throw bad_image();
+	}
+	return result;
+}
+
+
+std::unique_ptr<Gdiplus::Bitmap> LoadPixelImage(FileReader file)
+{
+	FileReader::PinnedRawDataView view = file.GetPinnedRawDataView();
+	return LoadPixelImage(view.span());
+}
+
+
+std::unique_ptr<Gdiplus::Metafile> LoadVectorImage(mpt::const_byte_span file)
+{
+	CComPtr<IStream> stream = GetStream(file);
+	std::unique_ptr<Gdiplus::Metafile> result = mpt::make_unique<Gdiplus::Metafile>(stream);
+	if(result->GetLastStatus() != Gdiplus::Ok)
+	{
+		throw bad_image();
+	}
+	if(result->GetWidth() == 0 || result->GetHeight() == 0)
+	{
+		throw bad_image();
+	}
+	return result;
+}
+
+
+std::unique_ptr<Gdiplus::Metafile> LoadVectorImage(FileReader file)
+{
+	FileReader::PinnedRawDataView view = file.GetPinnedRawDataView();
+	return LoadVectorImage(view.span());
+}
+
+
+} // namespace GDPI
+
+
+#endif // MPT_WITH_GDIPLUS
+
+
+#ifdef MPT_WITH_GDIPLUS
+
+std::unique_ptr<RawGDIDIB> ToRawGDIDIB(Gdiplus::Bitmap &bitmap)
+{
+	Gdiplus::BitmapData bitmapData;
+	Gdiplus::Rect rect{Gdiplus::Point{0, 0}, Gdiplus::Size{static_cast<INT>(bitmap.GetWidth()), static_cast<INT>(bitmap.GetHeight())}};
+	std::unique_ptr<RawGDIDIB> result = mpt::make_unique<RawGDIDIB>(bitmap.GetWidth(), bitmap.GetHeight());
+	if(bitmap.LockBits(&rect, Gdiplus::ImageLockModeRead, PixelFormat32bppARGB, &bitmapData) != Gdiplus::Ok)
+	{
+		throw bad_image();
+	}
+	RawGDIDIB::Pixel *dst = result->Pixels().data();
+	for(uint32 y = 0; y < result->Height(); ++y)
+	{
+		const GDIP::Pixel *src = GDIP::GetScanline(bitmapData, y);
+		for(uint32 x = 0; x < result->Width(); ++x)
+		{
+			*dst = GDIP::TORawGDIDIB(*src);
+			src++;
+			dst++;
+		}
+	}
+	bitmap.UnlockBits(&bitmapData);
+	return result;
+}
+
+#endif // MPT_WITH_GDIPLUS
+
+
+std::unique_ptr<RawGDIDIB> LoadPixelImage(mpt::const_byte_span file)
+{
+	#ifdef MPT_WITH_GDIPLUS
+		return ToRawGDIDIB(*GDIP::LoadPixelImage(file));
+	#else // !MPT_WITH_GDIPLUS
+		return ReadPNG(file);
+	#endif // MPT_WITH_GDIPLUS
+}
+
+
+std::unique_ptr<RawGDIDIB> LoadPixelImage(FileReader file)
+{
+	#ifdef MPT_WITH_GDIPLUS
+		return ToRawGDIDIB(*GDIP::LoadPixelImage(file));
+	#else // !MPT_WITH_GDIPLUS
+		return ReadPNG(file);
+	#endif // MPT_WITH_GDIPLUS
 }
 
 
 // Create a DIB for the current device from our PNG.
-bool PNG::Bitmap::ToDIB(CBitmap &bitmap, CDC *dc) const
+bool CopyToCompatibleBitmap(CBitmap &dst, CDC &dc, const RawGDIDIB &src)
 {
 	BITMAPINFOHEADER bi;
 	MemsetZero(bi);
 	bi.biSize = sizeof(BITMAPINFOHEADER);
-	bi.biWidth = width;
-	bi.biHeight = -(int32_t)height;
+	bi.biWidth = src.Width();
+	bi.biHeight = -static_cast<LONG>(src.Height());
 	bi.biPlanes = 1;
 	bi.biBitCount = 32;
 	bi.biCompression = BI_RGB;
-	bi.biSizeImage = width * height * 4;
-
-	if(dc == nullptr) dc = CDC::FromHandle(GetDC(NULL));
-	return bitmap.CreateCompatibleBitmap(dc, width, height)
-		&& SetDIBits(dc->GetSafeHdc(), bitmap, 0, height, GetPixels(), reinterpret_cast<BITMAPINFO *>(&bi), DIB_RGB_COLORS);
+	bi.biSizeImage = src.Width() * src.Height() * 4;
+	if(!dst.CreateCompatibleBitmap(&dc, src.Width(), src.Height()))
+	{
+		return false;
+	}
+	if(!SetDIBits(dc.GetSafeHdc(), dst, 0, src.Height(), src.Pixels().data(), reinterpret_cast<BITMAPINFO *>(&bi), DIB_RGB_COLORS))
+	{
+		return false;
+	}
+	return true;
 }
+
+
+#ifdef MPT_WITH_GDIPLUS
+
+bool CopyToCompatibleBitmap(CBitmap &dst, CDC &dc, Gdiplus::Image &src)
+{
+	if(!dst.CreateCompatibleBitmap(&dc, src.GetWidth(), src.GetHeight()))
+	{
+		return false;
+	}
+	CDC memdc;
+	if(!memdc.CreateCompatibleDC(&dc))
+	{
+		return false;
+	}
+	memdc.SelectObject(dst);
+	Gdiplus::Graphics gfx(memdc);
+	if(gfx.DrawImage(&src, 0, 0) != Gdiplus::Ok)
+	{
+		return false;
+	}
+	return true;
+}
+
+#endif // MPT_WITH_GDIPLUS
+
+
+
+bool LoadCompatibleBitmapFromPixelImage(CBitmap &dst, CDC &dc, mpt::const_byte_span file)
+{
+	try
+	{
+		#ifdef MPT_WITH_GDIPLUS
+			std::unique_ptr<Gdiplus::Bitmap> pBitmap = GDIP::LoadPixelImage(file);
+		#else // !MPT_WITH_GDIPLUS
+			std::unique_ptr<RawGDIDIB> pBitmap = ReadPNG(file);
+		#endif // MPT_WITH_GDIPLUS
+		if(!CopyToCompatibleBitmap(dst, dc, *pBitmap))
+		{
+			return false;
+		}
+	} catch(...)
+	{
+		return false;
+	}
+	return true;
+}
+
+
+bool LoadCompatibleBitmapFromPixelImage(CBitmap &dst, CDC &dc, FileReader file)
+{
+	try
+	{
+		#ifdef MPT_WITH_GDIPLUS
+			std::unique_ptr<Gdiplus::Bitmap> pBitmap = GDIP::LoadPixelImage(file);
+		#else // !MPT_WITH_GDIPLUS
+			std::unique_ptr<RawGDIDIB> pBitmap = ReadPNG(file);
+		#endif // MPT_WITH_GDIPLUS
+		if(!CopyToCompatibleBitmap(dst, dc, *pBitmap))
+		{
+			return false;
+		}
+	} catch(...)
+	{
+		return false;
+	}
+	return true;
+}
+
 
 OPENMPT_NAMESPACE_END

commit 6b51d06164bae580f0b7ad27766b27fba27ea0df

[Fix] CImageListEx::Create() needs to set a proper DC before calling CopyToCompatibleBitmap() which expects a proper reference to be passed in. Found with cppcheck. Broken since r10067.
git-svn-id: https://source.openmpt.org/svn/openmpt@10467 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/mptrack/CImageListEx.cpp
@@ -44,6 +44,8 @@ bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGr
 
 	bool result;
 
+	if(dc == nullptr) dc = CDC::FromHandle(GetDC(NULL));
+
 	// Use 1-bit transperency when there is no alpha channel.
 	if(GetDeviceCaps(dc->GetSafeHdc(), BITSPIXEL) * GetDeviceCaps(dc->GetSafeHdc(), PLANES) < 32)
 	{
@@ -93,7 +95,6 @@ bool CImageListEx::Create(UINT resourceID, int cx, int cy, int nInitial, int nGr
 		bi.bi.biSizeImage = static_cast<DWORD>(bitmapMask.size() * sizeof(decltype(bitmapMask[0])));
 		bi.col[1].rgbBlue = bi.col[1].rgbGreen = bi.col[1].rgbRed = 255;
 
-		if(dc == nullptr) dc = CDC::FromHandle(GetDC(NULL));
 		dibMask.CreateCompatibleBitmap(dc, bitmap->Width(), bitmap->Height());
 		SetDIBits(dc->GetSafeHdc(), dibMask, 0, bitmap->Height(), bitmapMask.data(), reinterpret_cast<BITMAPINFO *>(&bi), DIB_RGB_COLORS);
 