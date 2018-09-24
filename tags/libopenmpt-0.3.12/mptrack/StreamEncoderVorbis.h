bb907ebda57396df6861d41c56fa74daa38fbd10
manx <manx@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2018-09-24 06:42:52 +0000

    tag libopenmpt-0.3.12
    git-svn-id: https://source.openmpt.org/svn/openmpt@10856 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ tags/libopenmpt-0.3.12/mptrack/StreamEncoderVorbis.h
@@ -0,0 +1,37 @@
+/*
+ * StreamEncoder.h
+ * ---------------
+ * Purpose: Exporting streamed music files.
+ * Notes  : none
+ * Authors: Joern Heusipp
+ *          OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
+
+#pragma once
+
+#include "StreamEncoder.h"
+
+
+OPENMPT_NAMESPACE_BEGIN
+
+
+class VorbisEncoder : public EncoderFactoryBase
+{
+
+public:
+
+	std::unique_ptr<IAudioStreamEncoder> ConstructStreamEncoder(std::ostream &file, const Encoder::Settings &settings, const FileTags &tags) const;
+	bool IsBitrateSupported(int samplerate, int channels, int bitrate) const;
+	mpt::ustring DescribeQuality(float quality) const;
+	bool IsAvailable() const;
+
+public:
+
+	VorbisEncoder();
+	virtual ~VorbisEncoder();
+
+};
+
+
+OPENMPT_NAMESPACE_END
