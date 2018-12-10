svn2github/OpenMPT

commit d6cbf058410298331087d0698400d0e71e704227

[Fix] An instrument release node past the sustain loop end was not processed correctly with "IT-style envelope position advance + enable/disable behaviour" flag enabled (https://bugs.openmpt.org/view.php?id=1171, broken since r4009)
git-svn-id: https://source.openmpt.org/svn/openmpt@11013 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Sndmix.cpp
@@ -1241,7 +1241,7 @@ void CSoundFile::IncrementEnvelopePosition(ModChannel &chn, EnvelopeType envType
 
 		// IT compatiblity: OpenMPT processes the key-off flag earlier than IT. Grab the flag from the previous tick instead.
 		// Test case: EnvOffLength.it
-		if(insEnv.dwFlags[ENV_SUSTAIN] && !chn.dwOldFlags[CHN_KEYOFF])
+		if(insEnv.dwFlags[ENV_SUSTAIN] && !chn.dwOldFlags[CHN_KEYOFF] && (insEnv.nReleaseNode == ENV_RELEASE_NODE_UNSET || m_playBehaviour[kReleaseNodePastSustainBug]))
 		{
 			// Envelope sustained
 			start = insEnv[insEnv.nSustainStart].tick;

trunk/OpenMPT/mptrack/dlg_misc.cpp
@@ -601,6 +601,7 @@ BOOL CLegacyPlaybackSettingsDlg::OnInitDialog()
 		case kLegacyReleaseNode: desc = _T("Old volume envelope release node scaling behaviour"); break;
 		case kOPLBeatingOscillators: desc = _T("Beating OPL oscillators"); break;
 		case kST3OffsetWithoutInstrument: desc = _T("Notes without instrument use the previous note's sample offset"); break;
+		case kReleaseNodePastSustainBug: desc = _T("Broken release node after sustain end behaviour"); break;
 
 		default: MPT_ASSERT_NOTREACHED();
 		}

trunk/OpenMPT/soundlib/UpgradeModule.cpp
@@ -21,7 +21,6 @@ struct UpgradePatternData
 {
 	UpgradePatternData(CSoundFile &sf)
 		: sndFile(sf)
-		, chn(0)
 		, compatPlay(sf.m_playBehaviour[MSF_COMPATIBLE_PLAY]) { }
 
 	void operator() (ModCommand &m)
@@ -222,7 +221,7 @@ struct UpgradePatternData
 	}
 
 	const CSoundFile &sndFile;
-	CHANNELINDEX chn;
+	CHANNELINDEX chn = 0;
 	const bool compatPlay;
 };
 
@@ -336,14 +335,14 @@ void CSoundFile::UpgradeModule()
 	if(m_dwLastSavedWithVersion < MAKE_VERSION_NUMERIC(1, 22, 07, 01))
 	{
 		// Convert ANSI plugin path names to UTF-8 (irrelevant in probably 99% of all cases anyway, I think I've never seen a VST plugin with a non-ASCII file name)
-		for(PLUGINDEX i = 0; i < MAX_MIXPLUGINS; i++)
+		for(auto &plugin : m_MixPlugins)
 		{
 #if defined(MODPLUG_TRACKER)
-			const std::string name = mpt::ToCharset(mpt::CharsetUTF8, mpt::CharsetLocale, m_MixPlugins[i].Info.szLibraryName);
+			const std::string name = mpt::ToCharset(mpt::CharsetUTF8, mpt::CharsetLocale, plugin.Info.szLibraryName);
 #else
-			const std::string name = mpt::ToCharset(mpt::CharsetUTF8, mpt::CharsetWindows1252, m_MixPlugins[i].Info.szLibraryName);
+			const std::string name = mpt::ToCharset(mpt::CharsetUTF8, mpt::CharsetWindows1252, plugin.Info.szLibraryName);
 #endif
-			mpt::String::Copy(m_MixPlugins[i].Info.szLibraryName, name);
+			mpt::String::Copy(plugin.Info.szLibraryName, name);
 		}
 	}
 #endif // NO_PLUGINS
@@ -369,9 +368,9 @@ void CSoundFile::UpgradeModule()
 			if(Instruments[i] != nullptr && Instruments[i]->nVolSwing != 0 && Instruments[i]->nMidiChannel != MidiNoChannel)
 			{
 				bool hasSample = false;
-				for(size_t k = 0; k < CountOf(Instruments[k]->Keyboard); k++)
+				for(auto smp : Instruments[i]->Keyboard)
 				{
-					if(Instruments[i]->Keyboard[k] != 0)
+					if(smp != 0)
 					{
 						hasSample = true;
 						break;
@@ -611,7 +610,22 @@ void CSoundFile::UpgradeModule()
 		// Frequency slides were always in Hz rather than periods in this version range.
 		m_playBehaviour.set(kHertzInLinearMode);
 	}
-}
 
+	if(m_playBehaviour[kITEnvelopePositionHandling]
+		&& m_dwLastSavedWithVersion >= MAKE_VERSION_NUMERIC(1, 23, 01, 02) && m_dwLastSavedWithVersion < MAKE_VERSION_NUMERIC(1, 28, 00, 43))
+	{
+		// Bug that effectively clamped the release node to the sustain end
+		for(INSTRUMENTINDEX i = 1; i <= GetNumInstruments(); i++) if(Instruments[i] != nullptr)
+		{
+			if(Instruments[i]->VolEnv.nReleaseNode != ENV_RELEASE_NODE_UNSET
+				&& Instruments[i]->VolEnv.dwFlags[ENV_SUSTAIN]
+				&& Instruments[i]->VolEnv.nReleaseNode > Instruments[i]->VolEnv.nSustainEnd)
+			{
+				m_playBehaviour.set(kReleaseNodePastSustainBug);
+				break;
+			}
+		}
+	}
+}
 
 OPENMPT_NAMESPACE_END

commit 02a9f3d214c38a80b1dd01e01a972a84798f3714

[Fix] Release node completely disabled sustain loop in some cases (https://bugs.openmpt.org/view.php?id=1176) - regression introduced in r11013.
git-svn-id: https://source.openmpt.org/svn/openmpt@11020 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/soundlib/Sndmix.cpp
@@ -1239,7 +1239,7 @@ void CSoundFile::IncrementEnvelopePosition(ModChannel &chn, EnvelopeType envType
 
 		// IT compatiblity: OpenMPT processes the key-off flag earlier than IT. Grab the flag from the previous tick instead.
 		// Test case: EnvOffLength.it
-		if(insEnv.dwFlags[ENV_SUSTAIN] && !chn.dwOldFlags[CHN_KEYOFF] && (insEnv.nReleaseNode == ENV_RELEASE_NODE_UNSET || m_playBehaviour[kReleaseNodePastSustainBug]))
+		if(insEnv.dwFlags[ENV_SUSTAIN] && !chn.dwOldFlags[CHN_KEYOFF] && (chnEnv.nEnvValueAtReleaseJump == NOT_YET_RELEASED || m_playBehaviour[kReleaseNodePastSustainBug]))
 		{
 			// Envelope sustained
 			start = insEnv[insEnv.nSustainStart].tick;