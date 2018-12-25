svn2github/OpenMPT

commit 65874042bc2d7e19f0c6e4bfc830e89dc2696b74

[Var] libmodplug: Update public headers to v0.8.8.5. [Ref] libmodplug: Make use of MODPLUG_EXPORT (to the extent possible, that is). [Reg] libmodplug: Remove emulation of the ancient 0.8.7 API/ABI.
git-svn-id: https://source.openmpt.org/svn/openmpt@10730 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/libopenmpt/libopenmpt_modplug.c
@@ -28,20 +28,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* define to emulate 0.8.7 API/ABI instead of 0.8.8 API/ABI */
-/* #define LIBOPENMPT_MODPLUG_0_8_7 */
+#define MODPLUG_BUILD
+#ifdef _MSC_VER
+#ifdef MPT_BUILD_MSVC_SHARED
+#define DLL_EXPORT
+#endif /* MPT_BUILD_MSVC_SHARED */
+#ifdef MPT_BUILD_MSVC_STATIC
+#define MODPLUG_STATIC
+#endif /* MPT_BUILD_MSVC_STATIC */
+#endif /* _MSC_VER */
+#include "libmodplug/modplug.h"
 
 #ifdef _MSC_VER
-/* msvc errors when seeing dllexport declarations after prototypes have been declared in modplug.h */
 #define LIBOPENMPT_MODPLUG_API
 #else /* !_MSC_VER */
 #define LIBOPENMPT_MODPLUG_API LIBOPENMPT_API_HELPER_EXPORT
 #endif /* _MSC_VER */
-#ifdef LIBOPENMPT_MODPLUG_0_8_7
-#include "libmodplug/modplug_0.8.7.h"
-#else
-#include "libmodplug/modplug.h"
-#endif
 
 /* from libmodplug/sndfile.h */
 /* header is not c clean */
@@ -94,10 +96,8 @@ static ModPlug_Settings globalsettings = {
 	16,
 	44100,
 	MODPLUG_RESAMPLE_LINEAR,
-#ifndef LIBOPENMPT_MODPLUG_0_8_7
 	128,
 	256,
-#endif
 	0,
 	0,
 	0,
@@ -169,9 +169,7 @@ LIBOPENMPT_MODPLUG_API ModPlugFile* ModPlug_Load(const void* data, int size)
 			strcpy(file->message,"");
 		}
 	}
-#ifndef LIBOPENMPT_MODPLUG_0_8_7
 	openmpt_module_set_render_param(file->mod,OPENMPT_MODULE_RENDER_STEREOSEPARATION_PERCENT,file->settings.mStereoSeparation*100/128);
-#endif
 	openmpt_module_set_render_param(file->mod,OPENMPT_MODULE_RENDER_INTERPOLATIONFILTER_LENGTH,modplugresamplingmode_to_filterlength(file->settings.mResamplingMode));
 	return file;
 }
@@ -621,75 +619,5 @@ LIBOPENMPT_MODPLUG_API char ModPlug_ExportIT(ModPlugFile* file, const char* file
 	fprintf(stderr,"libopenmpt-modplug: error: ModPlug_ExportIT(%s) not implemented.\n",filepath);
 	return 0;
 }
-
-#ifdef _MSC_VER
-#ifdef _M_IX86
-#pragma comment(linker, "/EXPORT:ModPlug_Load=_ModPlug_Load")
-#pragma comment(linker, "/EXPORT:ModPlug_Unload=_ModPlug_Unload")
-#pragma comment(linker, "/EXPORT:ModPlug_Read=_ModPlug_Read")
-#pragma comment(linker, "/EXPORT:ModPlug_GetName=_ModPlug_GetName")
-#pragma comment(linker, "/EXPORT:ModPlug_GetLength=_ModPlug_GetLength")
-#pragma comment(linker, "/EXPORT:ModPlug_Seek=_ModPlug_Seek")
-#pragma comment(linker, "/EXPORT:ModPlug_GetSettings=_ModPlug_GetSettings")
-#pragma comment(linker, "/EXPORT:ModPlug_SetSettings=_ModPlug_SetSettings")
-#pragma comment(linker, "/EXPORT:ModPlug_GetMasterVolume=_ModPlug_GetMasterVolume")
-#pragma comment(linker, "/EXPORT:ModPlug_SetMasterVolume=_ModPlug_SetMasterVolume")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentSpeed=_ModPlug_GetCurrentSpeed")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentTempo=_ModPlug_GetCurrentTempo")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentOrder=_ModPlug_GetCurrentOrder")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentPattern=_ModPlug_GetCurrentPattern")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentRow=_ModPlug_GetCurrentRow")
-#pragma comment(linker, "/EXPORT:ModPlug_GetPlayingChannels=_ModPlug_GetPlayingChannels")
-#pragma comment(linker, "/EXPORT:ModPlug_SeekOrder=_ModPlug_SeekOrder")
-#pragma comment(linker, "/EXPORT:ModPlug_GetModuleType=_ModPlug_GetModuleType")
-#pragma comment(linker, "/EXPORT:ModPlug_GetMessage=_ModPlug_GetMessage")
-#pragma comment(linker, "/EXPORT:ModPlug_NumInstruments=_ModPlug_NumInstruments")
-#pragma comment(linker, "/EXPORT:ModPlug_NumSamples=_ModPlug_NumSamples")
-#pragma comment(linker, "/EXPORT:ModPlug_NumPatterns=_ModPlug_NumPatterns")
-#pragma comment(linker, "/EXPORT:ModPlug_NumChannels=_ModPlug_NumChannels")
-#pragma comment(linker, "/EXPORT:ModPlug_SampleName=_ModPlug_SampleName")
-#pragma comment(linker, "/EXPORT:ModPlug_InstrumentName=_ModPlug_InstrumentName")
-#pragma comment(linker, "/EXPORT:ModPlug_GetPattern=_ModPlug_GetPattern")
-#pragma comment(linker, "/EXPORT:ModPlug_InitMixerCallback=_ModPlug_InitMixerCallback")
-#pragma comment(linker, "/EXPORT:ModPlug_UnloadMixerCallback=_ModPlug_UnloadMixerCallback")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportS3M=_ModPlug_ExportS3M")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportXM=_ModPlug_ExportXM")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportMOD=_ModPlug_ExportMOD")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportIT=_ModPlug_ExportIT")
-#else /* !_M_IX86 */
-#pragma comment(linker, "/EXPORT:ModPlug_Load")
-#pragma comment(linker, "/EXPORT:ModPlug_Unload")
-#pragma comment(linker, "/EXPORT:ModPlug_Read")
-#pragma comment(linker, "/EXPORT:ModPlug_GetName")
-#pragma comment(linker, "/EXPORT:ModPlug_GetLength")
-#pragma comment(linker, "/EXPORT:ModPlug_Seek")
-#pragma comment(linker, "/EXPORT:ModPlug_GetSettings")
-#pragma comment(linker, "/EXPORT:ModPlug_SetSettings")
-#pragma comment(linker, "/EXPORT:ModPlug_GetMasterVolume")
-#pragma comment(linker, "/EXPORT:ModPlug_SetMasterVolume")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentSpeed")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentTempo")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentOrder")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentPattern")
-#pragma comment(linker, "/EXPORT:ModPlug_GetCurrentRow")
-#pragma comment(linker, "/EXPORT:ModPlug_GetPlayingChannels")
-#pragma comment(linker, "/EXPORT:ModPlug_SeekOrder")
-#pragma comment(linker, "/EXPORT:ModPlug_GetModuleType")
-#pragma comment(linker, "/EXPORT:ModPlug_GetMessage")
-#pragma comment(linker, "/EXPORT:ModPlug_NumInstruments")
-#pragma comment(linker, "/EXPORT:ModPlug_NumSamples")
-#pragma comment(linker, "/EXPORT:ModPlug_NumPatterns")
-#pragma comment(linker, "/EXPORT:ModPlug_NumChannels")
-#pragma comment(linker, "/EXPORT:ModPlug_SampleName")
-#pragma comment(linker, "/EXPORT:ModPlug_InstrumentName")
-#pragma comment(linker, "/EXPORT:ModPlug_GetPattern")
-#pragma comment(linker, "/EXPORT:ModPlug_InitMixerCallback")
-#pragma comment(linker, "/EXPORT:ModPlug_UnloadMixerCallback")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportS3M")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportXM")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportMOD")
-#pragma comment(linker, "/EXPORT:ModPlug_ExportIT")
-#endif /* _M_IX86 */
-#endif /* _MSC_VER */
 
 #endif /* NO_LIBMODPLUG */

trunk/OpenMPT/libopenmpt/libopenmpt_modplug_cpp.cpp
@@ -46,27 +46,27 @@ Metadata and other state is not provided or updated.
 #include <cstdlib>
 #include <cstring>
 
+#define MODPLUG_BUILD
 #ifdef _MSC_VER
-/* msvc errors when seeing dllexport declarations after prototypes have been declared in modplug.h */
+/* libmodplug C++ header is broken for MSVC DLL builds */
+#define MODPLUG_STATIC
+#endif /* _MSC_VER */
+#include "libmodplug/stdafx.h"
+#include "libmodplug/sndfile.h"
+
+#ifdef _MSC_VER
 #define LIBOPENMPT_MODPLUG_API
 #else /* !_MSC_VER */
 #define LIBOPENMPT_MODPLUG_API LIBOPENMPT_API_HELPER_EXPORT
 #endif /* _MSC_VER */
 
 namespace {
-
 template <class T>
 void Clear( T & x )
 {
 	std::memset( &x, 0, sizeof(T) );
 }
-
 }
-
-class LIBOPENMPT_MODPLUG_API CSoundFile;
-
-#include "libmodplug/stdafx.h"
-#include "libmodplug/sndfile.h"
 
 //#define mpcpplog() fprintf(stderr, "%s %i\n", __func__, __LINE__)
 #define mpcpplog() do{}while(0)

trunk/OpenMPT/include/modplug/include/libmodplug/modplug.h
@@ -11,6 +11,21 @@
 extern "C" {
 #endif
 
+#if defined(_WIN32) || defined(__CYGWIN__)
+# if defined(MODPLUG_BUILD) && defined(DLL_EXPORT)	/* building libmodplug as a dll for windows */
+#   define MODPLUG_EXPORT __declspec(dllexport)
+# elif defined(MODPLUG_BUILD) || defined(MODPLUG_STATIC)	/* building or using static libmodplug for windows */
+#   define MODPLUG_EXPORT
+# else
+#   define MODPLUG_EXPORT __declspec(dllimport)			/* using libmodplug dll for windows */
+# endif
+/* FIXME: USE VISIBILITY ATTRIBUTES HERE */
+#elif defined(MODPLUG_BUILD)
+#define MODPLUG_EXPORT
+#else
+#define MODPLUG_EXPORT
+#endif
+
 struct _ModPlugFile;
 typedef struct _ModPlugFile ModPlugFile;
 
@@ -29,21 +44,21 @@ typedef void (*ModPlugMixerProc)(int*, unsigned long, unsigned long);
 /* Load a mod file.  [data] should point to a block of memory containing the complete
  * file, and [size] should be the size of that block.
  * Return the loaded mod file on success, or NULL on failure. */
-ModPlugFile* ModPlug_Load(const void* data, int size);
+MODPLUG_EXPORT ModPlugFile* ModPlug_Load(const void* data, int size);
 /* Unload a mod file. */
-void ModPlug_Unload(ModPlugFile* file);
+MODPLUG_EXPORT void ModPlug_Unload(ModPlugFile* file);
 
 /* Read sample data into the buffer.  Returns the number of bytes read.  If the end
  * of the mod has been reached, zero is returned. */
-int  ModPlug_Read(ModPlugFile* file, void* buffer, int size);
+MODPLUG_EXPORT int  ModPlug_Read(ModPlugFile* file, void* buffer, int size);
 
 /* Get the name of the mod.  The returned buffer is stored within the ModPlugFile
  * structure and will remain valid until you unload the file. */
-const char* ModPlug_GetName(ModPlugFile* file);
+MODPLUG_EXPORT const char* ModPlug_GetName(ModPlugFile* file);
 
 /* Get the length of the mod, in milliseconds.  Note that this result is not always
  * accurate, especially in the case of mods with loops. */
-int ModPlug_GetLength(ModPlugFile* file);
+MODPLUG_EXPORT int ModPlug_GetLength(ModPlugFile* file);
 
 /* Seek to a particular position in the song.  Note that seeking and MODs don't mix very
  * well.  Some mods will be missing instruments for a short time after a seek, as ModPlug
@@ -51,7 +66,7 @@ int ModPlug_GetLength(ModPlugFile* file);
  * playing at that time.  (Doing so would be difficult and not very reliable.)  Also,
  * note that seeking is not very exact in some mods -- especially those for which
  * ModPlug_GetLength() does not report the full length. */
-void ModPlug_Seek(ModPlugFile* file, int millisecond);
+MODPLUG_EXPORT void ModPlug_Seek(ModPlugFile* file, int millisecond);
 
 enum _ModPlug_Flags
 {
@@ -97,24 +112,24 @@ typedef struct _ModPlug_Settings
 /* Get and set the mod decoder settings.  All options, except for channels, bits-per-sample,
  * sampling rate, and loop count, will take effect immediately.  Those options which don't
  * take effect immediately will take effect the next time you load a mod. */
-void ModPlug_GetSettings(ModPlug_Settings* settings);
-void ModPlug_SetSettings(const ModPlug_Settings* settings);
+MODPLUG_EXPORT void ModPlug_GetSettings(ModPlug_Settings* settings);
+MODPLUG_EXPORT void ModPlug_SetSettings(const ModPlug_Settings* settings);
 
 /* New ModPlug API Functions */
 /* NOTE: Master Volume (1-512) */
-unsigned int ModPlug_GetMasterVolume(ModPlugFile* file) ;
-void ModPlug_SetMasterVolume(ModPlugFile* file,unsigned int cvol) ;
+MODPLUG_EXPORT unsigned int ModPlug_GetMasterVolume(ModPlugFile* file) ;
+MODPLUG_EXPORT void ModPlug_SetMasterVolume(ModPlugFile* file,unsigned int cvol) ;
 
-int ModPlug_GetCurrentSpeed(ModPlugFile* file);
-int ModPlug_GetCurrentTempo(ModPlugFile* file);
-int ModPlug_GetCurrentOrder(ModPlugFile* file);
-int ModPlug_GetCurrentPattern(ModPlugFile* file);
-int ModPlug_GetCurrentRow(ModPlugFile* file);
-int ModPlug_GetPlayingChannels(ModPlugFile* file);
+MODPLUG_EXPORT int ModPlug_GetCurrentSpeed(ModPlugFile* file);
+MODPLUG_EXPORT int ModPlug_GetCurrentTempo(ModPlugFile* file);
+MODPLUG_EXPORT int ModPlug_GetCurrentOrder(ModPlugFile* file);
+MODPLUG_EXPORT int ModPlug_GetCurrentPattern(ModPlugFile* file);
+MODPLUG_EXPORT int ModPlug_GetCurrentRow(ModPlugFile* file);
+MODPLUG_EXPORT int ModPlug_GetPlayingChannels(ModPlugFile* file);
 
-void ModPlug_SeekOrder(ModPlugFile* file,int order);
-int ModPlug_GetModuleType(ModPlugFile* file);
-char* ModPlug_GetMessage(ModPlugFile* file);
+MODPLUG_EXPORT void ModPlug_SeekOrder(ModPlugFile* file,int order);
+MODPLUG_EXPORT int ModPlug_GetModuleType(ModPlugFile* file);
+MODPLUG_EXPORT char* ModPlug_GetMessage(ModPlugFile* file);
 
 
 #ifndef MODPLUG_NO_FILESAVE
@@ -122,29 +137,29 @@ char* ModPlug_GetMessage(ModPlugFile* file);
  * EXPERIMENTAL Export Functions
  */
 /*Export to a Scream Tracker 3 S3M module. EXPERIMENTAL (only works on Little-Endian platforms)*/
-char ModPlug_ExportS3M(ModPlugFile* file, const char* filepath);
+MODPLUG_EXPORT char ModPlug_ExportS3M(ModPlugFile* file, const char* filepath);
 
 /*Export to a Extended Module (XM). EXPERIMENTAL (only works on Little-Endian platforms)*/
-char ModPlug_ExportXM(ModPlugFile* file, const char* filepath);
+MODPLUG_EXPORT char ModPlug_ExportXM(ModPlugFile* file, const char* filepath);
 
 /*Export to a Amiga MOD file. EXPERIMENTAL.*/
-char ModPlug_ExportMOD(ModPlugFile* file, const char* filepath);
+MODPLUG_EXPORT char ModPlug_ExportMOD(ModPlugFile* file, const char* filepath);
 
 /*Export to a Impulse Tracker IT file. Should work OK in Little-Endian & Big-Endian platforms :-) */
-char ModPlug_ExportIT(ModPlugFile* file, const char* filepath);
+MODPLUG_EXPORT char ModPlug_ExportIT(ModPlugFile* file, const char* filepath);
 #endif // MODPLUG_NO_FILESAVE
 
-unsigned int ModPlug_NumInstruments(ModPlugFile* file);
-unsigned int ModPlug_NumSamples(ModPlugFile* file);
-unsigned int ModPlug_NumPatterns(ModPlugFile* file);
-unsigned int ModPlug_NumChannels(ModPlugFile* file);
-unsigned int ModPlug_SampleName(ModPlugFile* file, unsigned int qual, char* buff);
-unsigned int ModPlug_InstrumentName(ModPlugFile* file, unsigned int qual, char* buff);
+MODPLUG_EXPORT unsigned int ModPlug_NumInstruments(ModPlugFile* file);
+MODPLUG_EXPORT unsigned int ModPlug_NumSamples(ModPlugFile* file);
+MODPLUG_EXPORT unsigned int ModPlug_NumPatterns(ModPlugFile* file);
+MODPLUG_EXPORT unsigned int ModPlug_NumChannels(ModPlugFile* file);
+MODPLUG_EXPORT unsigned int ModPlug_SampleName(ModPlugFile* file, unsigned int qual, char* buff);
+MODPLUG_EXPORT unsigned int ModPlug_InstrumentName(ModPlugFile* file, unsigned int qual, char* buff);
 
 /*
  * Retrieve pattern note-data
  */
-ModPlugNote* ModPlug_GetPattern(ModPlugFile* file, int pattern, unsigned int* numrows);
+MODPLUG_EXPORT ModPlugNote* ModPlug_GetPattern(ModPlugFile* file, int pattern, unsigned int* numrows);
 
 /*
  * =================
@@ -161,8 +176,8 @@ ModPlugNote* ModPlug_GetPattern(ModPlugFile* file, int pattern, unsigned int* nu
  *
  * (Samples are signed 32-bit integers)
  */
-void ModPlug_InitMixerCallback(ModPlugFile* file,ModPlugMixerProc proc) ;
-void ModPlug_UnloadMixerCallback(ModPlugFile* file) ;
+MODPLUG_EXPORT void ModPlug_InitMixerCallback(ModPlugFile* file,ModPlugMixerProc proc) ;
+MODPLUG_EXPORT void ModPlug_UnloadMixerCallback(ModPlugFile* file) ;
 
 #ifdef __cplusplus
 } /* extern "C" */

trunk/OpenMPT/include/modplug/include/libmodplug/modplug_0.8.7.h
@@ -1,168 +1,0 @@
-/*
- * This source code is public domain.
- *
- * Authors: Kenton Varda <temporal@gauge3d.org> (C interface wrapper)
- */
-
-#ifndef MODPLUG_H__INCLUDED
-#define MODPLUG_H__INCLUDED
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-struct _ModPlugFile;
-typedef struct _ModPlugFile ModPlugFile;
-
-struct _ModPlugNote {
-	unsigned char Note;
-	unsigned char Instrument;
-	unsigned char VolumeEffect;
-	unsigned char Effect;
-	unsigned char Volume;
-	unsigned char Parameter;
-};
-typedef struct _ModPlugNote ModPlugNote;
-
-typedef void (*ModPlugMixerProc)(int*, unsigned long, unsigned long);
-
-/* Load a mod file.  [data] should point to a block of memory containing the complete
- * file, and [size] should be the size of that block.
- * Return the loaded mod file on success, or NULL on failure. */
-ModPlugFile* ModPlug_Load(const void* data, int size);
-/* Unload a mod file. */
-void ModPlug_Unload(ModPlugFile* file);
-
-/* Read sample data into the buffer.  Returns the number of bytes read.  If the end
- * of the mod has been reached, zero is returned. */
-int  ModPlug_Read(ModPlugFile* file, void* buffer, int size);
-
-/* Get the name of the mod.  The returned buffer is stored within the ModPlugFile
- * structure and will remain valid until you unload the file. */
-const char* ModPlug_GetName(ModPlugFile* file);
-
-/* Get the length of the mod, in milliseconds.  Note that this result is not always
- * accurate, especially in the case of mods with loops. */
-int ModPlug_GetLength(ModPlugFile* file);
-
-/* Seek to a particular position in the song.  Note that seeking and MODs don't mix very
- * well.  Some mods will be missing instruments for a short time after a seek, as ModPlug
- * does not scan the sequence backwards to find out which instruments were supposed to be
- * playing at that time.  (Doing so would be difficult and not very reliable.)  Also,
- * note that seeking is not very exact in some mods -- especially those for which
- * ModPlug_GetLength() does not report the full length. */
-void ModPlug_Seek(ModPlugFile* file, int millisecond);
-
-enum _ModPlug_Flags
-{
-	MODPLUG_ENABLE_OVERSAMPLING     = 1 << 0,  /* Enable oversampling (*highly* recommended) */
-	MODPLUG_ENABLE_NOISE_REDUCTION  = 1 << 1,  /* Enable noise reduction */
-	MODPLUG_ENABLE_REVERB           = 1 << 2,  /* Enable reverb */
-	MODPLUG_ENABLE_MEGABASS         = 1 << 3,  /* Enable megabass */
-	MODPLUG_ENABLE_SURROUND         = 1 << 4   /* Enable surround sound. */
-};
-
-enum _ModPlug_ResamplingMode
-{
-	MODPLUG_RESAMPLE_NEAREST = 0,  /* No interpolation (very fast, extremely bad sound quality) */
-	MODPLUG_RESAMPLE_LINEAR  = 1,  /* Linear interpolation (fast, good quality) */
-	MODPLUG_RESAMPLE_SPLINE  = 2,  /* Cubic spline interpolation (high quality) */
-	MODPLUG_RESAMPLE_FIR     = 3   /* 8-tap fir filter (extremely high quality) */
-};
-
-typedef struct _ModPlug_Settings
-{
-	int mFlags;  /* One or more of the MODPLUG_ENABLE_* flags above, bitwise-OR'ed */
-	
-	/* Note that ModPlug always decodes sound at 44100kHz, 32 bit, stereo and then
-	 * down-mixes to the settings you choose. */
-	int mChannels;       /* Number of channels - 1 for mono or 2 for stereo */
-	int mBits;           /* Bits per sample - 8, 16, or 32 */
-	int mFrequency;      /* Sampling rate - 11025, 22050, or 44100 */
-	int mResamplingMode; /* One of MODPLUG_RESAMPLE_*, above */
-	
-	int mReverbDepth;    /* Reverb level 0(quiet)-100(loud)      */
-	int mReverbDelay;    /* Reverb delay in ms, usually 40-200ms */
-	int mBassAmount;     /* XBass level 0(quiet)-100(loud)       */
-	int mBassRange;      /* XBass cutoff in Hz 10-100            */
-	int mSurroundDepth;  /* Surround level 0(quiet)-100(heavy)   */
-	int mSurroundDelay;  /* Surround delay in ms, usually 5-40ms */
-	int mLoopCount;      /* Number of times to loop.  Zero prevents looping.
-	                        -1 loops forever. */
-} ModPlug_Settings;
-
-/* Get and set the mod decoder settings.  All options, except for channels, bits-per-sample,
- * sampling rate, and loop count, will take effect immediately.  Those options which don't
- * take effect immediately will take effect the next time you load a mod. */
-void ModPlug_GetSettings(ModPlug_Settings* settings);
-void ModPlug_SetSettings(const ModPlug_Settings* settings);
-
-/* New ModPlug API Functions */
-/* NOTE: Master Volume (1-512) */
-unsigned int ModPlug_GetMasterVolume(ModPlugFile* file) ;
-void ModPlug_SetMasterVolume(ModPlugFile* file,unsigned int cvol) ;
-
-int ModPlug_GetCurrentSpeed(ModPlugFile* file);
-int ModPlug_GetCurrentTempo(ModPlugFile* file);
-int ModPlug_GetCurrentOrder(ModPlugFile* file);
-int ModPlug_GetCurrentPattern(ModPlugFile* file);
-int ModPlug_GetCurrentRow(ModPlugFile* file);
-int ModPlug_GetPlayingChannels(ModPlugFile* file);
-
-void ModPlug_SeekOrder(ModPlugFile* file,int order);
-int ModPlug_GetModuleType(ModPlugFile* file);
-char* ModPlug_GetMessage(ModPlugFile* file);
-
-
-#ifndef MODPLUG_NO_FILESAVE
-/*
- * EXPERIMENTAL Export Functions
- */
-/*Export to a Scream Tracker 3 S3M module. EXPERIMENTAL (only works on Little-Endian platforms)*/
-char ModPlug_ExportS3M(ModPlugFile* file, const char* filepath);
-
-/*Export to a Extended Module (XM). EXPERIMENTAL (only works on Little-Endian platforms)*/
-char ModPlug_ExportXM(ModPlugFile* file, const char* filepath);
-
-/*Export to a Amiga MOD file. EXPERIMENTAL.*/
-char ModPlug_ExportMOD(ModPlugFile* file, const char* filepath);
-
-/*Export to a Impulse Tracker IT file. Should work OK in Little-Endian & Big-Endian platforms :-) */
-char ModPlug_ExportIT(ModPlugFile* file, const char* filepath);
-#endif // MODPLUG_NO_FILESAVE
-
-unsigned int ModPlug_NumInstruments(ModPlugFile* file);
-unsigned int ModPlug_NumSamples(ModPlugFile* file);
-unsigned int ModPlug_NumPatterns(ModPlugFile* file);
-unsigned int ModPlug_NumChannels(ModPlugFile* file);
-unsigned int ModPlug_SampleName(ModPlugFile* file, unsigned int qual, char* buff);
-unsigned int ModPlug_InstrumentName(ModPlugFile* file, unsigned int qual, char* buff);
-
-/*
- * Retrieve pattern note-data
- */
-ModPlugNote* ModPlug_GetPattern(ModPlugFile* file, int pattern, unsigned int* numrows);
-
-/*
- * =================
- * Mixer callback
- * =================
- *
- * Use this callback if you want to 'modify' the mixed data of LibModPlug.
- * 
- * void proc(int* buffer,unsigned long channels,unsigned long nsamples) ;
- *
- * 'buffer': A buffer of mixed samples
- * 'channels': N. of channels in the buffer
- * 'nsamples': N. of samples in the buffeer (without taking care of n.channels)
- *
- * (Samples are signed 32-bit integers)
- */
-void ModPlug_InitMixerCallback(ModPlugFile* file,ModPlugMixerProc proc) ;
-void ModPlug_UnloadMixerCallback(ModPlugFile* file) ;
-
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
-
-#endif

trunk/OpenMPT/include/modplug/include/libmodplug/sndfile.h
@@ -456,7 +456,7 @@ typedef struct _MODCOMMAND
 // Mix Plugins
 #define MIXPLUG_MIXREADY			0x01	// Set when cleared
 
-class IMixPlugin
+class MODPLUG_EXPORT IMixPlugin
 {
 public:
 	virtual ~IMixPlugin();
@@ -535,7 +535,7 @@ typedef VOID (* LPSNDMIXHOOKPROC)(int *, unsigned long, unsigned long); // buffe
 
 
 //==============
-class CSoundFile
+class MODPLUG_EXPORT CSoundFile
 //==============
 {
 public:	// Static Members

trunk/OpenMPT/include/modplug/include/libmodplug/stdafx.h
@@ -48,6 +48,10 @@ inline void ProcessPlugins(int n) {}
 #define strnicmp(a,b,c)		strncasecmp(a,b,c)
 #define HAVE_SINF 1
 
+#ifndef isblank
+#define isblank(c) ((c) == ' ' || (c) == '\t')
+#endif
+
 #else
 
 #include <stdlib.h>
@@ -104,7 +108,7 @@ inline int8_t * GlobalAllocPtr(unsigned int, size_t size)
   return p;
 }
 
-inline void ProcessPlugins(int /* n */ ) {}
+inline void ProcessPlugins(int n) {}
 
 #define GlobalFreePtr(p) free((void *)(p))
 
@@ -120,6 +124,21 @@ inline void ProcessPlugins(int /* n */ ) {}
 #endif
 
 #endif // _WIN32
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+# if defined(MODPLUG_BUILD) && defined(DLL_EXPORT)	/* building libmodplug as a dll for windows */
+#   define MODPLUG_EXPORT __declspec(dllexport)
+# elif defined(MODPLUG_BUILD) || defined(MODPLUG_STATIC)	/* building or using static libmodplug for windows */
+#   define MODPLUG_EXPORT
+# else
+#   define MODPLUG_EXPORT __declspec(dllimport)			/* using libmodplug dll for windows */
+# endif
+/* FIXME: USE VISIBILITY ATTRIBUTES HERE */
+#elif defined(MODPLUG_BUILD)
+#define MODPLUG_EXPORT
+#else
+#define MODPLUG_EXPORT
+#endif
 
 #endif
 

commit 0acbc3f104f556545fdb999c50957f5305945410

[Fix] libmodplug: We need to exlicitly export public symbols of the C++ API in non-MSVC builds by setting the visibility attribute. The libmodplug headers do not achieve that right now. Fixes <https://bugs.openmpt.org/view.php?id=1180>. (broken since r10730)
git-svn-id: https://source.openmpt.org/svn/openmpt@11110 56274372-70c3-4bfc-bfc3-4c3a0b034d27

trunk/OpenMPT/libopenmpt/libopenmpt_modplug.c
@@ -37,13 +37,12 @@
 #define MODPLUG_STATIC
 #endif /* MPT_BUILD_MSVC_STATIC */
 #endif /* _MSC_VER */
-#include "libmodplug/modplug.h"
-
 #ifdef _MSC_VER
 #define LIBOPENMPT_MODPLUG_API
 #else /* !_MSC_VER */
 #define LIBOPENMPT_MODPLUG_API LIBOPENMPT_API_HELPER_EXPORT
 #endif /* _MSC_VER */
+#include "libmodplug/modplug.h"
 
 /* from libmodplug/sndfile.h */
 /* header is not c clean */

trunk/OpenMPT/libopenmpt/libopenmpt_modplug_cpp.cpp
@@ -51,14 +51,14 @@ Metadata and other state is not provided or updated.
 /* libmodplug C++ header is broken for MSVC DLL builds */
 #define MODPLUG_STATIC
 #endif /* _MSC_VER */
-#include "libmodplug/stdafx.h"
-#include "libmodplug/sndfile.h"
-
 #ifdef _MSC_VER
 #define LIBOPENMPT_MODPLUG_API
 #else /* !_MSC_VER */
 #define LIBOPENMPT_MODPLUG_API LIBOPENMPT_API_HELPER_EXPORT
 #endif /* _MSC_VER */
+class LIBOPENMPT_MODPLUG_API CSoundFile;
+#include "libmodplug/stdafx.h"
+#include "libmodplug/sndfile.h"
 
 namespace {
 template <class T>